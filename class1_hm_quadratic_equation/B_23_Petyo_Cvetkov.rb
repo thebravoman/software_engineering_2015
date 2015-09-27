2ce74863490a3d61e2c4e926c44f6f11393658da
petiocvetkov <petiopetio@mail.bg>
2015-09-27 15:43:35 +0300

    Update B_23_Petyo_Cvetkov.rb

--- class1_hm_quadratic_equation/B_23_Petyo_Cvetkov.rb
+++ class1_hm_quadratic_equation/B_23_Petyo_Cvetkov.rb
@@ -10,8 +10,8 @@ elsif d == 0
           x = -b / 2 * a
           puts "X = #{x}"
     else
-              x1 = (-b + Math.sqrt(D))/(2*a)
-              x2 = (b + Math.sqrt(D))/(2*a)
+              x1 = (-b + Math.sqrt(d))/(2*a)
+              x2 = (b + Math.sqrt(d))/(2*a)
             puts "X1 = #{x1}"
             puts "X2 = #{x2}"
   end
