11d4e1f840c9b262804cfbc68eaf79f41d8d117d
emchoko <emilfrogy@gmail.com>
2015-09-27 10:55:46 +0300

    Added Quadratic equation task

---
+++ class1_hm_quadratic_equation/A_13_Emil_Lozev.rb
@@ -0,0 +1,22 @@
+a = ARGV[0].to_i
+b = ARGV[1].to_i
+c = ARGV[2].to_i
+
+d = b**2 - 4 * a * c
+
+if d < 0
+
+	puts "The Quadratic Equation Has Unreal Roots"
+
+elsif d == 0
+	
+	x = (-b) / (2 * a)
+	puts x
+
+else
+
+	x1 = (-b + Math.sqrt(d)) / (2 * a)	
+	x2 = (-b - Math.sqrt(d)) / (2 * a)
+	
+	puts "#{x1},#{x2}"
+end
