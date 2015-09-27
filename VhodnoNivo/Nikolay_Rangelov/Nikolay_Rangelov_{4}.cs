0a8fffe5d0ee1d48259cc62fb7caea7a42798a82
Nikolay Rangelov <nikolay.rangelow@gmail.com>
2015-09-27 08:01:51 -0400

    Entry level added

---
+++ VhodnoNivo/Nikolay_Rangelov/Nikolay_Rangelov_{4}.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Task4
+{
+    static List<int> FibonacciNumbers(int number)
+    {
+        List<int> fibonacciNumbers = new List<int>();
+        fibonacciNumbers.Add(1);
+        fibonacciNumbers.Add(1);
+        int i = 1;
+        while(fibonacciNumbers[i] <= number)
+        {
+            fibonacciNumbers.Add(fibonacciNumbers[i] + fibonacciNumbers[i - 1]);
+            i++;
+        }
+        return fibonacciNumbers;
+    }
+
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        List<int> fibonacciNumbers = FibonacciNumbers(y);
+        foreach (int element in fibonacciNumbers)
+        {
+            if(element >= x && element <=y)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
---
+++ VhodnoNivo/Nikolay_Rangelov/Nikolay_Rangelov_{5}.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Task5
+{
+    static void Main()
+    {
+        //int a = int.Parse(Console.ReadLine());
+
+        double[] numbers;
+        numbers = new double[10];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Math.Cos(double.Parse(i.ToString()));
+        }
+
+        foreach(double element in numbers)
+        {
+            Console.WriteLine(element);
+        }
+
+        Console.WriteLine();
+        Array.Sort(numbers);
+        Array.Reverse(numbers);
+
+        foreach (double element in numbers)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
+
