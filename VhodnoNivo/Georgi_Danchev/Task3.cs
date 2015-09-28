15572918b1e7669968fdecfb36eea0a8811eb8ba
nooro <georgi_danchev@abv.bg>
2015-09-28 19:26:29 +0300

    Added first exam

---
+++ VhodnoNivo/Georgi_Danchev/Task3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task1
+{
+    class Task1
+    {
+        static void Main()
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            if (x > y)
+            {
+                for (int i = y; i <= x; i++)
+                {
+                    if (i % 10 == 3)
+                    {
+                        Console.WriteLine(i); 
+                    }
+                }
+            }
+            else if (y > x)
+            {
+                for (int i = x; i <= y; i++)
+                {
+                    if (i % 10 == 3)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
