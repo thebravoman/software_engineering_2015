200350e5cee09a7c2dec90af0b5e12b36c74dbfe
bakasura980 <bakasura@abv.bg>
2015-09-28 17:05:08 +0300

    Entry Level

---
+++ VhodnoNivo/Lubomir_Kiprov/Lubomir_Kiprov_{1}/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber, secondnumber, sum = 0;
+            do
+            {
+                Console.WriteLine("Input two numbers (first have to be smallest than second)");
+                firstnumber = int.Parse(Console.ReadLine());
+                secondnumber = int.Parse(Console.ReadLine());
+            } while (firstnumber > secondnumber);
+            for (;  firstnumber < secondnumber; firstnumber++)
+            {
+                if (firstnumber % 2 != 0)
+                {
+                    sum += firstnumber;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ VhodnoNivo/Lubomir_Kiprov/Lubomir_Kiprov_{9}/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            do
+            {
+                Console.WriteLine("Type number >= 0 or number < 10");
+                x = int.Parse(Console.ReadLine());
+            } while (x < 0 || x > 10);
+            double[] arr = new double[10];
+            int i = 0, cosargument = 1;
+            string xconvert = x.ToString();
+            do
+            {
+                string convert = Math.Cos(cosargument).ToString();
+                if (convert[0] != '-')
+                {
+                    if (convert[4] == xconvert[0])
+                    {
+                        arr[i] = double.Parse(convert);
+                        i++;
+                    }
+                }
+                else
+                {
+                    if (convert[5] == xconvert[0])
+                    {
+                        arr[i] = double.Parse(convert);
+                        i++;
+                    }
+                }
+              
+                cosargument++;
+            } while (i < arr.Length);
+            for (int j = 0; j < arr.Length; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+        }
+    }
+}
