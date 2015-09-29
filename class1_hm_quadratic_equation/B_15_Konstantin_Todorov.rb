94da9386e70c7204dbfaf127a3a7ff055e7a7cd5
Konstantin Todorov <konstantin.todorov98@Gmail.com>
2015-09-29 23:11:59 +0300

    Added ascendic order
    I should learn to read 1st ...

--- class1_hm_quadratic_equation/B_15_Konstantin_Todorov.rb
+++ class1_hm_quadratic_equation/B_15_Konstantin_Todorov.rb
@@ -4,15 +4,19 @@ c = ARGV[2].to_i()
 
 d=(b*b)-(4*a*c)
 
-if d<0 
+if (d<0) 
     puts "Nqma re6enie"
 
 elsif d==0;
     x= -b/(2*a);
-    puts "x1=#{x}";
+    puts "#{x}";
 else
     x1= -b+Math.sqrt(d)/(2*a)
     x2= -b-Math.sqrt(d)/(2*a)
-    puts "x1=#{x1},x2=#{x2}"
-end;
+        if (x1 < x2)
+    puts "#{x1},#{x2}"
+        elsif
+    puts "#{x2},#{x1}"
+        end;
 
+end;
