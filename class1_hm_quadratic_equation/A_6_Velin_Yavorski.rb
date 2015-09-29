0724602fd644e588d6342758e3389d62dbd8417b
velioo <velioocs@mail.bg>
2015-09-30 00:12:06 +0300

    Fixed QE

--- class1_hm_quadratic_equation/A_6_Velin_Yavorski.rb
+++ class1_hm_quadratic_equation/A_6_Velin_Yavorski.rb
@@ -5,13 +5,17 @@ c = ARGV[2].to_i
 disc = (b*b) - (4*a*c)
 
 if(disc < 0)
-	puts"No real roots!"
+	
 else
 x1 = (-b + Math.sqrt(disc)) / (2*a)
 x2 = (-b - Math.sqrt(disc)) / (2*a)
 
 if (x1 != x2)
-	puts "#{x1},#{x2}" 
+	if(x1 > x2)
+		puts "#{x2},#{x1}" 
+	else
+		puts "#{x1},#{x2}"
+	end
 else 
 	puts x1
 end
