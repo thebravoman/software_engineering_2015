4977d5a4009a1d54e40ae2c25d593a8b4f2b7ccf
HristiyanZahariev <ihzahariev@gmail.com>
2015-09-30 00:06:47 +0300

    Updated quadratic equation

--- class1_hm_quadratic_equation/A_27_Hristiyan_Zahariev.rb
+++ class1_hm_quadratic_equation/A_27_Hristiyan_Zahariev.rb
@@ -2,19 +2,16 @@ a = ARGV[0].to_i
 b = ARGV[1].to_i
 c = ARGV[2].to_i
 
-if a == 0
-	puts "Equation is not quadric"
-else
+if a != 0
 	d = b**2 - 4 * a * c
 
-	if d < 0
-		puts "There is no rational solution"
-	elsif d == 0
-		puts -b/2*a
-	else
+	if d == 0
+		p -b/2*a
+	elsif d > 0
 		x1 = ((-b) + Math.sqrt(d)) / (2*a)
 		x2 = ((-b) - Math.sqrt(d)) / (2*a)
 	
 		print x1,",",x2
 	end
 end
+
