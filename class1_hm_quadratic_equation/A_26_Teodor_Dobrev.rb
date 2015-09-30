797e2a8ba9780dbcfb5b8cf4518065df947b987c
Teodor-Dobrev <teodor_dobrew@abv.bg>
2015-09-30 21:47:10 +0300

    ...Still the same mistake...

--- class1_hm_quadratic_equation/A_26_Teodor_Dobrev.rb
+++ class1_hm_quadratic_equation/A_26_Teodor_Dobrev.rb
@@ -2,10 +2,15 @@
 b = ARGV[1].to_i
 c = ARGV[2].to_i
 
-fRoot1 = nil
-fRoot2 = nil
-
 fRoot1 = (-b + Math.sqrt((b ** 2) - (4 * a * c)) ) / (2 * a)
 fRoot2 = (-b - Math.sqrt((b ** 2) - (4 * a * c)) ) / (2 * a)
 
-printf("%.2f,%.2f", fRoot1, fRoot2)
+if(fRoot1 == fRoot2)
+puts fRoot1
+else
+if(fRoot1 < fRoot2)
+puts("#{fRoot1},#{fRoot2}")
+else
+puts("#{fRoot2},#{fRoot1}")
+end
+end
