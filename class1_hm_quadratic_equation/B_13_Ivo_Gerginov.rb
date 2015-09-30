f2969a8bc4d3b9a2e155fb70295461b25ed2dbeb
Ivo Gerginov <ivo.gerginov9806@gmail.com>
2015-09-30 19:33:45 +0300

    Update B_13_Ivo_Gerginov.rb

--- class1_hm_quadratic_equation/B_13_Ivo_Gerginov.rb
+++ class1_hm_quadratic_equation/B_13_Ivo_Gerginov.rb
@@ -2,9 +2,13 @@ a = ARGV[0].to_i
 b = ARGV[1].to_i
 c = ARGV[2].to_i
 D =  (b*b) - (4*a*c)
+x1 = (-b - Math.sqrt(D))/2*a 
+x2 = (-b + Math.sqrt(D))/2*a
 
 	if D > 0 then
-		puts "#{(-b + Math.sqrt(D))/2*a},#{(-b - Math.sqrt(D))/2*a}"
+		if x1 > x2 then
+		puts "#{x1},#{x2}"
+		else puts "#{x1},#{x2}"
 	elsif D == 0 then
 	puts "#{-b/2*a}"
 	else
