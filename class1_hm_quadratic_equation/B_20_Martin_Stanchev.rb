1c896d6c60f96e8f222fe37f7c183b253e376d40
Your Name <stanchev.martin@gmail.com>
2015-09-30 14:52:17 +0300

    Fixed output

--- class1_hm_quadratic_equation/B_20_Martin_Stanchev.rb
+++ class1_hm_quadratic_equation/B_20_Martin_Stanchev.rb
@@ -13,7 +13,7 @@ if D < 0
 elsif D > 0
   x1 = ((-b) + Math.sqrt(D)) / (2*a)
   x2 = ((-b) - Math.sqrt(D)) / (2*a)
-  puts "#{x1}, #{x2}"
+  puts "#{x1},#{x2}"
 elsif D == 0
   x = (-b) / (2*a)
   puts x
