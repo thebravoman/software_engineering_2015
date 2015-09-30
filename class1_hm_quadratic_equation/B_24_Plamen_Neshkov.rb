bbbac42f1b58484dd425b1e95bb891319791c5e1
Plamen Neshkov <plamen0neshkov@gmail.com>
2015-09-30 18:09:45 +0300

    Fix class 1 homework output

--- class1_hm_quadratic_equation/B_24_Plamen_Neshkov.rb
+++ class1_hm_quadratic_equation/B_24_Plamen_Neshkov.rb
@@ -2,19 +2,23 @@ a = ARGV[0].to_f
 b = ARGV[1].to_f
 c = ARGV[2].to_f
 
-if a == 0
-  raise "Not a quadratic equation."
-end
+# if a == 0
+#   raise "Not a quadratic equation."
+# end
 
 d = b**2 - 4 * a * c
 
 if d > 0
   x1 = (-b + Math.sqrt(d)) / 2 * a
   x2 = (-b - Math.sqrt(d)) / 2 * a
-  puts "#{x1}, #{x2}"
+  if x1 < x2
+    puts "#{x1},#{x2}"
+  else
+    puts "#{x2},#{x1}"
+  end
 elsif d == 0
   x = (-b - Math.sqrt(d)) / 2 * a
   puts x
-else
-  puts "No real roots."
+# else
+#   puts "No real roots."
 end
