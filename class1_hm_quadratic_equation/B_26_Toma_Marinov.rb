65cd6ab3906ac6aa58ea0df14f4f76d17f510ea1
DieEin <savageshadow@abv.bg>
2015-09-30 17:38:06 +0300

    fixed the output

--- class1_hm_quadratic_equation/B_26_Toma_Marinov.rb
+++ class1_hm_quadratic_equation/B_26_Toma_Marinov.rb
@@ -7,20 +7,20 @@ d = 0.0
 
 if a == 0
 	x1 = (-c) / b
-	puts "x1 = x2 = #{x1}"
+	puts "#{x1}"
 elsif b == 0
 	x1 = Math.sqrt(((-c) /a))
-	puts "x1 = x2 = #{x1}"
+	puts "#{x1}"
 else
 	d = b*b - 4*a*c
 	if d < 0
 		puts "There are no solutions"
 	elsif d == 0
 		x1 = (-b) / (2*a)
-		puts "x1 = x2 = #{x1}"
+		puts "#{x1}"
 	else
 		x1 = ((-b) + Math.sqrt(d)) / (2*a)
 		x2 = ((-b) - Math.sqrt(d)) / (2*a)
-		puts "x1 = #{x1}, x2 = #{x2}"
+		puts "#{x1},#{x2}"
 	end
 end
