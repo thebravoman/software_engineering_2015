0af2b399d738528c0752cf8a08107136c40d390b
llJukell <marto_dimitrov@mail.bg>
2015-09-30 21:05:36 +0300

    Fixed

--- class1_hm_quadratic_equation/A_17_Martin_Dimitrov.rb
+++ class1_hm_quadratic_equation/A_17_Martin_Dimitrov.rb
@@ -2,8 +2,9 @@ a = ARGV[0].to_i
 b = ARGV[1].to_i
 c = ARGV[2].to_i #Vuvejdane na stoinostite
 d = ( b * b ) - ( 4 * a * c ) #izchislqvane na D
+
 if ( a == 0)
-puts Not quadratic equation #Ako a e nula ne e kvadratno
+puts "Not quadratic equation" #Ako a e nula ne e kvadratno
 else
 
   if ( d < 0 )
@@ -12,13 +13,11 @@ else
     #Ako nqma realni Koreni
   elsif ( d == 0 )
 
-    puts ( -b )  ( 2  a )
+    puts ( -b )  / ( 2 * a )
     # Pri edin koren
   elsif ( d > 0 )
-
-    puts ((-b) + Math.sqrt(d)) / (2 * a)
-
-    puts ((-b) - Math.sqrt(d)) / (2 * a)
-    # Pri dva korena (nqma nujda ot poveche promenlivi)
+	x1 = ((-b) + Math.sqrt(d)) / (2 * a)
+	x2 = ((-b) - Math.sqrt(d)) / (2 * a)
+	puts "#{x1},#{x2}"
   end
 end
