62aaf9b627f9cf4759ad32c6154d9fa51fdc9cb5
Plamen Neshkov <plamen0neshkov@gmail.com>
2015-10-02 09:21:02 +0300

    Submit zipf homework

---
+++ count_words/B_24_Plamen_Neshkov.rb
@@ -0,0 +1,18 @@
+pattern = /\b[^\s]+\b/i
+
+words = {}
+
+File.open(ARGV[0]).each_line do |line|
+  line.downcase.scan(pattern).each do |word|
+    if words.key?(word)
+      words[word] += 1
+    else
+      words[word] = 1
+    end
+  end
+end
+
+sorted_words = words.sort_by { |word, occur| [-occur, word] }
+sorted_words.each do |word, occur|
+  puts "#{word},#{occur}"
+end
