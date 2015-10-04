6273ed574fdb98d5fd56a9a58be310d679a990a9
Vanessa1998 <vanessa_messi@abv.bg>
2015-10-04 20:40:59 +0300

    Added check_homeworks

--- class2_check_homeworks/make_csv.rb
+++ class2_check_homeworks/make_csv.rb
@@ -2,6 +2,6 @@ Dir.glob(ARGV[0]+"*").each do |file|
 
 	result = file.split('/').last.split('_')
 	p "#{result[0]}, #{result[1]}, #{result[2]}, #{result[3].split('.').first}"
-	`ruby #file 1 3 2`
+	`ruby #file 1 2 1`
 
 end
