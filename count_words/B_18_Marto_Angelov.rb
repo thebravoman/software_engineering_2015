ce7c6db9257d49305ea2b120bb6e61088fb811cc
Georgi Tsitselkov <g.tsitselkov@gmail.com>
2015-10-05 11:59:03 +0300

    Count words program

---
+++ count_words/B_18_Marto_Angelov.rb
@@ -0,0 +1,18 @@
+file = File.open(ARGV[0])
+hash = Hash.new
+
+
+file.each_line do |line|
+	line.split.each do |word|
+		if hash.has_key?(word)
+			hash[word] = hash[word] + 1
+		else
+			hash[word] = 1
+		end
+	end
+end
+
+
+hash.sort {|first,second| second[1] <=> first[1]}.each { |element|
+p "#{element[0]},#{element[1]}"
+} 
