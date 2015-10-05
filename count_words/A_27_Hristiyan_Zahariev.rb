44fcb1283d8972f00c658d3a5451eee6b54defe7
Hristiyan <ihzahariev@gmail.com>
2015-10-05 18:33:17 +0300

    Added task 2

---
+++ count_words/A_27_Hristiyan_Zahariev.rb
@@ -0,0 +1,26 @@
+mode = "r+"
+filepath = ARGV[0]
+filename = File.open(filepath, mode)
+
+wordsnum = Hash.new(0)
+
+
+filename.each_line do |line|
+  w = line.downcase.split(" ") 
+  w.each do |word|
+	word = word.gsub(/[,()'",,",.*!\^^?:]/,'')
+	wordsnum[word] = wordsnum[word] + 1
+  end
+end
+
+wordsnum = wordsnum.sort_by{|word, number| word.downcase}
+wordsnum = wordsnum.sort_by{|word, number| [-number,word]}
+
+wordsnum.each do |word, number|
+  p "#{word},#{number}"
+end
+
+#wordsnum.sort{|a,b| a[1]<=>b[1]}.each { |elem|
+ # puts "#{elem[0]},#{elem[1]}"
+#}
+
