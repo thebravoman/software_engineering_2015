98b98ecc1841674ea8fe9ca4e179e960c5d1ec3b
Ignat Georgiev <ignat.georgiev1@gmail.com>
2015-10-05 13:50:36 +0300

    Counting words in a txt file in ruby

---
+++ count_words/A_14_Ignat_Georgiev.rb
@@ -0,0 +1,15 @@
+file = File.open(ARGV[0])#opens the file
+content = String.new #declaring "content"- string
+count = Hash.new(0)#creates new Hash for counting words
+content = file.read#reading the file into the string
+content=content.downcase#downcasing all the words
+content=content.split(" ")#spliting the string into words
+content.each do |word|#counting the words 
+	count[word] += 1
+end
+count = count.sort_by{|word,num| word.downcase}#sorting alphabeticly
+count = count.sort_by {|word,num| [-num,word]}#sorting by the counter
+count.each do|word, num|
+	
+	puts word+','+num.to_s
+end
