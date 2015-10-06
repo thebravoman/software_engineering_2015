2ef8243b1068bbc83526c394d77cf188fe001d67
Nikolay Danailov <frostblooded@yahoo.com>
2015-10-06 14:01:15 +0300

    fixed my quadratic program

--- class1_hm_quadratic_equation/A_21_Nikolay_Danailov.rb
+++ class1_hm_quadratic_equation/A_21_Nikolay_Danailov.rb
@@ -10,8 +10,8 @@ else
 	if d == 0
 		puts "#{-b/(2*a)}"
 	elsif d > 0
-		x1 = (-b+Math.sqrt(d) / 2*a)
-		x2 = (-b-Math.sqrt(d) / 2*a)
+		x1 = ((-b+Math.sqrt(d)) / 2*a)
+		x2 = ((-b-Math.sqrt(d)) / 2*a)
 		puts "#{[x1, x2].min},#{[x1, x2].max}"
 	end
 end
