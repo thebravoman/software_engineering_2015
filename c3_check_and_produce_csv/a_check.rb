048dd9f74e7e5882cbc82505db707d88474591b8
Kiko <k.mitov@robopartans.com>
2015-10-06 12:47:19 +0300

    Adding files for class 3

---
+++ c3_check_and_produce_csv/a_check.rb
@@ -0,0 +1,33 @@
+require 'csv'
+
+CSV.open("result.csv","w") do |csv|
+	Dir.glob(ARGV[0]+"*").each do |filename|
+		name = filename.split("/").last.split("_")
+		p "#{name[0]},#{name[1]},#{name[2]},#{name[3].split(".").first}"
+		result= `ruby #{filename} 1 3 2`
+		solved = 0
+		if result=="-2.0,-1.0\n"
+			solved = 1
+		end
+		csv << [name[0],name[1],name[2],name[3].split(".").first,solved]
+	end	
+end
+
+my_csv = CSV.read 'result.csv'
+my_csv.sort! { |a,b| a[0] <=> b[0] }
+CSV.open("result.csv", "w") do |csv|
+	my_csv.each {|element| csv << element}
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
