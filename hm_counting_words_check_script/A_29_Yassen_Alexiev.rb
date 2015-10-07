e4253afb3d184d90332f60374807210bee88f466
Yassen Alexiev <yassen4o222@abv.bg>
2015-10-07 18:09:35 +0300

    fix file open

--- hm_counting_words_check_script/A_29_Yassen_Alexiev.rb
+++ hm_counting_words_check_script/A_29_Yassen_Alexiev.rb
@@ -1,26 +1,15 @@
-require 'csv'
+file = ARGV.first
+f = File.open(file, "r")
+hash = Hash.new(0)
 
-CSV.open("result.csv", "w") do |csv_array|
-	Dir.glob(ARGV[0]+"*").each do |filename|
-		name = filename.split("/").last.split("_")
-		p "#{name[0]}, #{name[1]},#{name[2]},#{name[3].split(".").first}"
-		result `ruby #{filename} "Create a program for counting words in a text file. Use method split. The name of the file is passed as first command line argument."`
-	
-		solved = 0
-		if result.strip=="a,2\nfile,2\nthe,2\nargument,1\nas,1\ncommand,1\ncounting,1\ncreate,1\nfirst,1\nfor,1\nin,1\nis,1\nline,1\nmethod,1\nname,1\nof,1\npassed,1\nprogram,1\nsplit,1\ntext,1\nuse,1\nwords,1\n"
-			solved = 1
-		end
-		csv_array << [name[0],name[1],name[2],name[3].split(".").first,solved]
+f.each_line do |line|
+	alignment = line.downcase.split
+	alignment.each do |word|
+		word = word.gsub(/[,()'"„“.*?:]/, '')
+			hash[word] += 1
 	end	
 end
 
-my_csv = CSV.read 'result.csv'
-	my_csv.sort! do |a, b|
-			(a[0] == b[0]) ? (a[1].to_i <=> b[1].to_i) : (a[0] <=> b[0])
-	end
-
-	CSV.open("result.csv","w") do |csv_array|
-		my_csv.each do |row|
-			csv_array << row
-		end
-	end
+hash.sort{|x, y| x <=> y}.sort{|x, y| y[1] <=> x[1]}.each do |word, count|	
+	puts "#{word},#{count}"
+end
