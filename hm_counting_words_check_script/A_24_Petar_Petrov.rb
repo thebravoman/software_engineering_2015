80ba0331470bd922a78b65ca6513c338f74671ed
viking1998 <google1999@abv.bg>
2015-10-07 12:13:07 -0700

    Sorry about committing after deadline just found out my mistake :(

--- hm_counting_words_check_script/A_24_Petar_Petrov.rb
+++ hm_counting_words_check_script/A_24_Petar_Petrov.rb
@@ -7,14 +7,14 @@ end
 CSV.open("result.csv", "w") do |my_csv|
   Dir.glob(ARGV[0]+"*").each do |filename|
     name = filename.split("/").last.split("_")
-    p "#{name[0]},#{name[1]},#{name[2].split(".").first}"#{name[3].split(".").first}"
+    p "#{name[0]},#{name[1]},#{name[2]},#{name[3].split(".").first}"
     result = `ruby #{filename} text.txt`
     solved = 0
     if result.gsub(/\n/,"") == "faith,2in,2just,2light,2the,2bro,1do,1have,1it,1put,1wow,1your,1"
       solved = 1
     end
     print result.gsub(/\n/,"")
-    my_csv << [name[0],name[1],name[2].split(".").first,solved] #name[3].split(".").first,solved]
+    my_csv << [name[0],name[1],name[2],name[3].split(".").first,solved]
   end
 end
 
