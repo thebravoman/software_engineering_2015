242e11476ec9d6d942210ed659ee507ba696f28f
Kiko <k.mitov@robopartans.com>
2015-10-08 12:23:39 +0300

    Adding working with class A on lecture 4

--- c3_check_and_produce_csv/a2_check.rb
+++ c3_check_and_produce_csv/a2_check.rb
@@ -1,16 +1,27 @@
 require 'csv'
 
+checked_files = []
+
 CSV.open("result.csv", "w") do |csv_array|
 	Dir.glob(ARGV[0]+"*").each do |filename|
-		name = filename.split("/").last.split("_")
-		p "#{name[0]},#{name[1]},#{name[2]},#{name[3].split(".").first}"
-		result= `ruby #{filename} 1 3 2`
-		
-		solved = 0
-		if result.strip=="-2.0,-1.0"
-			solved = 1
+			name = filename.split("/").last.split("_")
+			if name.size == 4
+				last_name = name[3].split(".").first
+				first_name = name[2]
+				number = name[1]
+				clazz = name[0]
+				if !checked_files.include? [first_name,last_name]
+					p "#{clazz},#{number},#{first_name},#{last_name}"
+					result= `ruby #{filename} 1 4 2`
+					p result
+					solved = 0
+					if result.gsub(/\s+/, "")=="-3.414213562373095,-0.5857864376269049"
+						solved = 1
+					end
+					csv_array << [clazz,number,first_name,last_name,solved]
+					checked_files << [first_name, last_name]
+				end
 		end
-		csv_array << [name[0],name[1],name[2],name[3].split(".").first,solved]
 	end
 end
 
