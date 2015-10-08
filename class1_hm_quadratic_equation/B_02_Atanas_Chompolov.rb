a8bc08333c9201925cf25c2ac48819c41023f733
Georgi Tsitselkov <g.tsitselkov@gmail.com>
2015-10-08 10:45:36 +0300

    Fixed output

--- class1_hm_quadratic_equation/B_02_Atanas_Chompolov.rb
+++ class1_hm_quadratic_equation/B_02_Atanas_Chompolov.rb
@@ -1,11 +1,8 @@
 def quadraticFormula(a, b, c)
-  fRoot1 = nil
-  fRoot2 = nil
-
   fRoot1 = (-b + Math.sqrt((b ** 2) - (4 * a * c)) ) / (2 * a)
   fRoot2 = (-b - Math.sqrt((b ** 2) - (4 * a * c)) ) / (2 * a)
 
-  printf("%.2f,%.2f\n", fRoot1, fRoot2)
+  printf("%.2f,%.2f\n", fRoot2, fRoot1)
 
 end
 
@@ -13,5 +10,5 @@ a = ARGV[0].to_i
 b = ARGV[1].to_i
 c = ARGV[2].to_i
 
-puts quadraticFormula(a, b, c)
+p quadraticFormula(a, b, c)
 
