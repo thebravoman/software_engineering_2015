d5487a5f2e153e6cd34d67292dcb8a093b72642e
emchoko <emilfrogy@gmail.com>
2015-10-11 19:42:30 +0300

    Marks integration

--- hm_count_words/A_12_Emil_Lozev.rb
+++ hm_count_words/A_12_Emil_Lozev.rb
@@ -2,6 +2,12 @@ filename = ARGV.first
 
 res = open(filename).read.split(' ')
 
+marks_sum = 0
+
+res.each do |word|
+	marks_sum += word.count(". , ! ? : ; -  _ ' \"[ ] ( ) „ “ * / \ ")
+end
+
 res.map!{|item| item.gsub(/\p{^Alnum}/, '') }
 res.map!(&:downcase)
 
@@ -18,3 +24,5 @@ histogram = histogram.sort_by{|word, i| [-i, word]}
 histogram.each do |word, i|
 	puts "#{word},#{i}"
 end
+
+puts "\"marks\",#{marks_sum}" if marks_sum != 0
