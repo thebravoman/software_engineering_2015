e9324702bbe69beb3ed77677be0492c70464b560
EmiliqnGospodinov <emiliqngospodinov@gmail.com>
2015-10-12 20:02:19 +0300

    Editing the program, so it can count punctuation marks

---
+++ hm_count_words/B_12_Emiliqn_Gospodinov.rb
@@ -0,0 +1,24 @@
+	result = Hash.new
+	words = File.read(ARGV[0])
+	marks_counter = 0
+	i = 0
+	words.each_line do |line|
+		wordz = line.downcase.split
+		wordz.each do |word|
+			i = marks_counter
+			marks_counter += word.count(". , ! ? : ;")
+			word = word.gsub(/[,()!.?_"]/,'')
+			next if i < marks_counter
+				if result.has_key?(word)
+					result[word]+=1
+				else
+					result[word] = 1
+				end
+		end
+	end
+	result.sort{|k,v| v[1]<=>k[1]}.each do |elements|
+		puts "#{elements[0]},#{elements[1]}"
+	end
+	if marks_counter!=0
+		puts '"marks",'+"#{marks_counter}"
+	end
