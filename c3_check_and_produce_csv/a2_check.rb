da6fe8a63fb863f42978c3a525c660e50567ffbd
Reaper9806 <ivo.gerginov9806@gmail.com>
2015-10-12 18:47:41 +0300

    Commmiting changes from class

--- c3_check_and_produce_csv/a2_check.rb
+++ c3_check_and_produce_csv/a2_check.rb
@@ -1,31 +1,35 @@
 require 'csv'
 
+namelist = Array.new
+
 CSV.open("result.csv", "w") do |csv_array|
 	Dir.glob(ARGV[0]+"*").each do |filename|
 		name = filename.split("/").last.split("_")
-		p "#{name[0]},#{name[1]},#{name[2]},#{name[3].split(".").first}"
-		result= `ruby #{filename} file.txt`
-		
-		solved = 0
-		if result.strip=="a,1\ncommit,2"
-			solved = 1
-		end
-		csv_array << [name[0],name[1],name[2],name[3].split(".").first,solved]
+	    if name.size == 4
+	    	clazz = name[0]
+	    	number = name[1]
+	    	f_name = name[2]
+	    	l_name = name[3].split(".").first
+	    		if	namelist.include?(f_name + l_name) == false
+						p "#{name[0]},#{name[1]},#{name[2]},#{name[3].split(".").first}"
+						result= `ruby #{filename} 1 3 2`
+					  	solved = 0
+					  	if result.strip=="-2.0,-1.0"
+					  		solved = 1
+					  	end
+					  	csv_array << [name[0],name[1],name[2],name[3].split(".").first,solved]
+
+	  			else namelist = (f_name + l_name)
+	  		end
+	  	end
 	end
 end
 
 my_csv = CSV.read 'result.csv'
-
-my_csv.sort! do |a, b| 
-	(a[0] == b[0]) ? (a[1].to_i <=> b[1].to_i): (a[0] <=> b[0])
+my_csv.sort! { |a,b| [a[0],a[1]] <=> [b[0],b[1]] }
+CSV.open("result.csv", "w") do |csv|
+  my_csv.each {|element| csv << element}
 end
-
-CSV.open("result.csv","w") do |csv_array|
-	my_csv.each do |row|
-		csv_array << row
-	end
-end
-
 
 
 
