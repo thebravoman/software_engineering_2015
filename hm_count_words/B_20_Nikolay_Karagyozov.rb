8675aef7ae26e1f558c1e8e4866c3969185997a2
Nikolay Karagyozov <newmast@abv.bg>
2015-10-12 19:51:03 +0300

    Version 2 of count words program added

--- hm_count_words/B_20_Nikolay_Karagyozov.rb
+++ hm_count_words/B_20_Nikolay_Karagyozov.rb
@@ -1,11 +1,17 @@
 word_list = Hash.new(0)
+marks = []
 
 file = File.new(ARGV[0], "r")
 while line = file.gets
   words = line.downcase.scan(/\w+/)
+  marks.push(line.scan(/[^\w\s]/).size)
+
   words.each { |word| word_list[word] += 1 }
 end
 file.close
 
 sorted_list = word_list.sort_by {|key, val| [-val, key] }.to_h
 sorted_list.each { |key, val| puts "#{key},#{val}" }
+
+marks_count = marks.inject(:+)
+puts "\"marks\",#{marks_count}" if marks_count > 0
