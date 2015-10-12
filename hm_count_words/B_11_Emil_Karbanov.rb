e8a013728bc56c865fa6cecd3ddfd9246d532f69
kingspray <random1hole@gmail.com>
2015-10-12 20:02:06 +0300

    Counting words 2.0i

--- hm_count_words/B_11_Emil_Karbanov.rb
+++ hm_count_words/B_11_Emil_Karbanov.rb
@@ -1,7 +1,10 @@
 the_file = ARGV.first
 h = Hash.new
 f = File.open(the_file, "r")
+comma = 0
+
 f.each_line do |line|
+  marks_sum += line.count("-].\)([,!?:;#@%$^&<>_`~'\"„“*-+/")
   words = line.split
   words.each do |w|
     if h.has_key?(w)
@@ -15,4 +18,6 @@ end
 h.sort{|a,b| b[1]<=>a[1]}.each { |elem|
   puts "#{elem[0]},#{elem[1]}"
 }
-
+if comma!=0
+	puts "\"marks\",#{comma}"
+end
