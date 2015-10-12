d63006d2e4d1d22ca687efe0a62115e06601f76d
Bojidar Zahov <bzahov98@abv.bg>
2015-10-12 20:38:00 +0300

    Update B_04_Bojidar_Zahov.rb

--- hm_count_words/B_04_Bojidar_Zahov.rb
+++ hm_count_words/B_04_Bojidar_Zahov.rb
@@ -1,10 +1,10 @@
-file = "I have a computer and i want, to use it, for programming  // /"
+#file=File.open(ARGV[0], 'r')
+file = "I have a computer and i use it, for programing with Ruby language vv^^!<>!<>!<>!<>!<>!^^vv Result 21"
 list = Hash.new
 
 marks = file.gsub(/\w/,"").split
 
 file.each_line do |line|
-    
 	words = line.downcase.gsub(/\W/,' ').split.each do |word|
 		if list.has_key?(word)
      			list[word] = list[word] + 1
@@ -17,10 +17,10 @@ end
 
 list.sort{ |a,b| a[1] <=> b[1] }.each do |element| 
 	puts "#{element[0]},#{element[1]}"
-
 end
 
 marks_str=marks.join('').length
+
 if (marks!=0)
-     puts "marks,#{marks_str}"
+     print "\"marks\",sum(#{marks_str})\n"
 end
