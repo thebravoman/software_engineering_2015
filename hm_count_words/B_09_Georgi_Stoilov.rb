45c72aba238619db703ed327ccda3241c0ad075a
georgistoilov8 <gogo.stoilov@abv.bg>
2015-10-12 19:32:02 +0300

    Counting words and marks punctuation

--- hm_count_words/B_09_Georgi_Stoilov.rb
+++ hm_count_words/B_09_Georgi_Stoilov.rb
@@ -1,9 +1,11 @@
 word_counter = {}
+marks = 0
 file = File.open(ARGV[0], "r")
 file.each_line do |line|
-  word = line.downcase.split
+  marks = line.scan(/[,.!?()":\[\];]/).count
+  word = line.downcase.split()
   word.each do |word|
-   word = word.gsub(/[,()!.?_"]/,'')
+    word = word.gsub(/[,.!?()":\[\];]/,'') 
     if word_counter.key?(word)
       word_counter[word] += 1
     else
@@ -14,4 +16,7 @@ end
 sorted_array = word_counter.sort_by {|key, value|[-value, key]}
 sorted_array.each do |key,value|
   puts "#{key},#{value}"
+end
+if marks != 0
+  puts "\"marks\",#{marks}"
 end
