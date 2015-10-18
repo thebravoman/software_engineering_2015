b401ee9f671b205ba9914b68c85739c693a941ba
emchoko <emilfrogy@gmail.com>
2015-10-18 17:56:08 +0300

    Made some changes

--- hm_csv_process/A_12_Emil_Lozev.rb
+++ hm_csv_process/A_12_Emil_Lozev.rb
@@ -1,53 +1,46 @@
 require 'csv'
 
 def sort_column in2,in3,arr_table
-	
+	sorted_array = []
 	if in2.to_i == 0 && in3 == 'ASC'
-		arr_table.sort! {|a,b| [a[0].downcase]<=> [b[0].downcase]}
-	elsif (in2.to_i == 1 || in2.to_i == 2) && in3 == 'ASC'
-		arr_table.sort! {|a,b| a[in2.to_i] <=> b[in2.to_i]}
+		sorted_array = arr_table.sort {|a,b| [a[0].downcase] <=> [b[0].downcase]}
+	elsif (in2.to_i == 1 || in2.to_i == 2) && in3 == 'ASC' 
+		sorted_array = arr_table.sort {|a,b| a[in2.to_i].to_i <=> b[in2.to_i].to_i} 
 	end
-
+		
 	if in2.to_i == 0 && in3 == 'DESC'
-		arr_table.sort! {|a,b| b[0].downcase <=> a[0].downcase}
-	elsif (in2.to_i == 1 || in2.to_i == 2) && in3 == 'DESC'
-		arr_table.sort! {|a,b| b[in2.to_i] <=> a[in2.to_i]}
+		sorted_array = arr_table.sort {|a,b| [b[in2.to_i].downcase] <=> [a[in2.to_i].downcase]}
+	elsif (in2.to_i == 1 || in2.to_i == 2) && in3 == 'DESC' 
+		sorted_array = arr_table.sort {|a,b| b[in2.to_i].to_i <=> a[in2.to_i].to_i} 
 	end
 
-	write_csv arr_table
-
-end#END of sort_column
-
-def write_csv arrtable
-
-	CSV.open("result.csv","w") do |csv_result|
-		arrtable.each do |line|
-			csv_result << line
-		end
-	end
-
-end#END of write_csv
+	write_csv sorted_array
+end#sort_column END
 
 def filter_cost in2,in3,arr_table
-	array_cost = []
-
+	cost_array = []
+	
 	arr_table.each do |line|
-		if line[2].to_i >= in2.to_i && line[2].to_i <= in3.to_i
-			array_cost << line
+		cost_array << line if line[2].to_i >= in2.to_i && line[2].to_i <= in3.to_i
+	end
+	cost_array.sort!{|a,b| a[2].to_i <=> b[2].to_i}
+	write_csv cost_array
+end#sort_column END
+
+def write_csv arrtable
+	CSV.open("result.csv","w") do |csv_array|
+		arrtable.each do |line|
+			csv_array << line
 		end
 	end
-
-	write_csv array_cost
-
-end#END of filter_cost
+end
 
 input0 = ARGV[0]
 input1 = ARGV[1]
 input2 = ARGV[2]
 input3 = ARGV[3]
 
-table = []
-table = CSV.read input0
+table = CSV.open input0
 
 if input1 == 'sort'
 	sort_column input2,input3,table
