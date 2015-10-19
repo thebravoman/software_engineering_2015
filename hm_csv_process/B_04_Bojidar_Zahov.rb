73ec7463567991c3662d5248b9c2871282eb6339
Bojidar Zahov <bzahov98@abv.bg>
2015-10-19 21:59:50 +0300

    Update B_04_Bojidar_Zahov.rb

--- hm_csv_process/B_04_Bojidar_Zahov.rb
+++ hm_csv_process/B_04_Bojidar_Zahov.rb
@@ -5,13 +5,13 @@ command = ARGV[1]
 first_option = ARGV[2]
 second_option = ARGV[3]
 def sort_option my_csv, first_option, second_option
-     if first_option == "1" 
+     if first_option == "0" 
         my_csv.sort!{|a,b| [a[0]] <=> [b[0]]}
     end
-    if first_option == "2"
+    if first_option == "1"
         my_csv.sort!{|a,b| [a[1]] <=> [b[1]]}
     end
-    if first_option == "3"
+    if first_option == "2"
         my_csv.sort!{|a,b| [a[2]] <=> [b[2]]}
     end
      if second_option.to_s.downcase == "desc"  
