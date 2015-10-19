7de1d0bad46bd6936af05b56b75c1dff3f78618e
Bojidar Zahov <bzahov98@abv.bg>
2015-10-19 22:25:58 +0300

    Now working, but it is too late

--- hm_csv_process/B_04_Bojidar_Zahov.rb
+++ hm_csv_process/B_04_Bojidar_Zahov.rb
@@ -6,16 +6,16 @@ first_option = ARGV[2]
 second_option = ARGV[3]
 def sort_option my_csv, first_option, second_option
      if first_option == "0" 
-        my_csv.sort!{|a,b| [a[0]] <=> [b[0]]}
+        my_csv.sort!{|a,b| a[0] <=> b[0]}
     end
     if first_option == "1"
-        my_csv.sort!{|a,b| [a[1]] <=> [b[1]]}
+        my_csv.sort!{|a,b| a[1] <=> b[1]}
     end
     if first_option == "2"
-        my_csv.sort!{|a,b| [a[2]] <=> [b[2]]}
+        my_csv.sort!{|a,b| a[2] <=> b[2]}
     end
      if second_option.to_s.downcase == "desc"  
-     	my_csv.reverse
+     	my_csv.reverse!
      end
      out_csv my_csv 	
 end
@@ -28,13 +28,14 @@ end
 def filter_cost my_csv, first_option, second_option
   CSV.open(OUT_FILE, "w") do |csv|
     my_csv.each do |element|          
-	if element[2].to_i >= first_option && element[2].to_i <= second_option
+	if element[2].to_i >= first_option.to_i && element[2].to_i <= second_option.to_i
 	  csv << element                      
 	end
     end
   end  
 end
 
+#main
 my_csv = CSV.read(input)
 	case command.downcase
 	    when "sort"
