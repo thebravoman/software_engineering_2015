8e8ab70c95a73aa7f93fda1966aeb52df5a38540
Kiko <k.mitov@robopartans.com>
2015-10-22 12:11:15 +0300

    Adding files from class A for the 2015-10-22

---
+++ test_Level1_CSV/2015-10-22/a/results/Pavel_Kostadinov_22_098886.rb
@@ -0,0 +1,32 @@
+require 'csv'
+
+arr1 = []
+
+CSV.open("result.csv", "w")  do |csv|
+  Dir.glob(ARGV[0] + "*").each do |filename|
+    name = filename.split("/").last.split("_") 
+      arr1 << [name[1],name[0]]
+   end
+  
+
+  Dir.glob(ARGV[1] + "*").each do |filename|
+    name = filename.split("/").last.split("_") 
+        if  !arr1.include?([[name[1],name[0]])
+            csv << [name[1],name[0]]
+        end
+    
+  end
+end
+
+def sort_result
+    my_csv = CSV.read 'result.csv'
+    my_csv.sort! { |a,b| a[0].downcase <=> b[0].downcase }
+    CSV.open("result.csv", "w") do |csv|
+      my_csv.each {|element| csv << element}
+    end
+end
+
+sort_result
+
+
+
