3647ae639baedc27334a9731dfb0a6253e56fc82
Atanas <a.chompolov@gmail.com>
2015-10-25 01:04:30 +0300

    Fixed 'if root1 == root2' output

--- hm_quadratic_equation/B_02_Atanas_Chompolov.rb
+++ hm_quadratic_equation/B_02_Atanas_Chompolov.rb
@@ -1,12 +1,22 @@
 def quadraticFormula(a, b, c)
- 	fRoot1 = (-b + Math.sqrt((b ** 2) - (4 * a * c)) ) / (2 * a)
- 	fRoot2 = (-b - Math.sqrt((b ** 2) - (4 * a * c)) ) / (2 * a)
-	
-	if fRoot2 > fRoot1
-		return "#{fRoot1},#{fRoot2}"
-	else
-		return "#{fRoot2},#{fRoot1}"
-	end
+    d = (b*b) - (4*a*c)
+    
+    if a == 0
+        return ""
+    elsif d > 0
+	   fRoot1 = ((-b) + Math.sqrt(d)) / (2*a)
+       fRoot2 = ((-b) - Math.sqrt(d)) / (2*a)
+
+       if fRoot1 < fRoot2
+            return "#{fRoot1},#{fRoot2}"
+        else
+            return "#{fRoot2},#{fRoot1}"
+        end
+    elsif d == 0
+        fRoot1 = (-b) / (2*a)
+        fRoot2 = fRoot1
+        return "#{fRoot1}"
+    end
 end
 
 a = ARGV[0].to_i
