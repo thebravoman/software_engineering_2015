3cadf5b6edcc175eb8e929ad8928d021334cea4a
Dani <danione@abv.bg>
2015-10-26 17:42:11 +0200

    Fixed the test problem

--- hm_fix_tests/2015-10-22/b/results/Yordan_Yankulov_15_941253.rb
+++ hm_fix_tests/2015-10-22/b/results/Yordan_Yankulov_15_941253.rb
@@ -1,29 +1,50 @@
 require 'csv'
 
-folder1 = ARGV[0]
-folder2 = ARGV[1]
-my_csv = []
-Dir.glob(folder1 + "*").each do |files|
-	name = files.split("/").last.split("_")
-	if name.size == 4
-		a = name[0].length + name[1].length + name[2].length + name[3].length
-		my_csv << [files,a]
+input_folder_1 = ARGV[0]
+input_folder_2 = ARGV[1]
+def is_digit? (digit)
+  digit =~ /[[:digit:]]/
+end
+help = []
+CSV.open("result.csv", "w") do |csv_array|
+  Dir.glob(input_folder_1 + "*").each do |files|
+    names = files.split("/").last.split("_")
+    if names.size == 3
+      name1 = names[0]
+      name2 = names[1]
+      digit = names[2].split(".").first
+      ext = names[2].split(".").last
+      if (ext != "rb") or (is_digit?(digit) != 0) or (name1 == nil) or (name2 == nil)
+				len = files.length
+        csv_array << [files.split("/").last,len]
+      end
+		elsif
+			len = files.length
+			csv_array << [files.split("/").last,len]
+		end
+end
+	Dir.glob(input_folder_2 + "*").each do |files|
+		names = files.split("/").last.split("_")
+		if names.size == 3
+			name1 = names[0]
+			name2 = names[1]
+			digit = names[2].split(".").first
+			ext = names[2].split(".").last
+			if (ext != "rb") or (is_digit?(digit) != 0) or (name1 == nil) or (name2 == nil)
+				len = files.length
+				csv_array << [files.split("/").last,len]
+			end
+		elsif
+			len = files.length
+			csv_array << [files.split("/").last,len]
+		end
+
 	end
 end
-
-Dir.glob(folder2 + "*").each do |files|
-	name = files.split("/").last.split("_")
-	if name.size == 4
-		a = name[0].length + name[1].length + name[2].length + name[3].length
-		my_csv << [files,a]
-	end
-end
-
-my_csv.sort!{|a,b|
-	(a[4] == b[4]) ? (a[0] <=> b[0]) : (b[0] <=> a[0])
-}
-my_csv.uniq!
-
-CSV.open('result.csv','w') do |csv|
-	my_csv.each{ |element| csv << element}
+my_csv = CSV.read 'result.csv'
+help = my_csv
+help.sort! { |a, b| (a[1] == b[1]) ? (a[0] <=> b[0]) : (b[1] <=> a[1]) }
+help.uniq!
+CSV.open("result.csv","w") do |csv|
+	help.each{|element| csv << element}
 end
