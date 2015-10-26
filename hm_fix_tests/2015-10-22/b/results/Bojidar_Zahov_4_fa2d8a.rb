be65651a2624f16e197c55ce3946a21ad0fc16ac
Bojidar Zahov <bzahov98@abv.bg>
2015-10-26 20:45:50 +0200

    Update Bojidar_Zahov_4_fa2d8a.rb

--- hm_fix_tests/2015-10-22/b/results/Bojidar_Zahov_4_fa2d8a.rb
+++ hm_fix_tests/2015-10-22/b/results/Bojidar_Zahov_4_fa2d8a.rb
@@ -1,40 +1,38 @@
 require 'csv'
 
-folder1 = ARGV[0]
-folder2 = ARGV[1]
+@my_csv = []
 
-my_csv = []
-Dir.glob(folder1 + '*').each do |file|
-     f = file.split("/").last
-     ar = f.split("_")
-     a = f.to_s.gsub(/[^_]/,'')
+def working folder
+    Dir.glob(folder + '*').each do |file|
+          f = file.split("/").last
+          a = f.to_s.gsub(/[^_]/,'')
    
-     if !(a == "__") 
+          if !(a == "__") 
      	     	       
-         	n_size = f.size
-           	my_csv << [f,n_size]
-    
-     end
-end
-Dir.glob(folder2 + '*').each do |file|
-     f = file.split("/").last
-     ar = f.split("_")
-     a = f.to_s.gsub(/[^_]/,'')
-   
-     if !(a == "__") 
-     	     	       
-         	n_size = f.size
-           	my_csv << [f,n_size]
+         	 	n_size = f.size
+           		@my_csv << [f,n_size]
      
+          end
+     end 
+end
+
+def open_csv 
+     CSV.open('result.csv', 'w') do |csv|   
+          @my_csv.each { |line| csv << line } 
+          end
+end
+
+def sort_csv
+     @my_csv.sort! do |a, b| 
+     	(a[1] == b[1]) ? (a[0] <=> b[0]): (b[0] <=> a[0])
      end
+	@my_csv.uniq!
 end
 
-my_csv.sort! do |a, b| 
-	(a[1] == b[1]) ? (a[0] <=> b[0]): (b[0] <=> a[0])
-end
-my_csv.uniq!
+working ARGV[0]
 
+working ARGV[1]
 
-CSV.open('result.csv', 'w') do |csv|
-  my_csv.each { |line| csv << line } 
-end
+sort_csv
+
+open_csv
