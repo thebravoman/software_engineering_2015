67faf49d2f32ed0d91e1181e8abf7e4ea7338115
Konstantin Todorov <konstantin.todorov98@Gmail.com>
2015-10-26 21:25:14 +0200

    Made program to return 1. (Hopefully :D)
    ayy lmao

--- hm_fix_tests/2015-10-22/b/results/Konstantin_Todorov_16_521116.rb
+++ hm_fix_tests/2015-10-22/b/results/Konstantin_Todorov_16_521116.rb
@@ -1,23 +1,36 @@
 require 'csv'
+def student_checked? checked_files, first_name, surname
+    checked_files.include? [first_name,surname]
+end
+def name_getter filename
+    filename.split("/").last.split("_")
+end
+def remove_file_ext value
+    value.split(".").first
+end
+checked_files = []
+CSV.open("result.csv", "w") do |csv_array|
+    previous_number = 0
+    Dir.glob(ARGV[0]+"*").each do |filename|
+        name = name_getter filename
+        number = remove_file_ext name[2]
+        surname = name[1]
+        first_name = name[0]
+        if !student_checked? checked_files,first_name,surname
+            if surname.size == 5
+                csv_array << [first_name,surname]
+                checked_files << [first_name, surname]
+            end
+        end
 
-path = ARGV[0]
-
-Dir.glob("path").each do |file|
-	result = file.split("_").first+1
-	name_digits = result.split("")
-	if name_digits == 5
-		CSV.open("result.csv", "wb") do |csv|
-			csv << file
-		end			
-	end
+    end
 end
 my_csv = CSV.read 'result.csv'
+my_csv.sort! { |a,b| b[0] <=> a[0] }
+CSV.open("result.csv", "w") do |csv|
+    my_csv.each {|element| csv << element}
+end
 
-my_csv.sort! {|a ,b| a[0].to_i <=> b[0].to_i}.reverse!
-
-CSV.open("result.csv", "wb") do |csv|
-	csv << my_csv
-end			
 
 
 	
