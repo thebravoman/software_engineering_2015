ca316dbb8144f23bf2ed060cc1b65be438259051
SWAGSLAYER <pepi2401_98@abv.bg>
2015-10-26 20:07:56 +0200

    1 FREAKING SIMPLE MISTAKE GODDAMMIT
    2 times in a row a stupid mistake
    one more time and i`m gonna kill myself

--- hm_fix_tests/2015-10-22/b/results/Petar_Georgiev_23_c7d5c4.rb
+++ hm_fix_tests/2015-10-22/b/results/Petar_Georgiev_23_c7d5c4.rb
@@ -8,26 +8,18 @@ def valid_names? names
 	names.size == 3
 end
 
-def remove_file_ext value
-	value.split(".").first
-end
-
-checked_files = []
 folder = ARGV[0]
 folder1 = ARGV[1]
 
-CSV.open("result.csv", "w") do |my_csv|
+CSV.open("result.csv", "w") do |csv_array|
 	Dir.glob(folder+"*").each do |filename|
 			name = get_names filename
 			if valid_names? name 
-				number = remove_file_ext name[2]
 				last_name = name[1]
 				first_name = name[0]
-					p "#{first_name},#{last_name},#{number}"
 					Dir.glob(folder1+"*").each do |filename1|
 						name1 = get_names filename1
 						if valid_names? name1
-							number1 = remove_file_ext name1[2]
 							last_name1 = name1[1]
 							first_name1 = name1[0]
 							if first_name.size == 5 && first_name == first_name1 && last_name == last_name1
