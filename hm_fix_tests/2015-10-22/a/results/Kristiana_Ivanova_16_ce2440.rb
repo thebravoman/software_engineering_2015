baff1eb53bcc64678eb99b3240113125bc4b150f
kriji98 <kriji98@abv.bg>
2015-10-27 13:09:03 +0200

    Update Kristiana_Ivanova_16_ce2440.rb

--- hm_fix_tests/2015-10-22/a/results/Kristiana_Ivanova_16_ce2440.rb
+++ hm_fix_tests/2015-10-22/a/results/Kristiana_Ivanova_16_ce2440.rb
@@ -7,7 +7,8 @@ CSV.open("result.csv", "w") do |csv|
   Dir.glob(ARGV[0]+"*").each do |filename|
     name = filename.split("/").last
     test= name.size.split("_")
-    if name.size !=3 
+    if name.size !=3
+    lenght = name.size
       csv << [name, name.lenght]
     end
   end
@@ -25,9 +26,10 @@ CSV.open("result.csv", "w") do |csv|
  end
 my_csv = CSV.read 'result.csv'
 
+my_csv.uniq
 my_csv.sort! {|a, b| a[1] <=> b[1]}
-my_csv.uniq!
 
+my_csv.sort_by{|name,length| name.downcase}
 CSV.open("result.csv", "w") do |csv_array|
 	my_csv.each do |row|
 		csv_array << row 
