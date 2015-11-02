c5f4ddf1b60a327e976269e0130c43ce972e59ea
Valchovski <bvalchovski@abv.bg>
2015-11-02 18:27:01 +0200

    Refactoring count_words

--- hm_count_words/B_03_Bojidar_Valchovski.rb
+++ hm_count_words/B_03_Bojidar_Valchovski.rb
@@ -1,8 +1,13 @@
 #!/usr/bin/ruby
 
+require 'json'
+require 'rexml/document'
+require 'csv'
+
 path = ARGV[0]
-words = {} 
-marks = {}
+format = ARGV[1]
+words = Hash.new
+marks = Hash.new
 file = File.open(path, "r")
 
 wordslist = file.read.downcase
@@ -20,8 +25,33 @@ end
 words = words.sort_by{|word,occ| word.downcase}	
 words = words.sort_by{|word,occ| [-occ,word]}	 
 
-words.each do |word,occ|	
-  puts word + ',' + occ.to_s
-end
+if format == "csv"
+  words.each do |word,occ|	
+    puts word + ',' + occ.to_s
+  end
+  
+  if marks != 0
+    puts "\"marks\",#{marks}"
+  end
 
-puts "\"marks\",#{marks}" 	
+elsif format == "json"
+  b = Hash[words.map {|key,value| [key,value]}]
+  puts JSON.pretty_generate({marks: marks, words: [b]})
+
+elsif format == "xml"
+  doc = REXML::Document.new('')
+  
+  word_counts = doc.add_element('word-counts')
+  marks = word_counts.add_element('marks').text = "#{marks}"
+  wordz = word_counts.add_element('words')
+
+  words.each do |index, key|
+    word = wordz.add_element('word', 'count' => key).text = "#{index}"
+  end
+
+  formatter = REXML::Formatters::Pretty.new
+  formatter.compact = true
+  formatter.write(doc, $stdout)
+  
+  puts
+end
