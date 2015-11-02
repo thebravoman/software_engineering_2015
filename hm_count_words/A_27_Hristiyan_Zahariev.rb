4b24c9981bad73a1b910fbf6bf8ef1d3990f07e7
HristiyanZahariev <ihzahariev@gmail.com>
2015-11-02 16:50:55 +0200

    Now it can work with json and xml as output

--- hm_count_words/A_27_Hristiyan_Zahariev.rb
+++ hm_count_words/A_27_Hristiyan_Zahariev.rb
@@ -1,6 +1,11 @@
+require 'json'
+require 'rexml/document'
+
 mode = "r+"
 filepath = ARGV[0]
 filename = File.open(filepath, mode)
+
+json_xml = ARGV[1]
 
 wordsnum = Hash.new(0)
 markz_count = 0
@@ -25,3 +30,26 @@ if markz_count != 0
    puts '"marks"'+','+markz_count.to_s
 end
 
+if json_xml == "json"
+  tempHash = {
+    "marks" => markz_count,
+    "words" => wordsnum
+  }
+  puts JSON.pretty_generate(tempHash)
+end
+
+if json_xml == "xml"
+  xml_counts = REXML::Document.new("")
+  xml_word_counts = xml_counts.add_element("word-counts")
+  xml_marks = xml_word_counts.add_element("marks")
+  xml_marks.add_text "#{markz_count}"
+  xml_words = xml_word_counts.add_element("words")
+  wordsnum.each do |index, key|
+    word = xml_words.add_element('word')
+    word.add_attribute("count", 'key.to_s')
+    word.add_text "#{index}"
+  end
+  xmlprint = ""
+  xml_counts.write(xmlprint, 4)
+  puts xmlprint
+end
