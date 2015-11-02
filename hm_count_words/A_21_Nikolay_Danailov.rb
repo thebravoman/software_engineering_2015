0f4b3370643799f39aa9fcd70114b060b86fdf08
frostblooded <frostblooded@yahoo.com>
2015-11-02 18:53:53 +0200

    refactored code

--- hm_count_words/A_21_Nikolay_Danailov.rb
+++ hm_count_words/A_21_Nikolay_Danailov.rb
@@ -5,32 +5,29 @@ require 'rexml/document'
 @input_file = ARGV[0]
 @output_format = ARGV[1]
 
-def output_csv counted_words, marks_count
-  counted_words.each { |word, count| puts word + "," + count.to_s }
+def output_csv(counted_words, marks_count)
+  counted_words.each { |word, count| puts word + ',' + count.to_s }
   puts "\"marks\"," + marks_count.to_s
 end
 
-def output_json counted_words, marks_count
-  json_output = { :marks => marks_count, :words => counted_words }
+def output_json(counted_words, marks_count)
+  json_output = { marks: marks_count, words: counted_words }
   puts JSON.pretty_generate(json_output)
 end
 
-def output_xml counted_words, marks_count
+def output_xml(counted_words, marks_count)
   document = REXML::Document.new
   word_counts = document.add_element 'word-counts'
-  marks = word_counts.add_element 'marks'
-  marks.add_text "#{marks_count}"
+  word_counts.add_element('marks').add_text("#{marks_count}")
   words = word_counts.add_element 'words'
 
   counted_words.each do |word, count|
-    word_element = words.add_element 'word'
-    word_element.add_attribute 'count', count
-    word_element.add_text word
+    words.add_element('word').add_attribute('count', count).add_text(word)
   end
 
-  out = ""
+  output = ''
   document.write(out, 1)
-  puts out
+  puts output
 end
 
 File.open(@input_file) do |file|
@@ -50,9 +47,9 @@ File.open(@input_file) do |file|
 
   counted_words = counted_words.sort_by { |word, count| [-count, word] }
 
-  if @output_format == "json"
+  if @output_format == 'json'
     output_json counted_words, marks_count
-  elsif @output_format == "xml"
+  elsif @output_format == 'xml'
     output_xml counted_words, marks_count
   else
     output_csv counted_words, marks_count
