81145ff3b2c6d0c589dedfdcf24fb880d944ff4b
frostblooded <frostblooded@yahoo.com>
2015-11-02 18:05:08 +0200

    made csv work

--- hm_count_words/A_21_Nikolay_Danailov.rb
+++ hm_count_words/A_21_Nikolay_Danailov.rb
@@ -1,13 +1,29 @@
-input_file = ARGV[0]
+require 'csv'
 
-File.open(input_file) do |file|
+@input_file = ARGV[0]
+@output_format = ARGV[1]
+
+def output_csv word_counts, marks_count
+  word_counts.each { |word, count| puts word + "," + count.to_s }
+  puts "\"marks\"," + marks_count.to_s
+end
+
+def output_json word_counts, marks_count
+  
+end
+
+def output_xml word_counts, marks_count
+
+end
+
+File.open(@input_file) do |file|
   text = ''
 
   file.each_line do |line|
     text += line
   end
 
-  marks = text.scan(/[,.!?:;"()\[\]]/).count
+  marks_count = text.scan(/[,.!?:;"()\[\]]/).count
   words = text.downcase.gsub(/[^a-z'\s-]/, '').split(' ')
   word_counts = Hash.new(0)
 
@@ -17,9 +33,11 @@ File.open(input_file) do |file|
 
   word_counts = word_counts.sort_by { |word, count| [-count, word] }
 
-  word_counts.each do |word, count|
-    puts "#{word},#{count}"
+  if @output_format == "json"
+    output_json word_counts, marks_count
+  elsif @output_format == "xml"
+    output_xml word_counts, marks_count
+  else
+    output_csv word_counts, marks_count
   end
-
-  puts "\"marks\",#{marks}" if marks > 0
 end
