c8d7fb22ae9862312c56a8c6b486cc03a9a8f5ff
Ignat Georgiev <ignat.georgiev1@gmail.com>
2015-11-06 20:54:46 +0200

    Adding parse(string) and parse_file(filename) , but broken xml :(

--- hm_count_words/A_14_Ignat_Georgiev.rb
+++ hm_count_words/A_14_Ignat_Georgiev.rb
@@ -25,39 +25,48 @@ class Result
     json_hash2= JSON.pretty_generate(json_hash)		
   end
   def to_xml
-   xml_new = REXML::Document.new('')
-    word_counts = xml_new.add_element('word-counts')
-    xml_mrks =word_counts.add_element('marks')
-    xml_mrks.add_text "#{marks_count}"
-    xml_words = word_counts.add_element('words')
-    word_counts.each do |word, count|
-      word = xml_words.add_element('word', 'count' => count).text = "#{word}"
-    end
-    formatter = REXML::Formatters::Pretty.new
-    formatter.compact = true
+  xml_new = REXML::Document.new('')
+  
+  word_counts = xml_new.add_element('word-counts')
+  xml_mrks =word_counts.add_element('marks')
+  xml_mrks.add_text "#{marks_count}"
+  xml_words = word_counts.add_element('words')
+
+  word_counts.each do |word, count|
+    word = xml_words.add_element('word', 'count' => count).text = "#{word}"
+  end
+
+  formatter = REXML::Formatters::Pretty.new
+  formatter.compact = true
    xml_new
   end
 
 end
-#class WordCounter
-# def parse_file(filename)
-#file = File.open(filname)
-# end
-#end
-file = File.open(ARGV[0])#opens the file
-command = ARGV[1]
-result = Result.new
-content = String.new #declaring "content"- string
-content = file.read#reading the file into the string
-content=content.downcase#downcasing all the words
-content=content.split(" ")#spliting the string into words
-content.each do |word|#counting the words 
-  result.marks_count += word.count(". , ! ? : ; -  _ ' \"[ ] ( ) „ “ * / \ ")#counting the marks in every word
-  word =word.gsub(/[,()'"„“.*!?:;]/, '')
-  result.word_counts[word] += 1
+class WordCounter
+  def parse(string)
+    result = Result.new
+    string = string.downcase.split(" ")
+    string.each do |word|
+      result.marks_count += word.count(". , ! ? : ; -  _ ' \"[ ] ( ) „ “ * /  ")
+      word = word.gsub(/[,()'"„“.*!?:;< >]/, '')
+      result.word_counts[word]+=1
+    end
+    result.word_counts = result.word_counts.sort_by{|word,num| word.downcase}#sorting alphabetically
+    result.word_counts = result.word_counts.sort_by {|word,num| [-num,word]}#sorting by the counter
+    result
+    end
+  def parse_file(filename)
+    file = File.open(filename)
+    string = ''
+    file.each_line do |line|
+      string += line
+    end
+   parse string
+  end
 end
-result.word_counts = result.word_counts.sort_by{|word,num| word.downcase}#sorting alphabetically
-result.word_counts = result.word_counts.sort_by {|word,num| [-num,word]}#sorting by the counter
+command = ARGV[1]
+word_counter = WordCounter.new
+result = word_counter.parse_file ARGV[0]
 if command == "json"
  puts result.to_json
 elsif command == "xml"
