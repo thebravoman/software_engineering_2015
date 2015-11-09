c2de05210ce21b55069b2985191a62e2e63d14bc
Dani <danione@abv.bg>
2015-11-09 20:31:46 +0200

    The new homework

--- hm_count_words/B_15_Yordan_Yankulov.rb
+++ hm_count_words/B_15_Yordan_Yankulov.rb
@@ -1,50 +1,82 @@
 require 'json'
 require 'rexml/document'
 
-txt_file = File.open(ARGV[0],"r")
-command = ARGV[1]
-help = Hash.new
-punctuation = 0
-txt_file.each_line{ |line|
-	the_words = line.downcase.split
-	the_words.each { |words|
-	punctuation += words.count(".,!?:;-_'\"[]()„“*/\ ")
-	words = words.gsub(/[,()!.?_"]/,'')
-	if help.has_key?(words)
-		help[words] = help[words] + 1
-	else
-		help[words] = 1
+file_name = ARGV[0]
+format_of_output = ARGV[1]
+
+class WordCounter
+
+	def parse(string)
+      help = Hash.new
+      punctuation = 0
+      string.each_line{ |line|
+      the_words = line.downcase.split
+      the_words.each { |words|
+        punctuation += words.count(".,!?:;-_'\"[]()„“*/\ ")
+        words = words.gsub(/[,()!.?_"]/,'')
+        if help.has_key?(words)
+          help[words] = help[words] + 1
+        else
+          help[words] = 1
+        end
+      }
+      my_help = help.sort {|first,second| (second[1] == first[1]) ? (first[0] <=> second[0]) : (second[1]<=>first[1])}
+      return Result.new(punctuation, my_help)
+    }
 	end
-	}
-}
-if command == 'json'
-	help = help.sort {|first,second| (second[1] == first[1]) ? (first[0] <=> second[0]) : (second[1]<=>first[1])}
-	if punctuation != 0
-		puts JSON.pretty_generate("marks" => punctuation,"words" => help)
-	else
-		puts JSON.pretty_generate("words" => help)
+	def parse_file(filename)
+    result = File.open(filename,"r")
+    result = result.read
+    parse result
+	end
+end
+
+class Result
+
+  def initialize punct, helpe
+    @punctuation = punct
+    @help = helpe
+  end
+	def to_csv
+    @help.each{ |element|
+  	puts "#{element[0]},#{element[1]}"
+  	}
+  	if @punctuation!=0
+  		puts "\"marks\", #{@punctuation}"
+  	end
 	end
 
-elsif command == 'xml'
-		help = help.sort {|first,second| (second[1] == first[1]) ? (first[0] <=> second[0]) : (second[1]<=>first[1])}
+	def to_json
+  	if @punctuation != 0
+  		puts JSON.pretty_generate("marks" => @punctuation,"words" => @help)
+  	else
+  		puts JSON.pretty_generate("words" => @help)
+  	end
+	end
+
+	def to_xml
 		my_xml = REXML::Document.new('')
 		word_counts = my_xml.add_element('word-counts')
-		if punctuation != 0
-			word_counts.add_element('marks').add_text"#{punctuation}"
+		if @punctuation != 0
+			word_counts.add_element('marks').add_text"#{@punctuation}"
 		end
 		words = word_counts.add_element('words')
-		help.each do |key,count|
+		@help.each do |key,count|
 		word = words.add_element('word', {'count' => "#{count}"}).add_text"#{key}"
 	end
 	formatter = REXML::Formatters::Pretty.new
 	formatter.compact = true
 	formatter.write(my_xml, $stdout)
-
-else
-	help = help.sort! {|first,second| (second[1] == first[1]) ? (first[0] <=> second[0]) : (second[1]<=>first[1])}.each { |element|
-	puts "#{element[0]},#{element[1]}"
-	}
-	if punctuation!=0
-		puts "\"marks\", #{punctuation}"
 	end
+end
+
+word_counter = WordCounter.new
+result = word_counter.parse_file file_name
+
+if format_of_output == 'json'
+  puts result.to_json
+elsif format_of_output == 'xml'
+  result.to_xml
+else
+  puts result.to_csv
 end
