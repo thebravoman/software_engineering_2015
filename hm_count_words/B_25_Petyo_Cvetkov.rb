45c31dbd18cba8bbfa35a998a610d1f13626c0eb
petiocvetkov <petiopetio@mail.bg>
2015-11-09 20:57:20 +0200

    count words with class

--- hm_count_words/B_25_Petyo_Cvetkov.rb
+++ hm_count_words/B_25_Petyo_Cvetkov.rb
@@ -1,71 +1,80 @@
-class CountWords
-  def initialize #openFile
-    #@text = 0#openFile
-    @words_list = Hash.new(0)
-    @marks = 0
-  end
-      def countWords
-        fileName = File.open(ARGV[0] , "r")
-        text = fileName.read.downcase
+class WordCounter
+  private
 
+  def parse(string)
+    result = Result.new
+    result.marks_count = string.scan(/[,.!?:;"()\[\]]/).count
+    words = string.downcase.gsub(/[^a-z'\s-]/, '').split(' ')
 
-        @marks = text.scan(/[,.!?:;"()\[\]]/).count
-
-        text = text.gsub(/[^a-z'\s-]/, '').split(" ")
-        text.each do |word|
-        #  if words_list.has_key?(word)
-            @words_list[word]+=1
-        #  else
-        #    words_list[word] = 1
-          end
-        @words_list = @words_list.sort_by {|word, count| [-count, word]}
+    words.each do |word|
+      result.word_counts[word] += 1
     end
+
+    result.word_counts = result.word_counts.sort_by { |word, _count| word }
+    result
+  end
+
+  public
+
+  def parse_file(filename)
+    text = ''
+
+    File.open(filename) do |file|
+      file.each_line do |line|
+        text += line
+      end
+    end
+
+    parse text
+  end
 end
 
-class WriteLike < CountWords
-  def likeCsv
-    @words_list.each do |word,i|
-  		puts "#{word},#{i}"
-  	end
-  	puts  "\"marks\",#{@marks}" if @marks != 0
-  end
 
-  def likeJson
+class Result
+
+  attr_accessor :marks_count
+  attr_accessor :word_counts
+  def initialize
+    @marks_count = 0
+    @word_counts = Hash.new 0
+  end
+  def to_csv
+    word_counts.each do |word,i|
+      puts "#{word},#{i}"
+    end
+    puts  "\"marks\",#{marks_count}" if marks_count != 0
+  end
+  def to_json
     require 'json'
-    my_json = {  :words => @words_list,:marks =>"#{@marks}".to_i  }
-    puts JSON.pretty_generate(my_json)
+    json_output = { marks: marks_count, words: word_counts }
+    JSON.pretty_generate(json_output)
   end
-
-  def likeXml
+  def to_xml
     require 'rexml/document'
     xml_counts = REXML::Document.new("")
     xml_word_counts = xml_counts.add_element('word-counts')
     xml_marks = xml_word_counts.add_element('marks')
-    xml_marks.add_text "#{@marks}"
+    xml_marks.add_text "#{marks_count}"
     xml_words = xml_word_counts.add_element('words')
-    @words_list.each do |index, key|
+    word_counts.each do |index, key|
       word = xml_words.add_element('word')
       word.add_attribute( 'count', key)
       word.add_text "#{index}"
     end
     out = ""
     xml_counts.write(out, 1)
-    puts out
+    return out
   end
 end
+file_name = ARGV[0]
+format = ARGV[1]
+word_counter = WordCounter.new
+word_counter = word_counter.parse_file file_name
 
-
-class Casse < WriteLike
-
-  def casse
-    case ARGV[1]
-    when "csv" then likeCsv
-    when "json" then likeJson
-    when "xml" then likeXml
-    else likeCsv
-    end
-  end
+if format == "json"
+  puts word_counter.to_json
+elsif format == "xml"
+  puts word_counter.to_xml
+else
+  puts word_counter.to_csv
 end
-clazz = Casse.new
-clazz.countWords
-clazz.casse
