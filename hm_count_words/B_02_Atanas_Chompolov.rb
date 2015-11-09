399c320d43087e94828176ef2c0f1bddb4f12675
Atanas <a.chompolov@gmail.com>
2015-11-09 21:12:55 +0200

    Json & Xml fixed

--- hm_count_words/B_02_Atanas_Chompolov.rb
+++ hm_count_words/B_02_Atanas_Chompolov.rb
@@ -1,25 +1,65 @@
-file = File.open(ARGV[0], "r")
-count = Hash.new(0)
-marks = 0
 
-file.each_line do |line|
-	marks += line.scan(/[,.!?:;"()\[\]]/).count
-	w = line.downcase.gsub(/[^a-z'\n- ]/, ' ').split(" ")
-    
-	w.each do |word|
-		count[word] += 1
-	end	
+require 'csv'
+require 'json'
+require 'rexml/document'
+
+@input_file = ARGV[0]
+@output_format = ARGV[1]
+
+def output_csv(counted_words, marks_count)
+  counted_words.each { |word, count| puts word + ',' + count.to_s }
+  puts "\"marks\"," + marks_count.to_s
 end
 
-
-count = count.sort_by{|word,num| word.downcase}
-count = count.sort_by{|word,num| [-num,word]}
-
-count.each do |word, num|
-	puts word + ',' + num.to_s
+def output_json(counted_words, marks_count)
+  json_output = { marks: marks_count, words: counted_words }
+  puts JSON.pretty_generate(json_output)
 end
 
-if marks != 0
-	puts '"marks",' + marks.to_s
+def add_words_to_xml(xml_doc, counted_words)
+  words = xml_doc.elements['word-counts/words']
+
+  counted_words.each do |word, count|
+    word_element = words.add_element 'word'
+    word_element.add_attribute 'count', count
+    word_element.add_text "#{word}"
+  end
 end
 
+def output_xml(counted_words, marks_count)
+  document = REXML::Document.new
+  word_counts = document.add_element 'word-counts'
+  word_counts.add_element('marks').add_text "#{marks_count}"
+  word_counts.add_element 'words'
+  add_words_to_xml document, counted_words
+
+  formatter = REXML::Formatters::Pretty.new(2)
+  formatter.compact = true # makes words show up on one line
+  formatter.write(document, $stdout)
+end
+
+File.open(@input_file) do |file|
+  text = ''
+
+  file.each_line do |line|
+    text += line
+  end
+
+  marks_count = text.scan(/[,.!?:;"()\[\]]/).count
+  words = text.downcase.gsub(/[^a-z'\s-]/, '').split(' ')
+  counted_words = Hash.new(0)
+
+  words.each do |word|
+    counted_words[word] += 1
+  end
+
+  counted_words = counted_words.sort_by { |word, count| [-count, word] }
+
+  if @output_format == 'json'
+    output_json counted_words, marks_count
+  elsif @output_format == 'xml'
+    output_xml counted_words, marks_count
+  else
+    output_csv counted_words, marks_count
+  end
+end
