9af216718490f060c20df73611ed031b2281ccca
DieEin <savageshadow@abv.bg>
2015-11-09 20:19:32 +0300

    finally working

--- hm_count_words/B_28_Toma_Marinov.rb
+++ hm_count_words/B_28_Toma_Marinov.rb
@@ -5,57 +5,31 @@ require 'rexml/document'
 file_name = ARGV[0]
 format = ARGV[1]
 
-class WordCounter
-	def parse_file file
-		result = File.open(file, 'r')
-		result = result.read
+class Result
 
-		parse result
-	end
-
-	def parse string
-		result = Result.new
-		
-		result.count += string.scan(/[[:punct:]]/).count
-		words = string.downcase.scan(/\w+/)
-
-		words.each do |word|
-			result.hash[word] += 1
-		end
-
-		new_hash = Hash.new()
-		new_hash = result.hash.sort_by { |key, value| [-value, key] }
-
-		return result
-	end
-end
-
-class Result < WordCounter
-	attr_accessor :count, :hash
-
-	def initialize
-		@count = 0.to_i
-		@hash = Hash.new()
+	def initialize counter, the_hash
+		@count = counter
+		@hash = the_hash
 	end
 
 	def to_csv
-		CSV.open('result.csv', 'w') do |file|
-			hash.to_a.each do |elem|
-				csv << elem
-			end
+    	CSV.open('result.csv', 'w') do |file|
+    		@hash.to_a.each do |elem|
+    			file << elem
+    		end
 
-			unless count == 0
-				csv << ["\"marks\",", count]
-			end
-		end
-	end
+    		unless @count == 0
+    			file << ["\"marks\"", @count]
+    		end
+    	end
+  	end
 
 	def to_json
 		tempHash = Hash.new()
 
 		tempHash = {
-			"marks" => count,
-			"words" => new_hash
+			"marks" => @count,
+			"words" => @hash
 		}
 
 		json_hash = JSON.pretty_generate(tempHash)
@@ -69,10 +43,10 @@ class Result < WordCounter
 		xml_file = REXML::Document.new()
 		xml_word_counts = xml_file.add_element('word-counts')
 		xml_marks = xml_word_counts.add_element('marks')
-		xml_marks.add_text "#{count}"
+		xml_marks.add_text "#{@count}"
 		xml_words = xml_word_counts.add_element('words')
 	
-		new_hash.each do |key, value|
+		@hash.each do |key, value|
 			word = xml_words.add_element('word')
 			word.add_attribute('count', value)
 			word.add_text "#{key}"
@@ -84,6 +58,34 @@ class Result < WordCounter
 		File.open('result.xml', 'w') do |file|
 			file << xml_with_tabs
 		end
+	end
+end
+
+class WordCounter
+	def parse_file file
+		result = File.open(file, 'r')
+		result = result.read
+
+		parse result
+	end
+
+	def parse string
+		hash = Hash.new(0)
+		count = 0.to_i
+
+		string.each_line do |line|
+			count += string.scan(/[[:punct:]]/).count
+			words = string.downcase.scan(/\w+/)
+
+			words.each do |word|
+				hash[word] += 1
+			end
+		end
+
+		new_hash = Hash.new(0)
+		new_hash = hash.sort_by { |key, value| [-value, key] }
+
+		return Result.new(count, new_hash)
 	end
 end
 
