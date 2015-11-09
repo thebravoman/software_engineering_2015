08c1ca5b8bf16b1c37d16c62df3e7571779141da
SWAGSLAYER <pepi2401_98@abv.bg>
2015-11-09 22:01:09 +0200

    Cant find the problem srry

--- hm_count_words/B_23_Petar_Georgiev.rb
+++ hm_count_words/B_23_Petar_Georgiev.rb
@@ -1,50 +1,96 @@
-require 'csv'
 require 'rexml/document'
 require 'json'
 
-count_words = Hash.new(0)
-file = File.new(ARGV[0], "r")
-marks = 0
-option_type = ARGV[1]
-#GSUB FAILED
-while (line = file.gets)
-  line.each_char { |character|
-    if character.match(/[[:punct:]]/)
-      marks = marks + 1
-    end
-  }
-  text = line.split(/\W+/)
-  text.each { |word|
-    word = word.downcase
-    if count_words.has_key?("#{word}")
-      count_words[word] += 1
-    else
-      count_words[word] = 1
-    end
-  }
-end
-count_words = count_words.sort_by {|word, count| [-count, word]}
+@file_name = ARGV[0]
+@option_type = ARGV[1]
 
-if option_type == "xml"
-  file = REXML::Document.new('')
-  word_counts = file.add_element('word-counts')
-  xml_marks = word_counts.add_element('marks').text = "#{marks}"
-  xml_words = word_counts.add_element('words')
-  count_words.each do |a, b|
-    word = xml_words.add_element('word', 'count' => b).text = "#{a}"
-  end
-  formatter = REXML::Formatters::Pretty.new
-  formatter.compact = true
-  formatter.write(file, $stdout)
-  puts
-elsif option_type == "json"
-  json = { :marks => "#{marks}".to_i, :words => count_words, }
-  puts JSON.pretty_generate(json)
+Class WordCounter
+    private
+    
+    def parse(string)
+        result = result.new
+        result.marks_count = string.scan(/[,.!?:;"()\[\]]/).count
+        file_text  = string.downcase.gsub(/[^a-z'\s-]/, '').split(' ')
+        file_text.each do |word|
+            word = word.downcase
+            if result.word_counts.has_key?("#{word}")
+                result.word_counts[word] += 1
+            else
+                result.word_counts[word] = 1
+            end
+        end
+        result.word_counts = result.word_counts.sort_by {|word, count| [-count, word]}
+        result
+    end
+    
+    public
+    def parse_file(file_name)
+        text = ''
+        File.open(file_name) do |file|
+            file.each_line do |line|
+                text += line
+            end
+        end
+        
+        parse text
+    end
+end
+Class Result
+    private
+    
+    def xml_add(file)
+        words = file.elements["word-count/words"]
+        word_counts.each do |a, b|
+            word = words.add_element 'word'
+            word.add_attribute 'count' , b
+            word_element.add_text "#{a}"
+        end
+    end
+    
+    public
+    attr_accessor :marks_count
+    attr_accessor :word_counts
+    
+    def initialize
+        @marks_count = 0
+        @word_counts = Hash.new 0
+    end
+    
+    def to_xml
+        file = REXML::Document.new('')
+        word_counts = file.add_element('word-counts')
+        xml_marks = word_counts.add_element('marks').add_text = "#{marks}"
+        xml_words = word_counts.add_element('words')
+        count_words.each do |a, b|
+            word = xml_words.add_element('word', 'count' => b).text = "#{a}"
+        end
+        xml_add file
+        file
+    end
+    def to_json
+        json = { :marks => "#{marks}".to_i, :words => count_words, }
+        JSON.pretty_generate(json)
+    end
+    def to_csv
+        csv_file = []
+        
+        word_counts.each do |a, b|
+            csv_file << a + ',' + b.to_s
+        end
+        if marks_count != 0
+            csv_file << "\"marks\"," + marks_count.to_s
+        end
+    end
+end
+words = WordCounter.new
+result = words.parse_file @file_name
+ 
+if @option_type == 'xml'
+    formatter = REXML::Formatters::Pretty.new
+    formatter.compact = true
+    formatter.write(result.to_xml, $stdout)
+elsif @option_type == 'json'
+    puts result.to_json
 else
-  count_words.each do |word,count|	
-    puts word + ',' + count.to_s
-  end
-  if marks != 0
-    puts "\"marks\",#{marks}"
-  end
+    puts result.to_csv
 end
