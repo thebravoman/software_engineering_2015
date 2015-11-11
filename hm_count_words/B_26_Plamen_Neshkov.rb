c86f6db9583a74f533fd59a1d5ca985762495d59
Plamen Neshkov <plamen0neshkov@gmail.com>
2015-11-11 11:30:44 +0200

    Instantiate classes

--- hm_count_words/B_26_Plamen_Neshkov.rb
+++ hm_count_words/B_26_Plamen_Neshkov.rb
@@ -70,3 +70,16 @@ class Result
     document.to_s
   end
 end
+
+filepath = ARGV[0]
+format = ARGV[1]
+
+word_counter = WordCounter.new
+result = word_counter.parse_file(filepath)
+if format == 'csv'
+  File.open('result.csv', 'w') { |file| file << result.to_csv }
+elsif format == 'json'
+  File.open('result.json', 'w') { |file| file << result.to_json }
+elsif format == 'xml'
+  File.open('result.xml', 'w') { |file| file << result.to_xml }
+end
