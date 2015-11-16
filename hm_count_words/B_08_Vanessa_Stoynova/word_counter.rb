265f647302c0e8bb82abed7e66872d1eb4370567
Nessi98 <vanesa.nessi98@gmail.com>
2015-11-16 21:05:57 +0200

    changed some mistakes

--- hm_count_words/B_08_Vanessa_Stoynova/word_counter.rb
+++ hm_count_words/B_08_Vanessa_Stoynova/word_counter.rb
@@ -10,13 +10,13 @@ class WordCounter
 			word = word.gsub(/[,.!?()":\[\];]/,'')
 			result.word_counter[word] += 1
 		end
-		result.word_counter[word] = result.word[counter].sort_by { |key, value| [-value, key] }
+		result.word_counter = result.word_counter.sort_by { |key, value| [-value, key] }
 		result 
 	end 
 	
 	def parse_file(file)
 		files = File.open(file, 'r')
-		what_is_in_the_file = file.read
+		what_is_in_the_file = files.read
 		parse_string what_is_in_the_file
 	end
 end
