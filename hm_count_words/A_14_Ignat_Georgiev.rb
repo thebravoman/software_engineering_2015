fa5dd30a51b4e02ac338bbe01f6a584c5308f517
Ignat Georgiev <ignat.georgiev1@gmail.com>
2015-11-16 17:09:47 +0200

    Now working with .rb files!

--- hm_count_words/A_14_Ignat_Georgiev.rb
+++ hm_count_words/A_14_Ignat_Georgiev.rb
@@ -49,15 +49,15 @@ end
 class WordCounter
   def parse(string)
     result = Result.new
-    string = string.downcase.split(" ") 
- 
-    string.each do |word|
+    result.marks_count = string.count(". , ! ? : ; -  _ ' \"[ ] ( ) „ “ * /  ")
+    string = string.gsub(/['"].+['"]|[^A-Za-z_]/, ' ').downcase
+    word_list = string.split(' ')
+    word_list.each do |word|
       result.marks_count += word.count(". , ! ? : ; -  _ ' \"[ ] ( ) „ “ * /  ")
-      word = word.gsub(/[,()'"„“.*!?:;< >]/, '')
       result.word_counts[word]+=1
     end
-    result.word_counts = result.word_counts.sort_by{|word,num| word.downcase}#sorting alphabetically
-    result.word_counts = result.word_counts.sort_by {|word,num| [-num,word]}#sorting by the counter
+    result.word_counts = result.word_counts.sort_by{|word,num| word.downcase}
+    result.word_counts = result.word_counts.sort_by {|word,num| [-num,word]}
     result
     end
   def parse_file(filename)
