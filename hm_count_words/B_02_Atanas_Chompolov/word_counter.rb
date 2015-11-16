52ff47a7610c0b5fb5d1ef8de78ecc72c43ee411
Atanas Chompolov <a.chompolov@gmail.com>
2015-11-16 17:35:46 +0200

    Uploading fixed word_counter.rb

--- hm_count_words/B_02_Atanas_Chompolov/word_counter.rb
+++ hm_count_words/B_02_Atanas_Chompolov/word_counter.rb
@@ -1,24 +1,24 @@
-+class WordCounter
-+
-+	def parse(contents)
-+		marks = contents.gsub(/[a-z\s]/, "");
-+		words = contents.gsub(/[^a-z'\s-]/, "").split
-+
-+		hash = Hash.new(0)
-+
-+		words.each do |word|
-+			hash[word] += 1
-+		end
-+
-+		hash = hash.sort_by{|word, count| [-count, word]}
-+
-+		return Result.new(marks.length, hash)
-+	end
-+
-+	def parse_file(filename)
-+		contents = ""
-+		contents = File.open(filename, "r").read.downcase
-+		return parse contents    	
-+	end
-+end
+class WordCounter
+
+	def parse(contents)
+		marks = contents.gsub(/[a-z\s]/, "");
+		words = contents.gsub(/[^a-z'\s-]/, "").split
+
+		hash = Hash.new(0)
+
+		words.each do |word|
+			hash[word] += 1
+		end
+
+		hash = hash.sort_by{|word, count| [-count, word]}
+
+		return Result.new(marks.length, hash)
+	end
+
+	def parse_file(filename)
+		contents = ""
+		contents = File.open(filename, "r").read.downcase
+		return parse contents    	
+	end
+end
 
