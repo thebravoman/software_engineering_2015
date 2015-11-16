34e3bb6b957762504eb2e1200d359bfe54257d08
DieEin <savageshadow@abv.bg>
2015-11-16 21:13:48 +0300

    folder commit

---
+++ hm_count_words/B_28_Toma_Marinov/word-counter.rb
@@ -0,0 +1,27 @@
+class WordCounter
+	def parse_file file
+		result = File.open(file, 'r')
+		result = result.read
+
+		parse result
+	end
+
+	def parse string
+		hash = Hash.new(0)
+		count = 0.to_i
+
+		string.each_line do |line|
+			count += string.scan(/[[:punct:]]/).count
+			words = string.downcase.scan(/\w+/)
+
+			words.each do |word|
+				hash[word] += 1
+			end
+		end
+
+		new_hash = Hash.new(0)
+		new_hash = hash.sort_by { |key, value| [-value, key] }
+
+		return Result.new(count, new_hash)
+	end
+end
