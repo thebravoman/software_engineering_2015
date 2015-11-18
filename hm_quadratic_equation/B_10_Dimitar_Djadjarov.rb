e3f63b779bf9ea8f3bb10b4e3ff4846c0ba29016
Euphoria4Men <dimitar_djadjarov@abv.bg>
2015-11-18 16:12:11 +0200

    -_-

--- hm_quadratic_equation/B_10_Dimitar_Djadjarov.rb
+++ hm_quadratic_equation/B_10_Dimitar_Djadjarov.rb
@@ -5,8 +5,8 @@ d = b**2 - 4 * a * c
   if (d == 0)
 	  puts -b/2*a
   elsif d > 0
-  	x1 = (-b - Math.sqrt((b ** 2) - (4 * a * c)) ) / (2 * a)
-	  x2 = (-b + Math.sqrt((b ** 2) - (4 * a * c)) ) / (2 * a)
+  	x1 = (-b + Math.sqrt(d)) / (2 * a)	
+	x2 = (-b - Math.sqrt(d)) / (2 * a)
   end 	
   if x2 > x1 
  	  puts "#{x1},#{x2}" 
