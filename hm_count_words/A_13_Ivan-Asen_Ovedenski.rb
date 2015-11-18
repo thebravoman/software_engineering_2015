f7108a3da678ca60d2e747cb1e256fdaf2e115a6
4o4oTo <4o4olino70@gmail.com>
2015-11-18 11:23:52 -0800

    Word_counter checking a2check.rb (i think reedy , not sure)

--- hm_count_words/A_13_Ivan-Asen_Ovedenski.rb
+++ hm_count_words/A_13_Ivan-Asen_Ovedenski.rb
@@ -1,73 +1,8 @@
 require 'json'
 require 'rexml/document'
+require './A_13_Ivan-Asen_Ovedenski/word_counter.rb'
+require './A_13_Ivan-Asen_Ovedenski/results.rb'
 
-class Result
-	def initialize(marksCount,wordCount)
-		@marks_count = marksCount
-		@word_count = wordCount
-	end
-	
-	def get_csv
-		@word_count.each { |word, count|  
-		puts word + "," + count.to_s
-			}
-			if @marks_count > 0 
-				puts "\"marks\", #{@marks_count}"
-			end
-	end
-	
-	def get_json
-		puts @word_count
-		json = {
-			"marks" => @marks_count,
-			"words" => @word_count.to_a
-		}
-		puts JSON.pretty_generate(json) 
-	end
-	
-	def get_xml
-		my_xml = REXML::Document.new
-		body = my_xml.add_element('word-counts') 
-		body.add_element('marks').add_text @marks_count.to_s
-		words = body.add_element('words') 
-	
-		@word_count.each do |word, count| 
-			words.add_element('word', {'count' => count}).add_text word 
-		end 
-	
-		printer = REXML::Formatters::Pretty.new(4) 
-		printer.compact = true 
-		printer.write(my_xml, $stdout)	
-	end
-end
-
-class WordCounter 
-
-	def parse_file(file)
-		text = File.open(file, "r")
-		parse(text)
-	end
-
-	def parse(text)
-		marks = 0
-		h = Hash.new(0)
-		text.each_line { |line|
-  			marks += line.scan(/[,.!?:;"()\[\]]/).count 
-			words = line.gsub(/[^a-z'^A-Z'\s-]/, '').split
-			words.each { |w|
-				w.downcase!
-				if h.has_key?(w) 
-					h[w] = h[w] + 1 
-				else
-					h[w] = 1 
-				end	
-			}
-			}
-			h = h.sort_by{|word, count| [-count, word]}
-		
-		Result.new(marks,h)
-	end
-end
 
 result = (WordCounter.new).parse_file(ARGV[0])
 #result.parse_file(ARGV[0])
