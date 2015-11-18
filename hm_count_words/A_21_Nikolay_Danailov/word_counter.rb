c7f3533b09734fd8c8bf4b337cd5f0c7ced1a4e2
frostblooded <frostblooded@yahoo.com>
2015-11-18 21:17:09 +0200

    made numbers part of words

--- hm_count_words/A_21_Nikolay_Danailov/word_counter.rb
+++ hm_count_words/A_21_Nikolay_Danailov/word_counter.rb
@@ -5,9 +5,9 @@ class WordCounter
   def parse(string)
     result = Result.new
     # the regex on the next line select all marks
-    result.marks_count = string.downcase.scan(/[^a-z_0-9 ]/).count
+    result.marks_count = string.downcase.scan(/[^a-z0-9_ ]/).count
     # the regex on the next line removes commented text, strings, regex and any other symbol that isn't a word
-    removal_regex = %r{[^a-z_ ]}
+    removal_regex = %r{[^a-z0-9_ ]}
     words = string.downcase.gsub(removal_regex, ' ').split(' ').reject(&:empty?)
 
     words.each do |word|
