7ed69ac13e899ec67359c2d5555d0664943db5ad
Euphoria4Men <dimitar_djadjarov@abv.bg>
2015-11-18 15:22:55 +0200

    FIXING THE NIGHTMARE
    abdulah azis

--- hm_quadratic_equation/B_10_Dimitar_Djadjarov.rb
+++ hm_quadratic_equation/B_10_Dimitar_Djadjarov.rb
@@ -1,18 +1,17 @@
-a = ARGV[0].to_i()
-b = ARGV[1].to_i()
-c = ARGV[2].to_i()
-d = (b*b) - (4*a*c)
-  if d<0 
-    puts "Nqma takova"
-  elsif d==0;
-    x= -b / (2 * a);
-    puts "#{x}";
-  else d!=0
-    x1= -b + Math . sqrt(d)/( 2*a)
-    x2= -b - Math . sqrt(d)/(2*a)
-  if x1<x2
-    puts "#{x1},#{x2}"
-  else
-    puts "#{x2},#{x1}"       
-end
-end
+a = ARGV[0].to_i
+b = ARGV[1].to_i
+c = ARGV[2].to_i
+d = b**2 - 4 * a * c
+  if (d == 0)
+	  puts -b/2*a
+  elsif d < 0
+  	puts "Nqma takova!"
+  else d > 0
+  	x1 = (-b - Math.sqrt((b ** 2) - (4 * a * c)) ) / (2 * a)
+	  x2 = (-b + Math.sqrt((b ** 2) - (4 * a * c)) ) / (2 * a)
+  end 	
+  if x2 > x1 
+ 	  puts "#{x1},#{x2}" 
+  else 
+ 	  puts "#{x2},#{x1}" 
+  end
