37e7d6cf5e1359c8c2c2773ee5e2b6f4a957a78c
Aleksandar Ivanov <aivanov92@gmail.com>
2015-11-20 01:39:55 +0200

    Describe the c10 homework

--- c10/fetch_website.rb
+++ c10/fetch_website.rb
@@ -5,11 +5,14 @@ require 'openssl'
 uri  = URI.parse(ARGV.first)
 
 http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
 
-# This is bad..
-# http://stackoverflow.com/questions/9199660/why-is-ruby-unable-to-verify-an-ssl-certificate#answer-9238221
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+if uri.scheme == 'https'
+  http.use_ssl = true
+
+  # This is bad..
+  # http://stackoverflow.com/questions/9199660/why-is-ruby-unable-to-verify-an-ssl-certificate#answer-9238221
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+end
 
 result = http.get(uri.request_uri)
 
