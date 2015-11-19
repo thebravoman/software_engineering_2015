7162c2d45e1b3504c241ed56a3ffb259e4b61559
Aleksandar Ivanov <aivanov92@gmail.com>
2015-11-19 12:32:44 +0200

    Ignore ssl certs in fetch_website :(

--- c10/fetch_website.rb
+++ c10/fetch_website.rb
@@ -1,10 +1,18 @@
 require 'net/http'
 require 'sanitize'
+require 'openssl'
 
-url  = URI.parse(ARGV.first)
+uri  = URI.parse(ARGV.first)
 
-html = Net::HTTP.get(url)
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
 
-text = Sanitize.fragment(html)
+# This is bad..
+# http://stackoverflow.com/questions/9199660/why-is-ruby-unable-to-verify-an-ssl-certificate#answer-9238221
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+result = http.get(uri.request_uri)
+
+text = Sanitize.fragment(result.body)
 
 puts text
