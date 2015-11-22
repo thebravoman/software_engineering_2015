66b384d14c046fc11abc27b4f19afb0df69feba6
velioo <velioocs@mail.bg>
2015-11-22 14:29:34 +0200

    Refactored code

--- hm_count_words/A_5_Velin_Yavorski/word_counter.rb
+++ hm_count_words/A_5_Velin_Yavorski/word_counter.rb
@@ -14,28 +14,4 @@ module WordCounter
     def self.parse_webpage(url)   
 			WebsiteParser.parse(url)
 		end
-end
-=begin
-    class Result
-			def marks_count
-      	@marks_count
-			end
-			
-			def word_counts 
-        @word_counts
-			end    
-
-      def to_csv   
-     		to_csv
-			end
-
-			def to_json    
-				to_json
-			end      
- 
-    	def to_xml 
-				to_xml
-			end
-		end
-end    
-=end      
+end     
