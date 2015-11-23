ad673973ee13840429ad7547443f48f91eb82199
HristiyanZahariev <ihzahariev@gmail.com>
2015-11-23 18:23:55 +0200

    TopKek Mates

--- hm_count_words/A_27_Hristiyan_Zahariev.rb
+++ hm_count_words/A_27_Hristiyan_Zahariev.rb
@@ -1,19 +1,49 @@
+require_relative 'A_27_Hristiyan_Zahariev/word_counter.rb'
 require 'csv'
 require 'json'
 require 'rexml/document'
-require './A_27_Hristiyan_Zahariev/result.rb'
-require './A_27_Hristiyan_Zahariev/word_counter.rb'
 
 input_filename = ARGV[0]
 output_option = ARGV[1]
 
-words_counter = WordCounter.new
-result = words_counter.parse_file ARGV[0]
+def checkFileOrSite(filename)
+  protocol = filename.split('/').first
+  return protocol == 'http:' || protocol == "https:"
+end
 
-if output_option == "json"
+def result(input_filename,output_option)
+  if File.file? input_filename
+    result = WordCounter.parse_file input_filename
+  elsif checkFileOrSite input_filename
+    result = WordCounter.parse_webpage input_filename
+  else
+    result = Parser.parse input_filename
+  end
+
+  if output_option == 'csv'
+    result.to_csv
+  elsif output_option == 'json'
+    result.to_json
+  elsif output_option == 'xml'
+    result.to_xml
+  else
+    result.to_csv
+  end
+  
+end 
+
+result(input_filename,output_option)
+
+=begin
+result = WordCounter.parse_website ARGV[0]
+## result = WordCounter.parse_file ARGV[0]
+
+if @output_option == "json"
   result.to_json
-elsif output_option == "xml"
+elsif @output_option == "xml"
   result.to_xml
 else
   result.to_csv
 end
+=end
+
