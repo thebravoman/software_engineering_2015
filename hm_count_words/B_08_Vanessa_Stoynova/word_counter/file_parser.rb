3c3ff422b88a95190dec514e4b2f079aec5a7bc5
Nessi98 <vanesa.nessi98@gmail.com>
2015-11-23 22:29:02 +0200

    remove a mistake

--- hm_count_words/B_08_Vanessa_Stoynova/word_counter/file_parser.rb
+++ hm_count_words/B_08_Vanessa_Stoynova/word_counter/file_parser.rb
@@ -5,8 +5,8 @@ module WordCounter
 	class FileParser
 	
 		def parse_file (file)
-			file = File.open(file, 'r')
-			file_contents = file.read
+			files = File.open(file, 'r')
+			file_contents = files.read
 			Parser.new.parse_string file_contents
 		end
 	end
