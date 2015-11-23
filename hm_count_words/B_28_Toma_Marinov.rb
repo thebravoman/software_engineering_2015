f44074f82137621df431548b06acdc83e96a910d
DieEin <savageshadow@abv.bg>
2015-11-23 19:28:44 +0300

    fixes and refactoring

--- hm_count_words/B_28_Toma_Marinov.rb
+++ hm_count_words/B_28_Toma_Marinov.rb
@@ -3,8 +3,12 @@ require './B_28_Toma_Marinov/word_counter/result'
 file_name = ARGV[0]
 format = ARGV[1]
 
-result = WordCounter::parse_file(file_name)
-#result = word_counter.parse_file file_name
+if file_name.match(%r{\Ahttps://})
+	result = WordCounter::parse_web(file_name)
+else
+	result = WordCounter::parse_file(file_name)
+	#result = word_counter.parse_file file_name
+end
 
 case format
 when 'json'
