7275d95c24124d9675140d48b152506addd3c1a6
Radostin Angelov <angelovradostin@gmail.com>
2015-11-23 21:54:20 +0200

    removed code repetition through random function

--- hm_count_words/B_27_Radostin_Angelov/word_counter.rb
+++ hm_count_words/B_27_Radostin_Angelov/word_counter.rb
@@ -4,34 +4,12 @@ module WordCounter
 
   def self.parse_file(path, file_format)
     text = FileParser.new.parse(path)
-    result = Result.new
-    result.word_counts(text)
-    result.marks_count(text)
-
-    case file_format
-      when "json"
-        result.to_json
-      when "xml"
-        result.to_xml
-      else
-        result.to_csv
-    end
+    WordCounter.log_result(text, file_format)
   end
 
   def self.parse_webpage(url, file_format)
     text = WebpageParser.new.parse(url)
-    result = Result.new
-    result.word_counts(text)
-    result.marks_count(text)
-
-    case file_format
-      when "json"
-        result.to_json
-      when "xml"
-        result.to_xml
-      else
-        result.to_csv
-    end
+    WordCounter.log_result(text, file_format)
   end
 
   def parse
@@ -39,6 +17,10 @@ module WordCounter
 
   def self.parse_string(string, file_format)
     text = Parser.new.parse(string)
+    WordCounter.log_result(text, file_format)
+  end
+
+  def self.log_result (text, file_format)
     result = Result.new
     result.word_counts(text)
     result.marks_count(text)
