9128cf65a6a40fc37281d72ab642e9343bf9364f
Radostin Angelov <angelovradostin@gmail.com>
2015-11-23 21:49:44 +0200

    introduced code repetition

--- hm_count_words/B_27_Radostin_Angelov.rb
+++ hm_count_words/B_27_Radostin_Angelov.rb
@@ -1,29 +1,14 @@
 module WordCounter
-  require './B_27_Radostin_Angelov/word_counter.rb'
-  require './B_27_Radostin_Angelov/result.rb'
+  require 'word_counter.rb'
 
   input_to_parse = ARGV[0]
   file_format = ARGV[1]
 
-  result = Result.new
-
   text = ""
 
   if input_to_parse.start_with? "http://" or input_to_parse.start_with? "https://"
-     text = WordCounter.parse_webpage(input_to_parse)
+    WordCounter.parse_webpage(input_to_parse, file_format)
   elsif
-    text = WordCounter.parse_file(input_to_parse)
-  end
-
-  result.word_counts(text)
-  result.marks_count(text)
-
-  case file_format
-    when "json"
-      result.to_json
-    when "xml"
-      result.to_xml
-    else
-      result.to_csv
+    WordCounter.parse_file(input_to_parse, file_format)
   end
 end
--- hm_count_words/B_27_Radostin_Angelov/word_counter.rb
+++ hm_count_words/B_27_Radostin_Angelov/word_counter.rb
@@ -1,19 +1,55 @@
 module WordCounter
   require_relative "word_counter/parser.rb"
+  require_relative 'word_counter/result.rb'
 
-  def self.parse_file(path)
-    return FileParser.new.parse(path)
+  def self.parse_file(path, file_format)
+    text = FileParser.new.parse(path)
+    result = Result.new
+    result.word_counts(text)
+    result.marks_count(text)
+
+    case file_format
+      when "json"
+        result.to_json
+      when "xml"
+        result.to_xml
+      else
+        result.to_csv
+    end
   end
 
-  def self.parse_webpage(url)
-    return WebpageParser.new.parse(url)
+  def self.parse_webpage(url, file_format)
+    text = WebpageParser.new.parse(url)
+    result = Result.new
+    result.word_counts(text)
+    result.marks_count(text)
+
+    case file_format
+      when "json"
+        result.to_json
+      when "xml"
+        result.to_xml
+      else
+        result.to_csv
+    end
   end
 
   def parse
   end
 
-  def self.parse_string(string)
-    string = Parser.new.parse(string)
-    return string
+  def self.parse_string(string, file_format)
+    text = Parser.new.parse(string)
+    result = Result.new
+    result.word_counts(text)
+    result.marks_count(text)
+
+    case file_format
+      when "json"
+        result.to_json
+      when "xml"
+        result.to_xml
+      else
+        result.to_csv
+    end
   end
 end
