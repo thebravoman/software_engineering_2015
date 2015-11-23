914e32e1d5fdd674468a3b67233eb9d86019945f
Martin_Galabov <martucha1@me.com>
2015-11-23 12:19:25 -0800

    More refactoring and HTML now!! V3 or is it V4? Aah to hell with it!

--- hm_count_words/B_21_Martin_Galabov.rb
+++ hm_count_words/B_21_Martin_Galabov.rb
@@ -2,12 +2,14 @@ require 'rexml/document'
 require 'csv'
 require 'rubygems'
 require 'json'
-require './B_19_Martin_Galabov/word_counter' 
+require './B_21_Martin_Galabov/word_counter' 
 file_path = ARGV[0]
 command = ARGV[1]
-
-word_counter_class = WordCounter.new
-result = word_counter_class.parse_file(file_path)
+if (file_path.start_with?('http://') || file_path.start_with?('https://'))
+	result = WordCounter.web_parse
+else
+	result = WordCounter.parse_file file_path
+end
 
 if command == 'json' 
 result.to_json
