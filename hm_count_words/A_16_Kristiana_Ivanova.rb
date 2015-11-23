65e77f81bbd511cda2c09fd0d1ca43667caa2d1b
Kristiana <you@example.com>
2015-11-23 18:44:49 +0200

    Homework

--- hm_count_words/A_16_Kristiana_Ivanova.rb
+++ hm_count_words/A_16_Kristiana_Ivanova.rb
@@ -1,15 +1,32 @@
-require 'rexml/document'
-require 'json'
-require './A_16_Kristiana_Ivanova/result'
-require './A_16_Kristiana_Ivanova/word_counter'
+#require 'rexml/document'
+#require 'json'
+#require './A_16_Kristiana_Ivanova/result'
+#require './A_16_Kristiana_Ivanova/word_counter'
+require_relative './A_16_Kristiana_Ivanova/word_counter'
+require 'builder'
 
-word_counter = WordCounter.new
-result = word_counter.parse_file ARGV[0]
-
-if ARGV[1] == "json"
- result.to_json
-elsif ARGV[1] == "xml"
- result.to_xml
-else
- result.to_csv
+def uri?(string)
+	start = string.split('/').first
+  	start == 'http:' || start == 'https:'
 end
+
+def get_result(filename)
+  if File.file? filename
+    WordCounter.parse_file filename
+  elsif uri? filename
+    WordCounter.parse_webpage filename
+  else
+    WordCounter.parse filename
+  end
+end
+
+if format == 'json'
+ puts result.to_json
+elsif format == 'xml'
+ puts result.to_xml
+else
+ puts result.to_csv
+end
+
+filename = ARGV[0]
+format = ARGV[1]
