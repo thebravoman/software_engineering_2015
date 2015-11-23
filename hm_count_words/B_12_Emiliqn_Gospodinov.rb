141cbdde33ed152cf16bb491a5b60340fa307fe2
EmiliqnGospodinov <emiliqngospodinov@gmail.com>
2015-11-23 17:30:09 +0200

    Update B_12_Emiliqn_Gospodinov.rb

--- hm_count_words/B_12_Emiliqn_Gospodinov.rb
+++ hm_count_words/B_12_Emiliqn_Gospodinov.rb
@@ -1,5 +1,5 @@
-require '/home/decisioner/Desktop/software_engineering_2015/hm_count_words/B_12_Emiliqn_Gospodinov/Result.rb' #Here must be the whole path
-require '/home/decisioner/Desktop/software_engineering_2015/hm_count_words/B_12_Emiliqn_Gospodinov/WordCounter.rb'
+require './B_12_Emiliqn_Gospodinov/Result'
+require './B_12_Emiliqn_Gospodinov/WordCounter'
 format = ARGV[1]
 
 word_counter = WordCounter.new
