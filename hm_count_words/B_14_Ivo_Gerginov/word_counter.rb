0d6381c3506040e4380caa86f0db40272e6439e3
Reaper9806 <ivo.gerginov9806@gmail.com>
2015-11-23 20:47:04 +0200

    Adding web parser

--- hm_count_words/B_14_Ivo_Gerginov/word_counter.rb
+++ hm_count_words/B_14_Ivo_Gerginov/word_counter.rb
@@ -1,5 +1,6 @@
 require './B_14_Ivo_Gerginov/word_counter/parser'
 require './B_14_Ivo_Gerginov/word_counter/fileParser'
+require './B_14_Ivo_Gerginov/word_counter/webParser'
 
 module WordCounter
     def self.parseStr f_str 
@@ -8,5 +9,8 @@ module WordCounter
 
     def self.parseFile f_path
       FileParser.new.parseFile(f_path)
+    end
+    def self.parseURL url
+      WebParser.new.parseURL(url)
     end
 end
