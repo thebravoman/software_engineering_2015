bfdf310217a5482e7b000f34569f00060e90c9f5
Yassen Alexiev <yassen4o222@abv.bg>
2015-11-23 15:54:43 +0200

    make homework, maybe true

--- hm_count_words/A_29_Yassen_Alexiev/word_counter.rb
+++ hm_count_words/A_29_Yassen_Alexiev/word_counter.rb
@@ -1,33 +1,16 @@
-class WordCounter
-	def parse_file(inputfile) 
-		result = [];
-		@inputfile = inputfile
-		f = File.open(@inputfile, "r")
-		hash = Hash.new(0)
+require 'word_counter/file_parser'
+require 'word_counter/web_parser'
 
-		marks_count = 0
+module WordCounter
+  def parse(string)
+    Parser.parse string
+  end
 
-		f.each_line do |line|
-			marks_count = marks_count + line.scan(/[-\].)(\[,!?:;%@#$^&<_>`~'"*-+\/]/).count
+  def parse_file(filename)
+    FileParser.parse filename
+  end
 
-			alignment = line.downcase.split
-			alignment.each do |word|
-				word = word.gsub(/[^a-z'\s-]/, '')
-					hash[word] += 1
-			end	
-		end
-		
-		hash = hash.sort_by {|word, count| [-count, word]}
-	
-		hash.each do |word, count|	
-			result << [word,count.to_s]
-		end		
-
-		output_result = Result.new(result, marks_count)
-		return output_result
-	end	
-
-	def marks_counter
-		@marks_count
-	end
+  def parse_webpage(url)
+    WebpageParser.parse url
+  end
 end
---
+++ hm_count_words/A_29_Yassen_Alexiev/word_counter/result.rb
@@ -0,0 +1,54 @@
+require 'json'
+require 'rexml/document'
+
+module WordCounter
+	class Result
+		def initialize(result, marks_count)
+			@result = result
+			@marks_count = marks_count
+		end
+	
+		def marks_count
+			@marks_count
+		end
+	
+		def word_counts
+			@result
+		end
+	
+		def to_csv
+			var = "" 
+			@result.each do |word, count|	
+				var = var + "#{word},#{count.to_s}\n"
+			end
+			var
+		end		
+		
+		def to_json
+			json_format = { :marks => "#{marks_count}", :words => @result.to_a }
+			puts JSON.pretty_generate(json_format)
+		end
+		
+		def to_xml
+			format_xml_files = REXML::Document.new("")
+		
+			format_xml_word_counts = format_xml_files.add_element('word-counts')
+		  
+			format_xml_marks = format_xml_word_counts.add_element('marks')
+			format_xml_marks.add_text "#{marks_count}"
+		  
+			format_xml_words = format_xml_word_counts.add_element('words')
+		  
+			@result.each do |index, key|
+				word = format_xml_words.add_element('word')
+				word.add_attribute( 'count', key)
+		
+				word.add_text "#{index}"
+			end
+		
+			out = ""
+			format_xml_files.write(out, 1)
+			puts out
+		end 
+	end
+end
