fc92932a7dc2ef0f3fe5754b840e8d1b9decf029
Bojidar Zahov <bzahov98@abv.bg>
2015-11-24 11:42:31 +0200

    V4

--- hm_count_words/B_04_Bojidar_Zahov.rb
+++ hm_count_words/B_04_Bojidar_Zahov.rb
@@ -1,21 +1,21 @@
 
-require './B_04_Bojidar_Zahov/V4/word_counter'
-#require_relative'./B_04_Bojidar_Zahov/result'
+require_relative './B_04_Bojidar_Zahov/V4/word_counter'
+#require_relative'./B_04_Bojidar_Zahov/word_counter'
 result = WordCounter.new.parse_file File.open(ARGV[0], "r")
 output = ARGV[1]
 case output
-when "json"
-File.open('result.json', 'w') {|json| json << result.to_json}
-to_json
-when "xml"
-result.to_xml
-when "all"
-File.open('result.json', 'w') {|json| json << result.to_json}
-to_json
-result.to_xml
-File.open('result.csv', 'w') {|csv| csv << result.to_csv}
-puts result.to_csv
-else
-File.open('result.csv', 'w') {|csv| csv << result.to_csv}
-puts result.to_csv
+  	when "json"
+    	File.open('result.json', 'w') {|json| json << result.to_json}	
+		to_json
+	when "xml"
+		result.to_xml
+	#when "all"
+		#File.open('result.json', 'w') {|json| json << result.to_json}
+		#to_json
+		#result.to_xml
+		#File.open('result.csv', 'w') {|csv| csv << result.to_csv}
+		#puts result.to_csv
+	else
+		File.open('result.csv', 'w') {|csv| csv << result.to_csv}
+		puts result.to_csv
 end
