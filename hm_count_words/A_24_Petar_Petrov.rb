896cfaaddbdcbc82203c2869c9ceef3669c4c96d
Petar <google1999@abv.bg>
2015-11-24 22:45:27 +0200

    Update A_24_Petar_Petrov.rb

--- hm_count_words/A_24_Petar_Petrov.rb
+++ hm_count_words/A_24_Petar_Petrov.rb
@@ -1,11 +1,5 @@
 require 'json'
 require 'rexml/document'
-<<<<<<< HEAD:hm_count_words/A_24_Petar_Petrov.rb~
-require './A_24_Petar_Petrov/word_counter/result.rb'
-require './A_24_Petar_Petrov/word_counter.rb'
-
-result = WordCounter.parse_file(ARGV[0])
-=======
 require 'word_counter'
 
 start = ARGV[0].split('/').first
@@ -14,7 +8,6 @@ if start == 'http:' || start == 'https:'
 else
   result = WordCounter.parse_file(ARGV[0])
 end
->>>>>>> 6deab35782f503bfa65afbca8303a7bc7c570690:hm_count_words/A_24_Petar_Petrov.rb
 
 if ARGV[1] == 'json'
   result.to_json
