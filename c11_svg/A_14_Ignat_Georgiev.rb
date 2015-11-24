5e7c511728100bdd979015dd913b4a75d6ae2166
IgnatGeorgiev <ignatgeorgiev1@gmail.com>
2015-11-24 13:44:10 +0200

    Flower

--- c11_svg/A_14_Ignat_Georgiev.rb
+++ c11_svg/A_14_Ignat_Georgiev.rb
@@ -2,12 +2,12 @@ def circle x,y,r
 '<circle cx="'+x.to_s+'" cy="'+y.to_s+'" r="'+r.to_s+'" stroke="black" stroke-width="3" fill="white" />'
 end
 def rect x,y,width,height
-'<rect width="'+width.to_s+'" y ="'+y.to_s+'" x ="'+x.to_s+'" height="'+height.to_s+'" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)"/>'
+'<rect width="'+width.to_s+'" y ="'+y.to_s+'" x ="'+x.to_s+'" height="'+height.to_s+'" style="fill:rgb(255,255,0);stroke-width:3;stroke:rgb(0,0,0)"/>'
 end
 
 File.open("result.svg","w") do |f|
 	f.write('<svg xmlns="http://www.w3.org/2000/svg">')
-	f.write(rect(100,100,40,40))
+	f.write(rect(95,95,55,55))
 	#left
 	f.write(circle(67,105,30))
 	#upishleft
@@ -20,5 +20,6 @@ File.open("result.svg","w") do |f|
 	f.write(circle(125,173,30))
 	#downleft
 	f.write(circle(78,165,30))
+	f.write(' <text x="100" y="250" fill="black">Ignat Georgiev</text>')
 	f.write('</svg>')
 	end
