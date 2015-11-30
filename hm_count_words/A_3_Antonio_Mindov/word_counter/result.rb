bcf3c3cf020381d806615d260cc7d4f39139113d
Antonio <amindov@abv.bg>
2015-11-30 21:59:17 +0200

    SVG

--- hm_count_words/A_3_Antonio_Mindov/word_counter/result.rb
+++ hm_count_words/A_3_Antonio_Mindov/word_counter/result.rb
@@ -40,5 +40,43 @@ module WordCounter
 	
 			xml
 		end
+
+		def get_rect(x,y,w,h)
+			'<rect width="' + w.to_s + '" height="' + h.to_s + '" x = "' + x.to_s + '" y = "' + y.to_s+ '" style="fill:blue;stroke-width:2;stroke:rgb(0,0,0)"/>'
+		end
+
+		def lerp(x,y,value)
+			(y - x) * value
+		end
+
+		def to_svg
+			c = 1
+			width = 50
+			offset = 20
+
+			minWidth = 1.0
+			maxWidth = 300.0
+
+			maxCount = 0.0;
+
+			File.open("result.svg","w") do |f|
+      			f.write('<svg xmlns="http://www.w3.org/2000/svg">')
+
+      			@word_counts.each do |word, count|
+
+      				if(maxCount < count)
+      					maxCount = count
+      				end
+
+      				height = lerp(minWidth.to_f,maxWidth.to_f, count.to_f/maxCount.to_f);
+
+      				f.write(get_rect(c * (width + offset), offset + maxWidth - height, width, height))
+      				f.write ('<text x="'+(c * (width + offset)).to_s+'" y="'+(offset + maxWidth + 10).to_s+'" style="writing-mode: tb;">'+word+'</text>')
+      				c += 1
+      			end
+      			
+      			f.write('</svg>');
+      		end
+		end
 	end
 end
