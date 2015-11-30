e86bf327ed927bf885ac67b7652745aafdff604d
radostin-angelov <angelovradostin@gmail.com>
2015-11-30 13:53:44 +0000

    rearranged files, fixed result type

--- hm_count_words/B_27_Radostin_Angelov/word_counter.rb
+++ hm_count_words/B_27_Radostin_Angelov/word_counter.rb
@@ -1,39 +1,21 @@
 module WordCounter
   require_relative "word_counter/parser.rb"
+  require_relative "word_counter/file_parser.rb"
+  require_relative "word_counter/web_parser.rb"
   require_relative 'word_counter/result.rb'
 
-  def self.parse_file(path, file_format)
-    text = FileParser.new.parse(path)
-    WordCounter.log_result(text, file_format)
+  def self.parse_file(path)
+    return FileParser.new.parse(path)
   end
 
-  def self.parse_webpage(url, file_format)
-    text = WebpageParser.new.parse(url)
-    WordCounter.log_result(text, file_format)
+  def self.parse_webpage(url)
+    return WebpageParser.new.parse(url)
   end
 
   def parse
   end
 
-  def self.parse_string(string, file_format)
-    text = Parser.new.parse(string)
-    WordCounter.log_result(text, file_format)
-  end
-
-  def self.log_result (text, file_format)
-    result = Result.new
-    result.word_counts(text)
-    result.marks_count(text)
-
-    case file_format
-      when "json"
-        result.to_json
-      when "xml"
-        result.to_xml
-      when "svg"
-      	result.to_svg
-      else
-        result.to_csv
-    end
+  def self.parse_string(string)
+    return Parser.new.parse(string)
   end
 end
