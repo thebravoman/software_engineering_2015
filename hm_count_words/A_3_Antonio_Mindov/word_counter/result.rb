06e3542d4b5b68218dd5e438f95051c41bbacc74
Antonio <amindov@abv.bg>
2015-11-30 22:33:09 +0200

    SHIT! I forgot the width and the height of the canvas.......

--- hm_count_words/A_3_Antonio_Mindov/word_counter/result.rb
+++ hm_count_words/A_3_Antonio_Mindov/word_counter/result.rb
@@ -60,7 +60,7 @@ module WordCounter
 			maxCount = 0.0;
 
 			File.open("result.svg","w") do |f|
-      			f.write('<svg xmlns="http://www.w3.org/2000/svg">')
+      			f.write('<svg width = "10000" height = "500" xmlns="http://www.w3.org/2000/svg">')
 
       			@word_counts.each do |word, count|
 
