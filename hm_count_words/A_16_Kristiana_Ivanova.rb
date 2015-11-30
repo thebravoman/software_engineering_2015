508f5fa776df800fd1e9524ca1a84107e12e89c3
Kristiana <you@example.com>
2015-11-30 18:37:22 +0200

    Homework

--- hm_count_words/A_16_Kristiana_Ivanova.rb
+++ hm_count_words/A_16_Kristiana_Ivanova.rb
@@ -1,28 +1,33 @@
-require_relative './A_16_Kristiana_Ivanova/word_counter'
-require 'builder'
-
-def uri?(string)
-	start = string.split('/').first
-  	start == 'http:' || start == 'https:'
-end
-
-def get_result(filename)
-  if File.file? filename
-    WordCounter.parse_file filename
-  elsif uri? filename
-    WordCounter.parse_webpage filename
-  else
-    WordCounter.parse filename
-  end
-end
-
-if format == 'json'
- puts result.to_json
-elsif format == 'xml'
- puts result.to_xml
-else
- puts result.to_csv
-end
+require 'csv'
+require 'json'
+require 'rexml/document'
+require './A_16_Kristiana_Ivanova/word_counter.rb'
+require './A_16_Kristiana_Ivanova/word_counter/result'
 
 filename = ARGV[0]
 format = ARGV[1]
+
+def file_site(filename)
+	start = string.split('/').first
+  	return start == "http:" || start == "https:"
+end
+
+def get_result(filename, format)
+  if File.file? filename
+    WordCounter.parse_file filename
+  elsif file_cite filename
+    get_result = WordCounter.parse_webpage filename
+  else
+    get_result = Parser.parse filename
+  end
+end
+
+if format == "json"
+ puts get_result(filename, format).to_json
+elsif format == "xml"
+ puts get_result(filename, format).to_xml
+elsif format == "svg"
+ puts get_result(filename, format).to_svg
+else
+ puts get_result(filename, format).to_csv
+end
