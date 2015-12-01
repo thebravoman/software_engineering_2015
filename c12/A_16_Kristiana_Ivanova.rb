08b0def9c0d8511d2505e2b2bac4d6bc58a59ccb
Kristiana <you@example.com>
2015-12-01 13:55:24 +0200

    changes

--- c12/A_16_Kristiana_Ivanova.rb
+++ c12/A_16_Kristiana_Ivanova.rb
@@ -1,7 +1,15 @@
 require 'json'
 
 content = File.read("A_16_Kristiana_Ivanova.json")
+parsed = Hash.new
+parsed = JSON.parse(content)
 
-parsered = JSON.parse(content)
-
-puts content
+parsed.each do |school, other|
+	puts "#{school}"
+  other.each do |classes, name_hash|
+  	puts " #{classes}"
+    name_hash.each do |name|
+      puts "  #{name}"
+    end
+  end
+end
