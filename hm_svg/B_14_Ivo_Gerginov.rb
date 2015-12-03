f9f50c8c36abf64cc1c9f1c496ddad24a955220d
Reaper9806 <ivo.gerginov9806@gmail.com>
2015-12-03 21:55:46 +0200

    Working on JSON Tree

--- hm_svg/B_14_Ivo_Gerginov.rb
+++ hm_svg/B_14_Ivo_Gerginov.rb
@@ -1,20 +1,24 @@
 require 'json'
 
+
+x = 40
+y = 40
+
 def treeNode(x, y, str)
 	'<circle cx = "'+x.to_s+'" cy = "'+y.to_s+'" r = "40" stroke = "black" stroke-width = "2" fill = "none"/>' + "\n" +
 	'<text x = "'+(x - 15).to_s+'" y = "'+(y+5).to_s+'" fill = "black">'+str.to_s+'</text> ' + "\n"
 end
 
-def makeTree(x, y, hash, file) 
+def makeTree(hash, file) 
   hash.each do |key, value|
     file << treeNode(x, y, key)
     x += 100
-    y += 100
     if value.kind_of?(Hash)
-	  file << makeTree(x, y, value, file)
+	  file << makeTree(value, file)
 	elsif value.kind_of?(Array)
 	  value.each do |e|
-	    file << makeTree(x, y, e, file)
+	  	y+= 100
+	    file << makeTree(e, file)
 	  end
 	end
   end
@@ -26,7 +30,7 @@ file = File.read(jsonFile)
 data = JSON.parse(file)
 
 File.open('B_14_Ivo_Gerginov.svg', 'w') do |svg|
-  svg << '<svg xmlns = "http://www.w3.org/2000/svg">' + "\n"
-  makeTree(40, 40, data, svg)
+  svg << '<svg xmlns = "http://www.w3.org/2000/svg" width="10000" height="10000">' + "\n"
+  makeTree(data, svg)
   svg << '</svg>'
 end
