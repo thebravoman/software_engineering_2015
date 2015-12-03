2d261f643feda94663fd886df3ef7bc18dd71f9c
frostblooded <frostblooded@yahoo.com>
2015-12-03 17:00:04 +0200

    made tree

--- hm_svg/A_21_Nikolay_Danailov.rb
+++ hm_svg/A_21_Nikolay_Danailov.rb
@@ -1,18 +1,88 @@
 require 'json'
 
-class SVGTreeGenerator
+class TreeNode
+  attr_accessor :value
+  attr_accessor :depth
+  attr_accessor :ancestor
+  attr_accessor :descendants
+
+  def initialize(value, ancestor = nil, descendants = [])
+    @value = value
+    @depth = 0
+    @ancestor = ancestor
+    @descendants = descendants
+
+    if(ancestor != nil)
+      @depth = ancestor.depth + 1
+
+      if(!ancestor.descendants.include?(self))
+        ancestor.descendants << self
+      end
+    end
+
+    if(descendants.size > 0)
+      @depth = descendants.first.depth - 1
+      descendants.each { |descendant| descendant.ancestor = self }
+    end
+  end
+end
+
+class Tree
+  attr_accessor :root
+  attr_accessor :current_node
 
   def initialize
-    @STARTING_X = 200
-    @STARTING_Y = 100
-    @RECT_WIDTH = 100
-    @RECT_HEIGHT = 50
+    @root = TreeNode.new 0
+    @current_node = @root
+    node_1 = TreeNode.new 1, @root
+    node_2 = TreeNode.new 2, @root
+    node_3 = TreeNode.new 2.5, node_2
+    node_4 = TreeNode.new 1.5, node_1
+  end
 
-    @result = ''
+  def generate_from_json(element, depth = 0)
+    if(element.class == Hash)
+      element.each do |key, value|
+        generate_from_json value, depth
+      end
+    elsif(element.class == Array)
+      element.each do |e|
+        generate_from_json e, depth + 1
+      end
+    else
+      if depth == 0
+        @root = TreeNode.new element
+      else
+
+      end
+    end
+  end
+
+  def print_tree(node = @root)
+    puts "#{' ' * node.depth}#{node.value}"
+
+    if node.descendants.size > 0
+      node.descendants.each { |descendant| print_tree descendant }
+    end
+  end
+end
+
+class SVGTreeGenerator
+  @STARTING_X = 200
+  @STARTING_Y = 100
+  @RECT_WIDTH = 100
+  @RECT_HEIGHT = 50
+
+  def initialize
+    @tree = Tree.new
   end
 
   def generate_from_json(json)
-    
+    @tree.generate_from_json(json)
+  end
+
+  def draw
+    @tree.print_tree
   end
 end
 
@@ -30,13 +100,10 @@ def text(x, y, str)
     '" fill="black">' + str.to_s + '</text>'
 end
 
+RESULT_FILE = "A_21_Nikolay_Danailov.svg"
+
 json_file = ARGV[0]
 file = File.read json_file
 json = JSON.parse file
-result_file_name = "A_21_Nikolay_Danailov.svg"
-
-File.open(result_file_name, 'w') do |f|
-  f << '<svg xmlns="http://www.w3.org/2000/svg">'
-  f << SVGTreeGenerator.new.generate_from_json(json)
-  f << '</svg>'
-end
+tree_generator = SVGTreeGenerator.new
+tree_generator.draw
