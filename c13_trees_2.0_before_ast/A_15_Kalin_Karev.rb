e89f59d73126568c740f4d37675450be7dbfb176
Kalin Karev <karev.kalin@gmial.com>
2015-12-03 11:30:47 +0100

    Updated

--- c13_trees_2.0_before_ast/A_15_Kalin_Karev.rb
+++ c13_trees_2.0_before_ast/A_15_Kalin_Karev.rb
@@ -17,3 +17,17 @@ xml_doc.root.elements.each("school") do |school|
 		
 	end 
 end
+
+def circle x, y, r, cr, cg, cb
+	'<circle cx="'+x.to_s+'" cy="'+y.to_s+'" r="'+r.to_s+'" stroke="red" stroke-width="3" fill="red" style="fill:rgb('+cr.to_s+', '+cg.to_s+', '+cb.to_s+')"/>'
+end
+
+File.open("A_15_Kalin_Karev.svg", "w") do |f|
+	f.write('<svg xmlns="http://www.w3.org/2000/svg">')
+	
+		f.write(circle 450, 80, 60, 255, 255, 0)
+		f.write(circle 200, 250, 50, 255, 255, 0)
+		f.write(circle 900, 250, 50, 255, 255, 0)
+	
+	f.write('</svg>')
+end
