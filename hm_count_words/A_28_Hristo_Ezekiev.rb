4a62f1b671a9413c9b0636d8d462c0e64c7c8393
Icaka <ici_ioana@abv.bg>
2015-12-05 17:40:55 +0200

    Om

--- hm_count_words/A_28_Hristo_Ezekiev.rb
+++ hm_count_words/A_28_Hristo_Ezekiev.rb
@@ -1,22 +1,25 @@
 require 'word_counter'
 filepath = ARGV[0]
+beginning = ARGV[0].split('/').first
+
 def webb
-	if( filepath.start_with?('http://') || filepath.start_with?('https://'))
+	if( beginning = 'http:' || beginning = 'https:')
 		result = WordCounter.parse_webpage(filepath)
 	else
 		result = WordCounter.parse_file(filepath)
 	end
 end
-webb
+
+webb(beginning)
+
 if ARGV[1] == 'json'
     puts result.to_json
 
 elsif ARGV[1] == 'xml'
     puts result.to_xml
-elsif ARGV[1] == "svg"
-	puts coming soon...
+#elsif ARGV[1] == "svg"
+	#result coming soon...
 else
     puts result.to_csv
 end
-
 
