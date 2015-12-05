ec579554495687f7696eb415faeda68c5b113d11
valentin <you@example.com>
2015-12-05 21:07:21 +0200

    sorted

--- hm_bonus_minify/B_7_Valentin_Stoyanov.rb
+++ hm_bonus_minify/B_7_Valentin_Stoyanov.rb
@@ -2,11 +2,22 @@ require 'csv'
 
 file = File.open(ARGV[0])
 date = ARGV[1]
+output = []
 
 file.each do |line|
 	if line.split.first == date
-		puts line
+		output << line
 	end
 end
 
+output.sort_by!{|d| 
+	arr = d.split('/')
+	m = arr[0].to_i
+	d = arr[1].to_i
+	y = arr[2].to_i
+	
+	[y, m, d]
+}
+
+puts output
 
