16b35f3403f2cda2e2731102031899786bb0f365
viking1998 <google1999@abv.bg>
2015-12-07 15:47:26 +0200

    Made multifunctional V6

--- hm_count_words/A_24_Petar_Petrov/word_counter/parser.rb
+++ hm_count_words/A_24_Petar_Petrov/word_counter/parser.rb
@@ -3,7 +3,11 @@ require_relative 'result'
 module WordCounter
   class Parser 
       def mark_count(text)
-      	text.scan(/[^a-z\s_0-9]/).size
+      	if ARGV[0].split('.').last == 'rb' || ARGV[0].split('/').first == 'http:' || ARGV[0].split('/').first == 'https:'
+        	text.scan(/[^a-z\s_0-9]/).size
+        else
+        	text.scan(/[^a-z _0-9]/).size
+        end
       end
     
       def split_string(text)
