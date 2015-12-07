0389f66ae51c7f767ac8ee5ba2165c6c5b513621
HristiyanZahariev <ihzahariev@gmail.com>
2015-12-08 00:21:02 +0200

    Added V2, but not sorted at all

---
+++ hm_bonus_minify/A_27_Hristiyan_Zahariev.rb
@@ -0,0 +1,13 @@
+require 'csv'
+
+csv_file = CSV.read ARGV[0]
+date = ARGV[1]
+value = ARGV[2].to_i
+csv_file.each do |element|
+  csv_file.sort!{|a,b| a[2] <=> b[2]} #I couldn't understand the requirement
+  if element[0] == date && element[3].to_i.between?((value-10),(value+10))
+    puts element * ","
+  elsif value == 0 && element[0] == date
+    puts element * ","
+  end
+end
