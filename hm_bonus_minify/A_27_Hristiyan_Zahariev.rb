53f3ee9df01e4ede4a3de477c14db563d2415c1b
HristiyanZahariev <ihzahariev@gmail.com>
2015-12-08 00:21:53 +0200

    Added V2, but not sorted at all

--- hm_bonus_minify/A_27_Hristiyan_Zahariev.rb
+++ hm_bonus_minify/A_27_Hristiyan_Zahariev.rb
@@ -4,7 +4,7 @@ csv_file = CSV.read ARGV[0]
 date = ARGV[1]
 value = ARGV[2].to_i
 csv_file.each do |element|
-  csv_file.sort!{|a,b| a[2] <=> b[2]} #I couldn't understand the requirement
+ # csv_file.sort!{|a,b| a[2] <=> b[2]} #I couldn't understand the requirement. This is sorted by category
   if element[0] == date && element[3].to_i.between?((value-10),(value+10))
     puts element * ","
   elsif value == 0 && element[0] == date
