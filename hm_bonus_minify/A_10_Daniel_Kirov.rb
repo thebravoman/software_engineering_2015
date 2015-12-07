5125a331571f1a2759d7322f5eb86268506286b7
frozdy <danielkirov@abv.bg>
2015-12-07 19:59:51 +0200

    YOLOOOOOO

--- hm_bonus_minify/A_10_Daniel_Kirov.rb
+++ hm_bonus_minify/A_10_Daniel_Kirov.rb
@@ -2,11 +2,11 @@ require 'csv'
 
 my_csv = CSV.read ARGV[0]
 date = ARGV[1]
-my_csv.each do |line|
-	line.each do |element|	
-		if element == date
-			puts line.join(",")
-		end
+value = ARGV[2].to_i
+puts value
+my_csv.each do |line|	
+	if line[0] == date && line[3].to_i.between?((value-10),(value+10))
+		puts line.join(",")
 	end
 end
 
