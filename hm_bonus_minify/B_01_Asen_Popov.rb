c139688f087fa82a7a3c243949a8f4c619b027ef
Asen Popov <apopov98@gmail.com>
2015-12-08 22:43:29 +0200

    refugee influx reduced

--- hm_bonus_minify/B_01_Asen_Popov.rb
+++ hm_bonus_minify/B_01_Asen_Popov.rb
@@ -2,10 +2,12 @@ require 'csv'
 
 fileName = ARGV[0]
 date = ARGV[1]
-
+value = ARGV[2].to_i
 
 CSV.read(fileName, 'r').each do |row|
 	if row[0] == date
-		puts row.join(" , ")
+		if (row[3].to_i > value-10) && (row[3].to_i < value+10) 
+			puts row.join(" , ")
+		end
 	end
 end
