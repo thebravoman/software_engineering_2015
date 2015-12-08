0e9c969270c8404d4476e2b1b0353907e5a595e8
emchoko <emilfrogy@gmail.com>
2015-12-08 20:37:33 +0200

    V2

--- hm_bonus_minify/A_12_Emil_Lozev.rb
+++ hm_bonus_minify/A_12_Emil_Lozev.rb
@@ -1,13 +1,20 @@
 require 'csv'
 file_line_argument = ARGV[0]
 date_line_argument = ARGV[1]
+value = ARGV[2]
 
 csv = CSV.read file_line_argument
 array_to_display = []
 
 csv.each do |line|
-	if line[0] == date_line_argument
-		array_to_display << line.join(",")
+	if ARGV[2] == nil
+		if line[0] == date_line_argument
+			array_to_display << line.join(",")
+		end
+	elsif ARGV[2] != nil
+		if line[0] == date_line_argument && (line[3].to_i >= (value.to_i - 10) && line[3].to_i <= value.to_i + 10)  
+			array_to_display << line.join(",")
+		end
 	end
 end
 
