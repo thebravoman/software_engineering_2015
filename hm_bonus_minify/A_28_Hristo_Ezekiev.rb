a7b910bf7e70072da2839ab59037a48766d2ad0d
Icaka <ici_ioana@abv.bg>
2015-12-08 19:21:06 +0200

    BONUS 2

--- hm_bonus_minify/A_28_Hristo_Ezekiev.rb
+++ hm_bonus_minify/A_28_Hristo_Ezekiev.rb
@@ -2,11 +2,21 @@ require 'csv'
 filename = ARGV[0]
 
 date = ARGV[1]
-
+input_val = ARGV[2].to_i
 DATE_ELEMENT = 0
 
-CSV.open(filename, 'r') do |row|
-  if row[DATE_ELEMENT] == date
-    puts row.join(",").to_s
-  end  
+if ARGV[2]
+	CSV.open(filename, 'r').each do |row|
+		val = row[3]
+		val = val.to_i
+		if row[DATE_ELEMENT] == date && (val-10)<input_val && input_val<(val+10)
+			puts row.join(",").to_s
+		end
+	end
+else
+	CSV.open(filename, 'r').each do |row|
+		if row[DATE_ELEMENT] == date
+		  puts row.join(",").to_s
+		end  
+	end
 end
