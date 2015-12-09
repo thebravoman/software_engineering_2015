5e743de38f57cad948674ce5b584f8b0903ddb1c
Nikolay Karagyozov <newmast@abv.bg>
2015-12-09 17:05:59 +0200

    fixed date sorting

--- hm_bonus_minify/B_22_Nikolay_Karagyozov.rb
+++ hm_bonus_minify/B_22_Nikolay_Karagyozov.rb
@@ -1,8 +1,13 @@
+require 'date'
+
+DATE_FORMAT = "%d/%m/%Y"
+
 file_path = ARGV[0]
 account = ARGV[1]
 
 matching_accounts = []
 matching_amount = 0
+
 File.readlines(file_path).each do |line|
    line_details = line.split(',')
    line_account = line_details[1]
@@ -13,6 +18,8 @@ File.readlines(file_path).each do |line|
      matching_amount += line_amount
    end
 end
+
+matching_accounts.sort_by! { |line| Date.strptime(line.split(',').first, DATE_FORMAT) }
 
 puts matching_accounts
 puts matching_amount.to_i
