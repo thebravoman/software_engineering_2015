abc39ccc05cc247f9cbb45d343a174f486eb444a
DieEin <savageshadow@abv.bg>
2015-12-09 19:57:06 +0300

    fixed bug in version 2

--- hm_bonus_minify/B_28_Toma_Marinov.rb
+++ hm_bonus_minify/B_28_Toma_Marinov.rb
@@ -2,9 +2,13 @@ require 'csv'
 
 file_name = ARGV[0]
 date = ARGV[1]
-value = ARGV[2].to_i
+bool_value = ARGV[2] == nil
 
-def write_result_to_array(file_name, date, value)
+unless bool_value
+	value = ARGV[2].to_i
+end
+
+def write_result_to_array(file_name, date, value, any_value)
 	result = []
 
 	File.readlines(file_name).each do |line|
@@ -12,12 +16,18 @@ def write_result_to_array(file_name, date, value)
 		current_line_date = current_line.first()
 		current_line_amount = current_line[3].to_i
 
-		if(current_line_date == date and (current_line_amount > (value - 10) and current_line_amount < (value + 10)))
-			result << line
+		if any_value
+			if current_line_date == date
+				result << line
+			end
+		else
+			if(current_line_date == date and (current_line_amount > (value - 10) and current_line_amount < (value + 10)))
+				result << line
+			end
 		end
 	end
 
 	return result
 end
 
-puts write_result_to_array(file_name, date, value)
+puts write_result_to_array(file_name, date, value, bool_value)
