24beb3d4b37139e5a72832ad03a047ed0a78612b
bakasura980 <bakasura@abv.bg>
2015-12-09 17:49:07 +0200

    V2

---
+++ hm_bonus_minify/B_18_Lubomir_Kipov.rb
@@ -0,0 +1,23 @@
+require 'csv'
+
+def chek_range_of_amount (value, line)
+	if line.split(',')[3]	.to_i > value.to_i - 10 and line.split(',')[3].to_i < value.to_i + 10
+		print_line(line)
+	end
+end
+
+def print_line (line)
+	puts line
+end
+
+filename = ARGV[0]
+searching_date = ARGV[1]
+File.open(filename, "r") do |row|
+	row.each do |line|
+		if line.include? searching_date
+			chek_range_of_amount ARGV[2], line
+		end
+	end
+end
+
+
