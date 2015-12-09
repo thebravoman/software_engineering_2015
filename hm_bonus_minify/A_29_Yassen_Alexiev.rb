698b3368583a611b4f266f30f795621ac449c3c5
Yassen Alexiev <yassen4o222@abv.bg>
2015-12-09 19:15:04 +0200

    V2

--- hm_bonus_minify/A_29_Yassen_Alexiev.rb
+++ hm_bonus_minify/A_29_Yassen_Alexiev.rb
@@ -3,9 +3,19 @@ require 'csv'
 array = []
 file_name = CSV.read ARGV[0]
 date = ARGV[1]
+value = ARGV[2].to_i
 
-file_name.each do |row|
-	if row[0] == date
+if value
+	file_name.each do |row|
+	if row[0] == date && row[3].to_i >= (value-10) && row[3].to_i <= (value+10)
 		puts row.join(",")
+	end
+end
+
+else
+	file_name.each do |row|
+		if row[0] == date
+			puts row.join(",")
+		end
 	end
 end
