f0783fb9fcf16406241c0fd70e26ac0911652889
radostin-angelov <angelovradostin@gmail.com>
2015-12-09 21:23:42 +0000

    printing tree with DFS

--- hm_svg/B_27_Radostin_Angelov.rb
+++ hm_svg/B_27_Radostin_Angelov.rb
@@ -20,9 +20,63 @@ def parse_xml path
 	doc.elements.each("inventory/section") { |element| puts element.attributes["name"] }
 end
 
-#result = parse_json(path)
-result = parse_xml(path)
-p result
+@current_width = 100
+def print_svg_tree_dfs(printer, node, svg, current_width, h)
+	if not node.is_a? Hash
+		#TODO: FIX THIS
+		return
+	end
+	
+	level_start_width = current_width
+	children_count = 0
+	node.each do |key, value|
+		if key == "children"
+			value.each do |nested_object|
+				children_count += 1;
+				print_svg_tree_dfs(printer, nested_object, svg, @current_width , h + 180)
+				#@current_width += 100
+				@current_width += 180
+				/if !nested_object["children"]
+					svg << printer.print_circle(w, h)
+				end/
+			end
+		end
+	end
+	
+	if !(node["children"])
+		svg << printer.print_circle(@current_width , h)
+		#p node["node_name"] + " at " + current_width.to_s + " " + h.to_s
+		text_width = @current_width - 70
+		svg << printer.print_text(text_width, h, node["node_name"])
+		printer.print_text(text_width, h, node["node_name"])
+	else
+		current_node_print_width = (@current_width - 180 + level_start_width) / 2
+		svg << printer.print_circle(current_node_print_width , h)
+		text_width = current_node_print_width - 70
+		svg << printer.print_text(text_width, h, node["node_name"])
+		#p node["node_name"] + " at " + current_node_print_width.to_s + " " + h.to_s
+	end
+	
+	
+end
+
+class SvgPrinter
+	def print_circle(w, h)
+		return '<circle cx="' + w.to_s + '" cy="' + h.to_s + '" r="80" stroke="black" stroke-width="3" fill="red" />'
+	end
+	
+	def print_text(width, height, text)
+		return '<text x="' + width.to_s + '" y="' + height.to_s + '">' + text.to_s + '</text>'
+	end
+end
+
+def print_svg(parsed_data)
+	File.open("B_27_Radostin_Angelov.svg", "w") do |svg|
+		svg << '<svg xmlns="http://www.w3.org/2000/svg" height="3000" width="1000">'
+		print_svg_tree_dfs(SvgPrinter.new, parsed_data, svg, 0, 200)
+		svg << '</svg>'
+	end
+end
 
 def print_tree(json, offset, count)
 	json.each do |key, value|
@@ -38,54 +92,7 @@ def print_tree(json, offset, count)
 	end
 end
 
-#print_tree(result, " ", 0)
-
-#uses BFS needs to use DFS
-def print_svg_tree(json, svg, w, h)
-	if json.is_a? String or json.is_a? Numeric or json.nil?
-		return
-	end
-	
-	json.each do |key, value|
-		if key == "node_name"
-			svg << '<circle cx="' + w.to_s + '" cy="' + h.to_s + '" r="40" stroke="black" stroke-width="3" fill="red" />'
-			p key + " : " + value + " height: " + h.to_s + " width: " + w.to_s
-		elsif key == "children"
-			old_width = w
-			if value.count == 1
-				p "value.count = 1"
-				value.each do |nested_object|
-					#@height = @height + 100 printing children of this node
-					print_svg(nested_object, svg, w, h + 100)
-					w = w + 100
-					#p nested_object
-				end
-			elsif value.count > 1
-				p "value.count > 1"
-				left_children_count = value.count / 2
-				w = w - (100 * left_children_count)
-				for i in 1..left_children_count
-					p i
-					print_svg_tree(value[i], svg, w, h + 100)
-					w = w + 100
-				end
-				
-				#next_child = left_children_count  + 1
-				w = old_width + 100
-				for i in left_children_count ..value.count
-					p "right"
-					print_svg_tree(value[i], svg, w, h + 100)
-					w = w + 100
-				end
-			end
-		end
-	end
-end
-
-def print_svg
-	File.open("B_27_Radostin_Angelov.svg", "w") do |svg|
-		svg << '<svg xmlns="http://www.w3.org/2000/svg" height="3000" width="1000">'
-		print_svg_tree(result, svg, 500, 200)
-		svg << '</svg>'
-	end
-end
+result = parse_json(path)
+print_svg(result)
+print_tree(result, " ", 0)
+#result = parse_xml(path)
