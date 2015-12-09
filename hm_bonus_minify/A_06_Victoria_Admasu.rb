4b4ad2070170bd6202edc32bedc173c11b867e55
vicky98 <vikiga98@gmail.com>
2015-12-09 19:10:13 +0200

    Version 2

--- hm_bonus_minify/A_06_Victoria_Admasu.rb
+++ hm_bonus_minify/A_06_Victoria_Admasu.rb
@@ -2,10 +2,15 @@ require 'csv'
 
 file = ARGV[0]
 date = ARGV[1]
+value = ARGV[2].to_i
+v1 = value - 10
+v2 = value + 10
 csv = CSV.read file
 
 csv.each { |x|
 	if x[0] == date
-		puts x.join(',').to_s
+		if (x[3].to_i >= v1) && (x[3].to_i <= v2) 
+			puts x.join(',').to_s
+		end
 	end
 }
