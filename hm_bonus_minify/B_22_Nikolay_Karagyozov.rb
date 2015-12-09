cbdb5d3cfd5c878ae37ab4f85e6bfeca04773764
Nikolay Karagyozov <newmast@abv.bg>
2015-12-09 16:00:05 +0200

    version 2 now not sorting whats already sorted

--- hm_bonus_minify/B_22_Nikolay_Karagyozov.rb
+++ hm_bonus_minify/B_22_Nikolay_Karagyozov.rb
@@ -3,11 +3,13 @@ date = ARGV[1]
 value = ARGV[2].to_f
 
 output = []
+
 File.readlines(file_path).each do |line|
    line_details = line.split(',')
    line_date = line_details.first
+   line_account = line_details.second
    line_amount = line_details[3].to_f
    output.push(line) if line_date == date && line_amount.between?(value - 10, value + 10)
 end
-output.sort!
+
 puts output
