98a480ceefd6182728e03439a5f32d6e71e61e30
viking1998 <google1999@abv.bg>
2015-12-09 21:54:23 +0200

    Update

--- hm_svg/A_24_Petar_Petrov.rb
+++ hm_svg/A_24_Petar_Petrov.rb
@@ -10,12 +10,14 @@ class SVG_Parser
 		@stroke_width = 2
 		@stroke_color = 'black'
 		@c_counter = 0
-		@line_x_1 = 490 
-		@line_y_1 = 55
-		@line_x_2 = @line_x_1 + 20
+		@line_x_1 = @initial_x - 30 
+		@line_y_1 = @initial_y + 30
+		@line_x_2 = @line_x_1 - 20
 		@line_y_2 = @line_y_1 + 20
 		@rect_width = 100
 		@rect_height = 50
+		@arr_index = 0
+		@hash_index = 0
 	end
 	def generate_svg_tree(parsed)
 		File.open("A_24_Petar_Petrov.svg", "w") do |svg_file|
@@ -28,65 +30,62 @@ class SVG_Parser
 	end
 	
 	private
-		def iteration(hash,file,index=0,size=1)
+		def iteration(hash,file,arr_size=1,hash_size=1)
 			if hash.is_a?(Hash)	
-				hash.each_with_index do |(key, value), i|	
-					if index >= 1
-						iteration(value,file,index,size) 
-					else
-						iteration(value,file,i,size) 
-					end
-					#draw_line(file)
+				hash.each_with_index do |(key, value), i|
+						@hash_index = i	
+						iteration(value,file,arr_size,hash.size)
 				end
 			elsif hash.is_a?(Array)
 				hash.each_with_index do |el,arr_index|
-					puts hash
-					puts hash.size
-					iteration(el,file,arr_index,hash.size)
+					@arr_index = arr_index
+					iteration(el,file,hash.size,hash_size)
 				end
 			else
-				if index > 0
-					draw_rectangle(hash,file,index)
+
+				if hash_size == 1
+					draw_rectangle(hash,file,arr_size,hash_size)
 				else
-					draw_circle(hash,file,index) 
+					draw_circle(hash,file,arr_size,hash_size) 
 				end
+				draw_line(file)
 			end
 		end
 		
 		def draw_line(file)
-			file.write('<line x1="'+@line_x_1.to_s+'" y1="'+@line_y_1.to_s+'" x2="'+@line_x_2.to_s+'" y2="'+@line_y_2.to_s+'" stroke-width="2"/>')
+			file.write('<line x1="'+@line_x_1.to_s+'" y1="'+@line_y_1.to_s+'" x2="'+@line_x_2.to_s+'" y2="'+@line_y_2.to_s+'" style="stroke:blue;stroke-width:2" />')
 		end
 		
-		def draw_circle(parent,file,index)
+		def draw_circle(parent,file,arr_size,hash_size)
 			if(@c_counter > 0)
-				if index == 0
-					@initial_x -= 100
+				if @hash_index == 0 && @arr_index == 0
+					@initial_x -= 250*(arr_size-1)
+					@initial_y += 100
+					@line_x_1 -= @initial_x + 10
+					@line_y_1 += @initial_y + 5
+					@line_x_2 += @line_x_1 + 20
+					@line_y_2 += @line_y_1 + 10
+				elsif @arr_index > 0 && @hash_index == 0
+					@initial_x += 2*200	
+					@line_x_1 += @initial_x - 20		
+				end
+			end
+			file.write('<circle cx="'+@initial_x.to_s+'" cy="'+@initial_y.to_s+'" r="'+@circle_radius.to_s+'" fill="'+@fill+'" stroke="'+@stroke_color+'" stroke-width="'+@stroke_width.to_s+'"/>')
+			file.write('<text x="'+(@initial_x-26).to_s+'" y="'+(@initial_y+3).to_s+'">'+parent+'</text>')
+			@c_counter = 1
+		end
+		
+		def draw_rectangle(child,file,arr_size,hash_size)
+			if @hash_index == 0 && @arr_index == 0
+					@initial_x -= 100*(arr_size-1)
 					@initial_y += 100
 					@line_x_1 -= 100 + 10
 					@line_y_1 += 100 + 5
 					@line_x_2 += @line_x_1 + 20
 					@line_y_2 += @line_y_1 + 20
-				elsif index > 0
-					@initial_x += 2*100	
-					@line_x_1 += 2*100 - 10		
-				end
-			end
-			file.write('<circle cx="'+@initial_x.to_s+'" cy="'+@initial_y.to_s+'" r="'+@circle_radius.to_s+'" fill="'+@fill+'" stroke="'+@stroke_color+'" stroke-width="'+@stroke_width.to_s+'"/>')
-			file.write('<text x="'+(@initial_x-26).to_s+'" y="'+(@initial_y+3).to_s+'">'+parent+'</text>')
-			@c_counter += 1
-		end
-		
-		def draw_rectangle(child,file,index)
-			if index == 0
-					@initial_x -= 100
-					@initial_y += 50
-					@line_x_1 -= 100 + 10
-					@line_y_1 += 100 + 5
-					@line_x_2 += @line_x_1 + 20
-					@line_y_2 += @line_y_1 + 20
-				elsif index > 0
-					@initial_x += 2*100	
-					@line_x_1 += 2*100 - 10		
+				elsif @hash_index == 0 && @arr_index > 0
+					@initial_x += 150	
+					@line_x_1 += 150 - 10		
 				end		
 			file.write('<rect x="'+@initial_x.to_s+'" y="'+@initial_y.to_s+'" width="'+@rect_width.to_s+'" height="'+@rect_height.to_s+'" fill="'+@fill+'" stroke="'+@stroke_color+'" stroke-width="'+@stroke_width.to_s+'"></rect>')
 			file.write('<text x="'+(@initial_x+(100/child.length)).to_s+'" y="'+(@initial_y+30).to_s+'">'+child+'</text>')
