2184e9704e641c5fe27f55d30498a9e2a233b15a
unknown <hihihu@abv.bg>
2015-12-09 19:09:55 +0200

    Adding version 2

--- hm_bonus_minify/B_06_Boriana_Savova.rb
+++ hm_bonus_minify/B_06_Boriana_Savova.rb
@@ -1,12 +1,16 @@
 require 'csv'
 filename = ARGV[0]
 date = ARGV[1]
+value = ARGV[2].to_i
+
 
 file = File.open(filename, 'r')
 
 file.each do |row|
 	row = row.split(',')
 	if row[0] == date
-		puts row.join(',')
+		if (row[3].to_i<value+10) && (row[3].to_i>value-10)
+			puts row.join(',')
+		end
 	end
 end
