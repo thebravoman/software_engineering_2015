e064710d338fbac094640e8857e2bf151c19575e
velioo <velioocs@mail.bg>
2015-12-10 12:29:09 +0200

    Added v2

--- hm_bonus_minify/A_05_Velin_Yavorski.rb
+++ hm_bonus_minify/A_05_Velin_Yavorski.rb
@@ -6,7 +6,7 @@ $value = ARGV[2].to_i
 
 def date_value? line
 	if ARGV[2]
-			puts line.join(",") if ( (line[0] == $date) && (line[3].to_i.between?($value-10, $value+10)))
+		puts line.join(",") if ( (line[0] == $date) && (line[3].to_i.between?($value-10, $value+10)))
 	else
 		puts line.join(",") if line[0] == $date
 	end
