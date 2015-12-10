311300e1d516fe8081eac5b4eae2cb7091cc1804
bufferies98 <pavelcska1948@abv.bg>
2015-12-10 11:26:56 +0200

    Update A_22_Pavel_Kostadinov.rb
    version 2

--- hm_bonus_minify/A_22_Pavel_Kostadinov.rb
+++ hm_bonus_minify/A_22_Pavel_Kostadinov.rb
@@ -1,7 +1,7 @@
 require 'csv'
-def printing(csv_file, date)
+def printing(csv_file, date, value)
   csv_file.each do |line|
-    if line[0] == date
+    if line[0] == date && line[3].to_i.between?((value - 10), (value + 10))
       puts line.join(',')
     end
   end
@@ -9,6 +9,7 @@ end
 
 file = CSV.read ARGV[0]
 date = ARGV[1]
+value = ARGV[2].to_i
 
 
-printing(file, date)
+printing(file, date, value)
