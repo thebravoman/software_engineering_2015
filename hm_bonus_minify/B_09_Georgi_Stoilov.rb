428e10421e368b71b2d30671c1f69e1e4e4ce1e5
Georgi Stoilov <gogo.stoilov@abv.bg>
2015-12-10 18:31:01 +0200

    Version 2 of Minify Againnnn.

--- hm_bonus_minify/B_09_Georgi_Stoilov.rb
+++ hm_bonus_minify/B_09_Georgi_Stoilov.rb
@@ -19,8 +19,9 @@ file = File.open(file, 'r') do |f|
     amount = line[3].to_i
     if line[0] == sec_argv && !$is_there_value
       puts line.join(',')
-    elsif line[0] == sec_argv && $is_there_value && (amount >= value.to_i-10 && amount <= value.to_i+10)
-      puts line.join(',')
+    else-if line[0] == sec_argv && $is_there_value && (amount >= value.to_i-10 && amount <= value.to_i+10)
+        puts line.join(',')
+      end
     end
   end
 end
