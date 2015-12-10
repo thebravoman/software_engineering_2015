a85d4c3f70700b634c0faa0811326c7886cfc332
4o4oTo <4o4olino70@gmail.com>
2015-12-10 11:24:36 -0800

    V2 ready , i think it may actually work

--- hm_bonus_minify/A_13_Ivan-Asen_Ovedenski.rb
+++ hm_bonus_minify/A_13_Ivan-Asen_Ovedenski.rb
@@ -2,12 +2,13 @@ require 'csv'
 
 filename = ARGV[0]
 date = ARGV[1]
+value = ARGV[2].to_i
 output_info = []
 
 my_csv = CSV.read filename
 
 my_csv.each do |line|
-	if line[0] == date 
+	if (line[0] == date)  && (line[3].to_i > (value - 10)) && (line[3].to_i < (value + 10))
 		output_info << line.join(',')
 	end
 end
@@ -16,4 +17,3 @@ output_info.each do |element|
 	print element
 end
 puts
-
