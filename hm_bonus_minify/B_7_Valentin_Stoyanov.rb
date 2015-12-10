686fef02ec87cb71b085b502285c8a95fe0768c7
valentin <you@example.com>
2015-12-10 17:08:15 +0200

    Monefy V2

--- hm_bonus_minify/B_7_Valentin_Stoyanov.rb
+++ hm_bonus_minify/B_7_Valentin_Stoyanov.rb
@@ -1,8 +1,16 @@
 file = File.open(ARGV[0])
 date = ARGV[1]
+value = ARGV[2]
+output = []
  
 file.each do |line|
-	if line.split(',').first == date
-		puts line
- 	end
+	l = line.split(',')
+	if l[0] == date 
+		value = value.to_i
+		if l[3].to_i > value - 10 and l[3].to_i < value + 10
+			output << line
+		end
+	end
 end
+
+puts output
