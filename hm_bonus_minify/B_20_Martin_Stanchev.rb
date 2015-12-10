b08aca6d8bf46e7b06196d66430226d49ed8e42c
Your Name <stanchev.martin@gmail.com>
2015-12-10 17:21:18 +0200

    not sure about the output but  V3 complete

--- hm_bonus_minify/B_20_Martin_Stanchev.rb
+++ hm_bonus_minify/B_20_Martin_Stanchev.rb
@@ -1,27 +1,50 @@
 require 'csv'
 
-def print_by_date csv_doc, date_path, value
+class PersonalFinance
+  
+  def value_and_date csv_doc, date_path, value
     
-  margin = date_path.split("/").first  
+    csv_doc.each do |row|
+      if row[0] == date_path.to_s and ((row[3].to_i <= value + 10) and (row[3].to_i >= value - 10))
+        puts row.join(",")
+        
+      elsif value == 0 and row[0] == date_path.to_s
+        puts row.join(",")
+      end
+    end 
+  end
+
+  def by_account csv_doc, string 
+    sum = 0
+    acc = []
     
-  csv_doc.each do |row|
-    if row[0] == date_path.to_s and ((row[3].to_i <= value + 10) and (row[3].to_i >= value - 10))
-      puts row.join(",")
-      
-    elsif value == 0 and row[0] == date_path.to_s
-      puts row.join(",")
+    csv_doc.each do |row|
+      if row[1] == string
+        acc << row.join(",")
+        sum += row[3].to_i
+      end
     end
-   
+    
+    acc.sort_by! { |date| date}
+    puts acc 
+    puts sum
   end
   
 end
 
 
 filepath = ARGV[0]
-date_path = ARGV[1]
+in_string = ARGV[1]
 value = ARGV[2]
 
 csv_doc = CSV.read(filepath)
-csv_doc.shift
+csv_doc.shift 
 
-print_by_date(csv_doc, date_path, value.to_i)
+my_account = PersonalFinance.new
+
+if in_string.count("/") == 2 #the string is a date becasue date format is DD/MM/YYYY
+  my_account.value_and_date csv_doc, in_string, value.to_i
+  
+elsif in_string =~ /[\w]/ #the string has letters so it's not a date
+  my_account.by_account csv_doc, in_string
+end
