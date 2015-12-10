d4495e74a2e7ef5d99360fbd3ea44f7c5a3d8788
frozdy <danielkirov@abv.bg>
2015-12-10 22:59:11 +0200

    v3?

--- hm_bonus_minify/A_10_Daniel_Kirov.rb
+++ hm_bonus_minify/A_10_Daniel_Kirov.rb
@@ -1,24 +1,36 @@
 require 'csv'
 
 my_csv = CSV.read ARGV[0]
-date = ARGV[1]
-value = ARGV[2].to_i
+first_argv = ARGV[1]
+second_argv = ARGV[2].to_i
 sum = 0
-rows = []
+final_array = []
 
-
+if !(first_argv =~ /\d/) && !(first_argv.include? '/') && !(first_argv.include? 'xml') 
+  my_csv.each do|line|
+    if line[1] == first_argv
+      sum += line[3].to_i
+      final_array.push line.join(',')
+    end
+  end
+  final_array.sort!
+  final_array.each do |row|
+    puts row
+  end
+  puts sum
+end
 
 if ARGV[2]
 my_csv.each do |line|	
-	if line[0] == date && line[3].to_i.between?((value-10),(value+10))
-		puts line.join(",")
-	end
+  if line[0] == first_argv && line[3].to_i.between?((second_argv-10),(second_argv+10))
+    puts line.join(",")
+  end
 end
 else 
   my_csv.each do |line|	
-	if line[0] == date
-		puts line.join(",")
+	if line[0] == first_argv
+	  puts line.join(",")
 	end
-end
+  end
 end
 
