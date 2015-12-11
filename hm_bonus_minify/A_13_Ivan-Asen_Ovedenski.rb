59f3c998f7420eb260da3907761c09f60cf5a966
4o4oTo <4o4olino70@gmail.com>
2015-12-11 02:10:55 -0800

    V2 works as V1 now

--- hm_bonus_minify/A_13_Ivan-Asen_Ovedenski.rb
+++ hm_bonus_minify/A_13_Ivan-Asen_Ovedenski.rb
@@ -1,19 +1,32 @@
 require 'csv'
 
+def match_dates? line, date
+	line[0] == date
+end
+
+def is_within_value? line, date, value
+	(line[3].to_i > (value - 10)) && (line[3].to_i < (value + 10))
+end
+
 filename = ARGV[0]
-date = ARGV[1]
+first_argument = ARGV[1]
 value = ARGV[2].to_i
 output_info = []
+sum = 0
 
 my_csv = CSV.read filename
 
 my_csv.each do |line|
-	if (line[0] == date)  && (line[3].to_i > (value - 10)) && (line[3].to_i < (value + 10))
+	if value == 0
+		if	(line[0] == first_argument)
+			output_info << line.join(',')
+		end
+	elsif (is_within_value? line, first_argument, value) && (match_dates? line, first_argument)
 		output_info << line.join(',')
 	end
 end
 
 output_info.each do |element|
-	print element
+	puts element
 end
-puts
+
