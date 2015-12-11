e34729486ea46cbe700b352a23e34ae85a2d98ed
frostblooded <frostblooded@yahoo.com>
2015-12-11 11:27:24 +0200

    some changes

--- hm_bonus_minify/A_21_Nikolay_Danailov.rb
+++ hm_bonus_minify/A_21_Nikolay_Danailov.rb
@@ -19,7 +19,9 @@ arg2 = ARGV[1]
 arg3 = ARGV[2].to_f if ARGV.size > 2
 
 if date? arg2
-  output = CSVSearcher.print_date_output arg1, arg2, arg3
-elsif !number? arg2 && arg2 != 'xml'
-  output = CSVSearcher.print_account_output arg1, arg2
+  CSVSearcher.print_date_output arg1, arg2, arg3
+elsif !number? arg2 || arg2 != 'xml'
+  CSVSearcher.print_account_output arg1, arg2
+else
+  puts 'xml'
 end
