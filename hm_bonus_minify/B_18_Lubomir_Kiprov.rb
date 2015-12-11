67eba6fdf04c1e9214340e7a7afe65a3819fd1af
bakasura980 <bakasura@abv.bg>
2015-12-11 19:45:55 +0200

    Add V3

--- hm_bonus_minify/B_18_Lubomir_Kiprov.rb
+++ hm_bonus_minify/B_18_Lubomir_Kiprov.rb
@@ -1,23 +1,41 @@
 require 'csv'
 
-def chek_range_of_amount (value, line)
-	if (line.split(',')[3].to_i > value.to_i - 10 and line.split(',')[3].to_i < value.to_i + 10) || ARGV.size == 2
-		print_line(line)
+def chek_range_of_amount (value, line, date_or_not)
+	date = date_or_not.split("/").size
+	if date > 1
+		if (line.split(',')[3].to_i > value.to_i - 10 and line.split(',')[3].to_i < value.to_i + 10) || ARGV.size == 2
+			print_line(line)
+		end
+	else if date == 1
+			if (line.split(',')[3].to_i == date_or_not.to_i)
+				$sum += line.split(',')[3].to_i
+				print_line(line	)
+			end
+		end
 	end
 end
 
 def print_line (line)
-	puts line
+	$arr << line
+
 end
 
 filename = ARGV[0]
 searching_date = ARGV[1]
+$sum = 0
+$arr = Array.new
 File.open(filename, "r") do |row|
 	row.each do |line|
 		if line.include? searching_date
-			chek_range_of_amount ARGV[2], line
+			chek_range_of_amount ARGV[2], line, ARGV[1]
 		end
 	end
 end
+ 
+$arr.sort! { |a, b| Date.parse(a.split(',')[0]) <=> Date.parse(b.split(',')[0])}
+$arr.each do |ar|
+	puts ar
+end
+puts $sum
 
 
