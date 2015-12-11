16558c6d56deb7c974b5a9f8f9adc7b6fb601cad
Bojidar Zahov <bzahov98@abv.bg>
2015-12-11 21:56:24 +0200

    V3 but need to add sorting

--- hm_bonus_minify/B_04_Bojidar_Zahov.rb
+++ hm_bonus_minify/B_04_Bojidar_Zahov.rb
@@ -2,6 +2,24 @@ file  = ARGV[0]
 date  = ARGV[1]
 value = ARGV[2].to_i
 require 'csv'
+sum = 0
+aaa = date.gsub(/^\//,'')
+if !(aaa == "///" || aaa == "xml") 
+    CSV.open('B_04_Bojidar_Zahov.csv', 'w') do |write|
+     csv = CSV.read file
+	csv.each do |row|
+		if row[1].to_s == aaa 
+	
+			     puts row.join(",")
+			          write << [row.join(",")]
+			          sum+=row[3].to_i
+			          
+		end
+	end
+	
+	puts sum
+end
+end
 
 CSV.open('B_04_Bojidar_Zahov.csv', 'w') do |write|
      csv = CSV.read file
