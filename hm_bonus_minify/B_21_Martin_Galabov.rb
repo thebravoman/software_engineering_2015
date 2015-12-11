5725d0e26affd2f9757f71074fee30db9b79e0f6
Martin_Galabov <martucha1@me.com>
2015-12-11 11:02:39 -0800

    Add Version 3 of Bonus Minify! AWESOMEgit add B_21_Martin_Galabov.rb! I DID ITgit add B_21_Martin_Galabov.rb! I am as surprised as you are, btw!

--- hm_bonus_minify/B_21_Martin_Galabov.rb
+++ hm_bonus_minify/B_21_Martin_Galabov.rb
@@ -1,9 +1,21 @@
-file = ARGV[0]
-date = ARGV[1]
-value = ARGV[2].to_i
-File.open(file).each do |line|
-	if((line.split(",")[0] == date) && (line.split(",")[3].to_i<(value+10) ) && (line.split(",")[3].to_i>(value-10)))
-		puts line
-	end
-end
+require 'csv' 
+file = ARGV[0] 
+string = ARGV[1] 
+output = [] 
+csv_input = CSV.read(file) 
+value = 0 
+csv_input.sort_by! do |first_row| 
+	date = first_row[0].split("/") 
+	[date[2].to_i, date[1].to_i, date[0].to_i] 
+end  
+csv_input.each do |line| 
+	if( line[1] == string) 
+		output.push(line.join(","))
+		value += line[3].to_i 
+	end 
+end 
+puts output
+puts value
+ 
+ 
 
