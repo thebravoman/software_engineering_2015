950545811b6025ed56f023219a2fddb1274735d8
Kalin Karev <karev.kalin@gmial.com>
2015-12-11 17:46:17 +0100

    Minify V3 working

--- hm_bonus_minify/A_15_Kalin_Karev.rb
+++ hm_bonus_minify/A_15_Kalin_Karev.rb
@@ -1,15 +1,17 @@
 require 'csv'
 
 filename=ARGV[0].to_s 
+file_csv = CSV.read(filename)
+
 $data_work = nil
 $account = nil
 $sum = 0;
 
 ARGV[1]
-if (ARGV[1].include?("/"))
-	$data_work = ARGV[1]
-else 
+if (!ARGV[1].include?("/") && !(ARGV[1] =~ /\d/) && (ARGV[1] != "xml"))
 	$account = ARGV[1]
+else 
+	$data_work = ARGV[1]
 end
 
 $value = ARGV[2].to_i;
@@ -21,6 +23,7 @@ def sum_account val
 	end
 end
 
+
 def data_print? val 
 	if $value != nil
 		puts val.join(",") if ((val[0] == $data_work) && (val[3].to_i.between?($value-10, $value+10)))
@@ -29,12 +32,19 @@ def data_print? val
 	end
 end
 
-CSV.read(filename).each do |row|
+
+file_csv.sort_by! do |f|
+	t = f[0].split("/") 
+	[t[2].to_i, t[1].to_i, t[0].to_i];
+end
+
+
+file_csv.each do |row|
 	if ($data_work != nil)
 		data_print? row
 	else 
-		sum_account row if ($account != nil)
+		sum_account row if($account != nil)
 	end 
 end
 
-puts $sum.round(2) if ($acount != nil) 
+puts $sum;
