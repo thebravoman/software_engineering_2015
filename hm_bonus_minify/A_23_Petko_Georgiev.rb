f4f7c42c0e79604ac9ab65b7812a1ba3c6514033
BassMuff <funkmetaljazzmaster@gmail.com>
2015-12-12 21:30:35 +0200

    Add Version 4 of bonus minify homework

--- hm_bonus_minify/A_23_Petko_Georgiev.rb
+++ hm_bonus_minify/A_23_Petko_Georgiev.rb
@@ -1,4 +1,5 @@
 require_relative 'A_23_Petko_Georgiev/read_csv.rb'
+require_relative 'A_23_Petko_Georgiev/xml_printer.rb'
 
 def date? str
   /\d{1,2}\/\d{1,2}\/\d*/.match str
@@ -16,6 +17,7 @@ if date? arg1
 	ReadCSV.print_date_input filename, arg1, arg2
 elsif number? arg1
 elsif arg1 == 'xml'
+	XMLPrinter.print_to_xml filename
 else
 	ReadCSV.print_account_input filename, arg1
 end
