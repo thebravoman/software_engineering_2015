e2a8ec6dfbece54ee106253708aca5f2eaede1d0
viking1998 <google1999@abv.bg>
2015-12-12 16:41:33 +0200

    Finally V4

--- hm_bonus_minify/A_24_Petar_Petrov.rb
+++ hm_bonus_minify/A_24_Petar_Petrov.rb
@@ -3,7 +3,11 @@ require_relative './A_24_Petar_Petrov/csv_parser.rb'
 file = ARGV[0]
 if file.split(".").last == "csv"	
 	result = CSVParser.new.parse_csv(file)
-	result.each do |row| 
-	 puts row.join(',')
+	if ARGV[1] == "xml"
+		puts result
+	else
+		result.each do |row| 
+	 		puts row.join(',')
+		end
 	end
 end
