e952c0de7786d87a54346706652caad1c2ba366d
frostblooded <frostblooded@yahoo.com>
2015-12-12 19:26:10 +0200

    made program into more files

---
+++ hm_bonus_minify/A_21_Nikolay_Danailov/minify_account_printer.rb
@@ -0,0 +1,24 @@
+require_relative 'minify_printer.rb'
+
+class MinifyAccountPrinter < MinifyPrinter
+  def self.print_account_output(csv_filename, account)
+    output = []
+    sum = 0.0
+    csv = CSV.read(csv_filename)[1..-1]
+
+    csv.each do |row|
+      if row[ACCOUNT_COLUMN] == account
+        output << row
+        sum += row[AMOUNT_COLUMN].to_f
+      end
+    end
+
+    output.sort_by! do |row| 
+      date = row[DATE_COLUMN].split '/'
+      [date[2].to_i, date[1].to_i, date[0].to_i]
+    end
+
+    print_csv output
+    puts sum.round 2 if output.size > 0
+  end
+end
