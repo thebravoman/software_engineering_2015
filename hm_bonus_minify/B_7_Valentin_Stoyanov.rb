02fd76f5f3daa8e8d0a0dc3241fd787aa831d7ea
valentin <you@example.com>
2015-12-12 21:52:06 +0200

    Monefy V4

--- hm_bonus_minify/B_7_Valentin_Stoyanov.rb
+++ hm_bonus_minify/B_7_Valentin_Stoyanov.rb
@@ -1,28 +1,52 @@
-file = File.open(ARGV[0])
-string = ARGV[1]
-output = []
-sum = 0
+require 'rexml/document'
+require 'nokogiri'
 
-file.each do |line|
-	l = line.split(',')
-	if l[1].to_s == string.to_s
-		output << line
+#date account category amount currency description
+#01/12/2014 PersonalCash K -100 BGN Some description
+
+@my_csv = []
+string = ARGV[1]
+
+def sortby_account
+	file = File.open(ARGV[0], 'r')
+	file.each do |line|
+		@my_csv << line
+	end
+	@my_csv.sort_by! do |element|
+		[element[1]]
 	end
 end
 
-output.sort_by!{|d| 
-	arr = d.split('/')
-	m = arr[0].to_i
-	d = arr[1].to_i
-	y = arr[2].to_i
-	
-	[y, m, d]
-}
-
-output.each do |element|
-	element = element.split(',')
-	sum += element[3].to_f
+def to_xml  
+	@my_csv.sort_by! { |a| [a[1].to_s.downcase , a[0].split("/").last , a[0].split("/")[1], a[0].split("/").first, a[3].to_f]}
+	my_xml = REXML::Document.new('')
+	minify = my_xml.add_element('minify')
+	@my_csv.each do |line|
+		l = line.split(',')
+		account = minify.add_element('account')
+		account.add_attribute('account',l[1].to_s)
+		date = account.add_element('date')
+		date.add_attribute('date', l.first.to_s)
+		amount = date.add_element('amount')
+		amount.add_text(l[3].to_s)
+	end
+	my_xml
 end
 
-puts output
-puts sum.round(2)
+def sort_csv
+	@my_csv.sort! {|a, b| [b[1].to_s, a[0].to_s, a[3].to_i] <=> [a[1].to_s, b[0].to_s, b[3].to_i] }
+end
+
+def xml_formatter xml
+	formatter = REXML::Formatters::Pretty.new()
+	formatter.compact = true
+	formatter.write(xml, $stdout)
+	puts ''
+end
+
+if string == 'xml'
+	sortby_account
+	sort_csv
+	converted = to_xml
+	xml_formatter converted
+end
