c6e5765d0cdd35babc42396e96af7c28016739ec
frostblooded <frostblooded@yahoo.com>
2015-12-13 07:52:10 +0200

    not minify works with urls

--- hm_bonus_minify/A_21_Nikolay_Danailov/minify_date_printer.rb
+++ hm_bonus_minify/A_21_Nikolay_Danailov/minify_date_printer.rb
@@ -1,10 +1,13 @@
 require 'csv'
+require 'net/http'
+require 'openssl'
+require 'sanitize'
 require_relative 'minify_printer.rb'
 
 class MinifyDatePrinter < MinifyPrinter
-  def self.print_date_output(csv_filename, date, value = nil)
+
+  def self.print_date_output(csv, date, value = nil)
     output = []
-    csv = CSV.read(csv_filename)[1..-1]
 
     csv.each do |row|
       wanted = row[DATE_COLUMN] == date
