aad6609e0f2a92ae21ab89c2060074336bfbbff4
radostin-angelov <angelovradostin@gmail.com>
2015-12-13 18:45:22 +0000

    increased code quality

--- hm_bonus_minify/B_27_Radostin_Angelov.rb
+++ hm_bonus_minify/B_27_Radostin_Angelov.rb
@@ -3,8 +3,8 @@ require 'rexml/document'
 require 'net/http'
 require 'sanitize'
 require 'openssl'
-#require 'open-uri'
 
+DATE_FORMAT  = '%d/%m/%Y'
 
 class Csv_Parser
 	def initialize
@@ -50,26 +50,52 @@ class Csv_Parser
 	end
 end
 
-def read_csv path
-	result = []
-	data = CSV.read(path)
-	data.shift
-	data.each do |row|
-		result.push(row)
+class ResultPrinter
+	def initialize
 	end
 	
-	return result
-end
+	def print_result result
+		sum = 0
+		result.each do |row_values|
+			puts row_values.join(',')
+			sum += row_values[3].to_i
+		end
+		
+		if not result.empty?
+			puts sum
+		end
+	end
+	
+	def to_xml result
+		xml_result = REXML::Document.new("")
+		
+		xml_minify = xml_result.add_element('minify')
+		
+		result_hash = array_to_hash(result)
+		result_hash = result_hash.sort.to_h
+		
+		#xml_marks = xml_word_counts.add_element('marks')
+		#xml_marks.add_text "#{@marks_count}"
 
-def print_result result
-	sum = 0
-	result.each do |row_values|
-		puts row_values.join(',')
-		sum += row_values[3].to_i
-	end
-	
-	if not result.empty?
-		puts sum
+		result_hash.each do |account, dates|
+			xml_account = xml_minify.add_element('account')
+			#xml_account.add_text(key)
+			#value.sort
+			#p account
+			sorted_dates = dates.sort{|first, second| Date.strptime(first[0], DATE_FORMAT) <=> Date.strptime(second[0], DATE_FORMAT)}
+			sorted_dates.each do |date_key, amounts|
+				xml_date = xml_account.add_element('date')
+				#xml_date.add_text(date_key)
+				
+				amounts.sort! {|a, b| a.to_i <=> b.to_i}
+				amounts.each do |amount|
+					xml_amount = xml_date.add_element('amount')
+					xml_amount.add_text(amount)
+				end
+			end
+		end
+
+		puts xml_result
 	end
 end
 
@@ -98,50 +124,19 @@ def array_to_hash array
 	return hash
 end
 
-def to_xml result
-	xml_result = REXML::Document.new("")
-	
-	xml_minify = xml_result.add_element('minify')
-	
-	result_hash = array_to_hash(result)
-	result_hash = result_hash.sort.to_h
-	
-	#xml_marks = xml_word_counts.add_element('marks')
-	#xml_marks.add_text "#{@marks_count}"
 
-	result_hash.each do |account, dates|
-		xml_account = xml_minify.add_element('account')
-		#xml_account.add_text(key)
-		#value.sort
-		#p account
-		date_format = '%d/%m/%Y'
-		sorted_dates = dates.sort{|first, second| Date.strptime(first[0], date_format) <=> Date.strptime(second[0], date_format)}
-		sorted_dates.each do |date_key, amounts|
-			xml_date = xml_account.add_element('date')
-			#xml_date.add_text(date_key)
-			
-			amounts.sort! {|a, b| a.to_i <=> b.to_i}
-			amounts.each do |amount|
-				xml_amount = xml_date.add_element('amount')
-				xml_amount.add_text(amount)
-			end
-		end
-	end
-
-	puts xml_result
-end
 
 path = ARGV[0]
 account = ARGV[1]
 
 parser = Csv_Parser.new
+printer = ResultPrinter.new
 
 if account != "xml"
 	result = parser.parse(path, account)
-	date_format = '%d/%m/%Y'
-	result.sort! {|first, second| Date.strptime(first[0], date_format) <=> Date.strptime(second[0], date_format)}
-	print_result(result)
+	result.sort! {|first, second| Date.strptime(first[0], DATE_FORMAT) <=> Date.strptime(second[0], DATE_FORMAT)}
+	printer.print_result(result)
 else
 	result = read_csv(path)
-	to_xml(result)
+	printer.to_xml(result)
 end
