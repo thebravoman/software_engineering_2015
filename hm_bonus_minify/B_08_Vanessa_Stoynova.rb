9dce880708edeb86a19878620a3eccaaa9ce89b6
Nessi98 <vanesa.nessi98@gmail.com>
2015-12-14 22:08:38 +0200

    can't do V6 so remake it to V5

--- hm_bonus_minify/B_08_Vanessa_Stoynova.rb
+++ hm_bonus_minify/B_08_Vanessa_Stoynova.rb
@@ -8,7 +8,7 @@ sum = 0
 
 file = ARGV[0]
 account = ARGV[1].to_s
-	
+
 def to_xml my_csv
 
 	my_csv = my_csv.sort_by { |a| [a[1].to_s.downcase , a[0].split("/")[2].to_i, a[0].split("/")[1].to_i, a[0].split("/")[0].to_i, a[3].to_i]}
@@ -55,13 +55,16 @@ def link_convertor file
   
 end
 
-def print_and_sort_result result
+def sort_result result
 
 	result.sort_by! do |date|
 		time = date[0].split("/") 
 		[time[2].to_i, time[1].to_i, time[0].to_i];
 	end
-	
+
+end
+
+def print_result result
 	result.each do |line|
 		puts line.join(",")
 	end
@@ -81,20 +84,12 @@ my_csv.each do |line|
 		puts to_xml(my_csv)	
 		break
 		
-	elsif ARGV[1].to_i > 0 && ARGV[2] == nil # version 6
-		month_sum = []
-		month = line[0].split("/")[1].to_i
-		
-		month_sum[month] = line[3].to_i + month_sum[month]
-		
-		#puts month_sum[month]
-	
 	elsif (ARGV[1].split("/").first.to_i >= 0 && ARGV[1].split("/").first.to_i <= 99) && ARGV[2] == nil # version 1
 		date = ARGV[1]
 		if line[0] == date
 			result.push(line)
 		end
-	end 
+	end
 	
 	if ARGV[2] == nil # version 3
 		account = ARGV[1].to_s
@@ -116,7 +111,8 @@ my_csv.each do |line|
 	end
 end
 	
-print_and_sort_result result
+sort_result result
+print_result result
 
 if ARGV[2] == nil && ARGV[1] != "xml" && !(ARGV[1].split("/").first.to_i >= 0 && ARGV[1].split("/").first.to_i <= 31)
 	puts "The amount value for all the output rows is: #{sum}\n"
