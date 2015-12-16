047fab3181f2141dd7ef216fe06a0983fd9cf839
bluemoon1237 <georgynedkov1237@gmail.com>
2015-12-16 23:56:51 +0200

    .

--- hm_quadratic_equation/A_8_Georgi_Tsitselkov.rb
+++ hm_quadratic_equation/A_8_Georgi_Tsitselkov.rb
@@ -2,20 +2,18 @@ a = ARGV[0].to_i
 b = ARGV[1].to_i
 c = ARGV[2].to_i
 
-d = (b**2) - (4*a*c)
+d = (b**2) - 4 * a * c 
 
-elsif d==0
+if d == 0
  
- x = (-b)/(2*a)
- 
+ x = -b / (2 * a)
  print x
 
-else
- 
- x1 = ((-b) + Math.sqrt(d)) / (2*a)
- x2 = ((-b) - Math.sqrt(d)) / (2*a)
- 
- if (x1 < x2)
+elsif d > 0
+ x1 = (-b - Math.sqrt(d)) / (2 * a)
+ x2 = (-b + Math.sqrt(d)) / (2 * a)
+
+if (x1 < x2)
   print "#{x1},#{x2}"
  
  else
