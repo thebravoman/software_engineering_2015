f48056dc041c3faa52cc5ec724772c11fcd497d9
IgnatGeorgiev <ignatgeorgiev1@gmail.com>
2015-12-21 19:19:50 +0200

    Adding lines for svg_tree

--- hm_svg/A_14_Ignat_Georgiev.rb
+++ hm_svg/A_14_Ignat_Georgiev.rb
@@ -15,11 +15,15 @@ def circle x,y,name
 string = '<circle cx="'+x.to_s+'" cy="'+y.to_s+'" r="50" stroke="black" stroke-width="3" fill="white" />' + 
 '<text x="' + (x - 25).to_s + '" y="' + (y - 5).to_s + '" font-family="Verdana" font-size="20" fill="black" >' + name.to_s + '</text>'
 end
+def line x, y, x1, y1
+  '<line x1="'+x.to_s+'" y1="'+y.to_s+'" x2="'+x1.to_s+'" y2="'+y1.to_s+'" style="stroke:rgb(139,69,19);stroke-width:3" />'
+end
 File.open("A_14_Ignat_Georgiev.svg","w") do |f|
 	f.write('<svg xmlns="http://www.w3.org/2000/svg">')
 	parsed.each do |key,value|
 		if key == 'node_name'
 		puts value
+		#f.write(lines 670, 85, (280 + def_pos_x), 250)
 		f.write(circle(def_pos_x,def_pos_y,value))
 		def_pos_y += 200
 		elsif key != 'node_name'
@@ -28,14 +32,16 @@ File.open("A_14_Ignat_Georgiev.svg","w") do |f|
 				new_key.each do |newer_key,classes|
 					if (newer_key=='node_name')
 						puts " "+classes
+						#f.write(lines(def_pos_x,def_pos_y,def_pos_x-70,def_pos_y+200))
 						f.write(circle(def_pos_x,def_pos_y,classes))
-						def_pos_x+=200
-						
+						def_pos_x+=200	
 					elsif(newer_key=='children')
 						classes.each do |newest_key,newest_value|
 							newest_key.each do |name,name_value|
 								#def_pos_y-=200
+
 								puts "  #{name_value}"
+								f.write(lines(def_pos_x-200,def_pos_y,def_pos_x-temp_x,def_pos_y+160))
 								f.write(rect(def_pos_x-temp_x,def_pos_y+temp_y,name_value))
 								#temp_y-=50
 								temp_x -=70
