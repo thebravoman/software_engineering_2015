f139bed73fed12e196ef444e526e5eda8a94c431
George Tsitselkov <g.tsitselkov@gmail.com>
2015-12-21 18:05:21 +0200

    yolo

--- hm_count_words/A_08_Georgi_Tsitselkov/word_counter.rb
+++ hm_count_words/A_08_Georgi_Tsitselkov/word_counter.rb
@@ -1,1 +1,18 @@
-asd
+require 'word_counter/file_parser'
+require 'word_counter/web_parser'
+require 'word_counter/parser'
+
+module WordCounter
+	def self.parse(string)
+		Parser.new.parse(string)
+	end 
+
+	def self.parse_file(filename)
+    		FileParser.new.parse(filename)
+  	end
+ 
+  	def self.parse_webpage(url)
+  		WebParser.new.parse(url)
+ 	end
+end
+
