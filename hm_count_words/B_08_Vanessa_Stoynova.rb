6f2b5eef43a712c5fab11b12976e2888f474f2bd
Nessi98 <vanesa.nessi98@gmail.com>
2015-12-28 19:56:24 +0200

    working on V7

--- hm_count_words/B_08_Vanessa_Stoynova.rb
+++ hm_count_words/B_08_Vanessa_Stoynova.rb
@@ -4,19 +4,21 @@ filename = ARGV[0]
 file_format = ARGV[1]
 
 if filename.start_with?("https://") || filename.start_with?("http://")
-  	result = WordCounter.parse_webpage(filename)
-  else
-    	result = WordCounter.parse_file(filename)
+	result = WordCounter.parse_webpage(filename)
+elsif ARGV[0] == "-d"
+	folder = ARGV[0]
+	result = WordCounter.parse_folder(folder)
+else
+	result = WordCounter.parse_file(filename)
 end
 
 case file_format
 	when 'json'
-  		puts result.to_json
+		puts result.to_json
 	when 'xml'
-  		puts result.to_xml
+		puts result.to_xml
 	when 'svg'
-  		result.to_svg
+		result.to_svg
 	else
-  		puts result.to_csv
+		puts result.to_csv
 end
-
