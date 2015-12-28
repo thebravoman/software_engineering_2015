e60cab5d477e1e65034a7079aeada8e4d84b60a0
Martin_Galabov <martucha1@me.com>
2015-12-28 11:54:34 -0800

    Adding V7 of count words

--- hm_count_words/B_21_Martin_Galabov.rb
+++ hm_count_words/B_21_Martin_Galabov.rb
@@ -7,6 +7,12 @@ file_path = ARGV[0]
 command = ARGV[1]
 if (file_path.start_with?('http://') || file_path.start_with?('https://'))
 	result = WordCounter.web_parse
+elsif(file_path == "-d")
+	folder = command.gsub("\n", '')
+  folder.insert(folder.size, '/**/*.*') 
+	Dir.glob(folder).each do |file|
+		result = WordCounter.parse_file file
+	end
 else
 	result = WordCounter.parse_file file_path
 end
