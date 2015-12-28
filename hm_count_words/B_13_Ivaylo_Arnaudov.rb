c832b59513d13b87e9f0cf0d1d95b60a23bd1e4f
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2015-12-28 16:42:46 +0200

    Added hm_count_words v7 - recursive dir traversal.

--- hm_count_words/B_13_Ivaylo_Arnaudov.rb
+++ hm_count_words/B_13_Ivaylo_Arnaudov.rb
@@ -1,12 +1,23 @@
 require 'word_counter'
+require 'optparse'
 
-file_path = ARGV[0]
+folder = ''
+OptionParser.new do |opt|
+  opt.on('-d', '-d directory_name', 'The name of the directory') { |o| folder = o}
+end.parse!
+
 format = ARGV[1] == nil ? 'csv' : ARGV[1].downcase
 
-if file_path.start_with?("http://", "https://")
-  result = WordCounter.parse_webpage(file_path)
+if folder == ''
+  file_path = ARGV[0]
+
+  if file_path.start_with?("http://", "https://")
+    result = WordCounter.parse_webpage(file_path)
+  else
+    result = WordCounter.parse_file(file_path)
+  end
 else
-  result = WordCounter.parse_file(file_path)
+  result = WordCounter.parse_directory(folder)
 end
 
 case format
--- hm_count_words/B_13_Ivaylo_Arnaudov/word_counter.rb
+++ hm_count_words/B_13_Ivaylo_Arnaudov/word_counter.rb
@@ -1,6 +1,7 @@
 require_relative './word_counter/file_parser'
 require_relative './word_counter/parser'
 require_relative './word_counter/web_parser'
+require_relative './word_counter/directory_parser'
 
 module WordCounter
   def self.parse(string)
@@ -9,6 +10,10 @@ module WordCounter
 
   def self.parse_file(filename)
     FileParser.new.parse(filename)
+  end
+
+  def self.parse_directory(dirname)
+    DirectoryParser.new.parse_dir(dirname)
   end
 
   def self.parse_webpage(url)
