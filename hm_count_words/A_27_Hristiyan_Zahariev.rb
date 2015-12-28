739af4113313e18e285bb32d0a66775216418eea
HristiyanZahariev <ihzahariev@gmail.com>
2015-12-28 19:06:56 +0200

    It doesnt work properly

--- hm_count_words/A_27_Hristiyan_Zahariev.rb
+++ hm_count_words/A_27_Hristiyan_Zahariev.rb
@@ -2,40 +2,62 @@ require_relative 'A_27_Hristiyan_Zahariev/word_counter.rb'
 require 'csv'
 require 'json'
 require 'rexml/document'
+require 'fileutils'
 
 input_filename = ARGV[0]
 output_option = ARGV[1]
 
-def checkFileOrSite(filename)
+def FileOrSite?(filename)
   protocol = filename.split('/').first
   return protocol == 'http:' || protocol == "https:"
 end
 
-def result(input_filename,output_option)
+def WhatToCall? (input_filename, output_option)
   if File.file? input_filename
     result = WordCounter.parse_file input_filename
-  elsif checkFileOrSite input_filename
+  elsif FileOrSite? input_filename
     result = WordCounter.parse_webpage input_filename
   else
     result = Parser.parse input_filename
   end
+  printing(result, output_option)
+end
 
+def printing (result, output_option)
   if output_option == 'csv'
     result.to_csv
-    result.to_svg
   elsif output_option == 'json'
     result.to_json
-    result.to_svg
   elsif output_option == 'xml'
     result.to_xml
-    result.to_svg
-  elsif output_option == "svg"
-    result.to_svg
   else
     result.to_csv
-    result.to_svg
   end
+  result.to_svg
+end
 
-end 
+def PathParser (input_filename, output_option)
+  files = Dir.glob("#{input_filename}/**/*").select { |f| File.file? f }
+  combined_files = []
 
-result(input_filename,output_option)
+  files.each do |file|
+    con = File.read(file)
+    combined_files << con
+  end
+  Dir.mkdir("SomeFolder")  unless Dir.exist?("SomeFolder") #Trying something stupid
+  File.open("SomeFolder/combined.txt","w") do |file| 
+    combined_files.each do |line|
+      file << line 
+    end
+  end
+  WhatToCall?("SomeFolder/combined.txt",output_option)
+  FileUtils.remove_dir("SomeFolder")
+end
+
+if ARGV[0] == '-d'
+  input_filename = ARGV[1]
+  output_option = ARGV[2]
+  PathParser(input_filename, output_option)
+else
+  WhatToCall?(input_filename,output_option)
+end
