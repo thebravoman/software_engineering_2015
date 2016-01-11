0d08fd424c9a6863ccb1073a2e71cc9922a2c7c2
frostblooded <frostblooded@yahoo.com>
2016-01-11 16:12:28 +0200

    now makes the results folder if it doesnt exits

--- hm_3_million_lines/A_21_Nikolay_Danailov/main.rb
+++ hm_3_million_lines/A_21_Nikolay_Danailov/main.rb
@@ -1,19 +1,26 @@
+
+
 require 'word_counter'
 require_relative 'src/SVG/SVG.rb'
 
 def write_to_file(file, data)
   File.open(file, 'w') do |f|
-    f << data
+  f << data
   end
 end
 
 def handle_repo_parsing(folder, file_extension, json_res_file, svg_res_file)
-	result = WordCounter.parse_folder folder, file_extension
-	write_to_file json_res_file, result.to_json
-	SVGWriter.make_svg result, svg_res_file
+  result = WordCounter.parse_folder folder, file_extension
+  write_to_file json_res_file, result.to_json
+  SVGWriter.make_svg result, svg_res_file
 end
 
 folder = ARGV[0]
+
+if !File.directory? 'results'
+  Dir.mkdir 'results'
+end
+
 handle_repo_parsing folder, :rb, "results/ruby.json", "results/ruby.svg"
 handle_repo_parsing folder, :cpp, "results/cpp.json", "results/cpp.svg"
 handle_repo_parsing folder, :java, "results/java.json", "results/java.svg"
