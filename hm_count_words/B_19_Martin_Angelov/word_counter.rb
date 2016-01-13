f7609d81c3f8da3fe3d3f973cbceb09c9ad59972
Martin <mart.com@mail.bg>
2016-01-13 18:11:13 +0200

    Update word_counter.rb

--- hm_count_words/B_19_Martin_Angelov/word_counter.rb
+++ hm_count_words/B_19_Martin_Angelov/word_counter.rb
@@ -1,17 +1,24 @@
 require_relative 'word_counter/file_parser'
 require_relative 'word_counter/parser'
 require_relative 'word_counter/web_parser'
+require_relative 'word_counter/result'
 
 module WordCounter
-  def self.parse_file(filename)
-    FileParser.new.parse(filename)
-  end
-  
+	
   def self.parse(string)
-    Parser.new.parse(string)
+    Parser.new.parse_string(string)
   end
-  
-  def self.parse_webpage(uri)
-    WebpageParser.new.parse(uri)
+
+  def self.parsing_the_file(filename)
+    FileParser.new.parse_file(filename)
   end
+
+  def self.parsing_web_source(url)
+    WebParser.new.parse_web(url)
+  end
+
+  def self.parse_folder(folder, format)
+    FolderParse.new.parsing_folder(folder, format)
+  end
+
 end
