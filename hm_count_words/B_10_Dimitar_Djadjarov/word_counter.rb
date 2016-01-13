4842866d35988d65b9600ef43a51c0f16703a0b3
Euphoria4Men <dimitar_djadjarov@abv.bg>
2016-01-13 17:03:06 +0200

    Update word_counter.rb

--- hm_count_words/B_10_Dimitar_Djadjarov/word_counter.rb
+++ hm_count_words/B_10_Dimitar_Djadjarov/word_counter.rb
@@ -5,15 +5,21 @@ require './B_10_Dimitar_Djadjarov/Legit/parser'
 require './B_10_Dimitar_Djadjarov/Legit/result'
 
 module WordCounter
-	def self.parse string
-	  Parser.new.parse(string)
-	end
 	
-	def self.parse_file path
-	  FileParser.new.parse(path)
-	end
+  def self.parse(string)
+    Parser.new.parse_string(string)
+  end
 
-	def self.parse_web path
-	  WebParser.new.parse(path)
-	end
+  def self.parsing_the_file(filename)
+    FileParser.new.parse_file(filename)
+  end
+
+  def self.parsing_web_source(url)
+    WebParser.new.parse_web(url)
+  end
+
+  def self.parse_folder(folder, format)
+    FolderParse.new.parsing_folder(folder, format)
+  end
+
 end
