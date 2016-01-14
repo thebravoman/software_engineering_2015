2be1fb40b8066d52385006e2e3c519566a788582
frostblooded <frostblooded@yahoo.com>
2016-01-14 12:29:46 +0200

    db puts files into database correctly, but doesn't correctly check for file existence

--- hm_count_words/A_21_Nikolay_Danailov/word_counter/folder_parser.rb
+++ hm_count_words/A_21_Nikolay_Danailov/word_counter/folder_parser.rb
@@ -1,4 +1,5 @@
 require 'word_counter/file_parser'
+require_relative '../db_utils.rb'
 
 module WordCounter
   # Parses files
@@ -8,7 +9,9 @@ module WordCounter
       result = WordCounter::Result.new
 
       files.each do |file|
+        get_res_from_for_file file
         temp = super file
+        save_res_to_db temp, file
         result.word_counts.merge!(temp.word_counts.to_h) { |_, oldval, newval| newval + oldval }
         result.marks_count += temp.marks_count
       end
