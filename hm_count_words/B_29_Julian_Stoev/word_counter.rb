9ae01a823f72376d707b2671d514da693bcbbe32
OmniJ <xxthespinerxx@gmail.com>
2016-01-19 16:17:31 +0200

    Fixed V7

--- hm_count_words/B_29_Julian_Stoev/word_counter.rb
+++ hm_count_words/B_29_Julian_Stoev/word_counter.rb
@@ -4,6 +4,11 @@ require 'sanitize'
 require 'openssl'
 
 class WordCounter
+	
+	def initialize
+		@data = ""
+	end
+	
 	def parse txt
 		words_hash = Hash.new(0)
 
@@ -39,33 +44,21 @@ class WordCounter
 
 		parse txt 
 	end
-
-	def process_files file
-		txtfile = File.open(file, "r")
-		txt = IO.read(txtfile)
-		
-		words_hash = Hash.new(0)
-
-		sumofmarks = txt.scan(/[[:punct:]=`~$^+|<>]/u).length
-
-		content = txt.downcase.tr('^A-Z ^0-9 ^a-z', ' ').split(' ')
-		content.each do |word|
-			words_hash[word] = words_hash[word]+1 
-		end
-
-		words_hash = words_hash.sort_by {|key, value| [-value, key]}	
-		
-		return Result.new(words_hash, sumofmarks)
-	end
 	
-	def parse_folder folder
-		Dir.glob(folder + '*') do |obj|
-					
-			if File.file? obj
-				data = parse_file(obj)
-			else
-				parse_folder(obj)
+	def parse_folder_data folder
+		Dir.glob("#{folder}/*").each_with_object({}) do |obj, h|	
+			if File.file?(obj)
+				@data << File.read(obj)
+			elsif File.directory?(obj)
+				@data = parse_folder(obj)
 			end							
 		end
 	end
+	
+	def parse_folder folder
+		parse_folder_data(folder)
+		parse(@data)
+	end	
 end
+
+
