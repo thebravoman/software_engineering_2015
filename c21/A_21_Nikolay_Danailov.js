29093567a4cb9a7ece8d04d82e8e1d525e40b8c3
frostblooded <frostblooded@yahoo.com>
2016-02-17 18:10:02 +0200

    task 6

--- c21/A_21_Nikolay_Danailov.js
+++ c21/A_21_Nikolay_Danailov.js
@@ -1,3 +1,15 @@
-rect(10, 10, 100, 100);
-fill(0, 0, 0);
-text('hello world!', 100, 100);
+var draw_matrix = function(matrix){
+    var RECT_WIDTH = 50;
+    var RECT_HEIGHT = 50;
+    
+    for(var i = 0; i < matrix.length; i++){
+        for(var j = 0; j < matrix[i].length; j++){
+            fill(255, 255, 255);
+            rect(i * RECT_WIDTH, j * RECT_HEIGHT, RECT_WIDTH, RECT_HEIGHT);
+            fill(0, 0, 0);
+            text(matrix[i][j], i * RECT_WIDTH + RECT_WIDTH * 0.3, j * RECT_HEIGHT + RECT_HEIGHT / 2);
+        }
+    }
+};
+
+draw_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
