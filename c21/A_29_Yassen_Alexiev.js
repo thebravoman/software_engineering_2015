dbce2f19059b1341dfd03df10f324a114fe31a9b
Streetfighter4 <groznika123@gmail.com>
2016-02-20 10:56:49 +0200

    task6

--- c21/A_29_Yassen_Alexiev.js
+++ c21/A_29_Yassen_Alexiev.js
@@ -1,5 +1,12 @@
-for (var i = 0; i < 7; i++) {
-    for (var j = 0; j < 4; j++){
-        rect(50*i, 50*j, 50, 50); 
+var matrix = function(matrix){
+    for(var j = 0; j < matrix.length; j++){
+        for(var i = 0; i < matrix[j].length; i++){
+            fill(0, 255, 0);
+            rect(j * 80, i * 80, 80, 80);
+            fill(0, 0, 0);
+            text(matrix[i][j], j * 80 + 80 * 0.3, i * 80 + 80 / 2);
+        }
     }
-}
+};
+
+matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
