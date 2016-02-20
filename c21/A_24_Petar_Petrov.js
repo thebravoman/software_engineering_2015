3e95bdaae615ce9bccddf6fe153ddf79579f0a04
viking1998 <google1999@abv.bg>
2016-02-20 14:10:52 +0200

    Task 6

--- c21/A_24_Petar_Petrov.js
+++ c21/A_24_Petar_Petrov.js
@@ -1,16 +1,24 @@
 var matrix_cols = 5; 
-var matrix_rows = 6; 
-var matrix_x = 0; 
-var matrix_y = 0; 
-var matrix_width = 30; 
-var matrix_height = 20; 
-fill(43, 219, 27); 
-for(var i = 0; i<matrix_rows; i++) {     
+var matrix_rows = 7; 
+var matrix = new Array(matrix_rows); 
+for(var i=0; i<matrix_rows; i++) {     
+	matrix[i] = new Array(matrix_cols);     
 	for(var c=0; c<matrix_cols; c++) {         
-		rect(matrix_x,matrix_y,matrix_width,matrix_height);         
-		matrix_x += 30;     
-	}     
-	matrix_y += 20;     
-	matrix_x = 0; 
-}
+		matrix[i][c] = (i+1)*c;     
+	} 
+} 
+var Draw = function (matrix,rows,cols) {  
+   var matrix_x = 5;     
+   var matrix_y = 5;   
+   fill(212, 30, 30);     
+   for(var i=0; i<rows; i++) {         
+		 for(var c = 0; c<cols; c++) {       
+   		 text(matrix[i][c],matrix_x,matrix_y,20,20);             
+   		 matrix_x += 20;         
+   	 }         
+   	 matrix_x = 5;         
+   	 matrix_y += 20;     
+   } 
+};  
+Draw(matrix, matrix_rows, matrix_cols);
 
