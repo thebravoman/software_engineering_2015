0cbfb51ce177000f260cea11b29a33fda167d27c
Icaka <ici_ioana@abv.bg>
2016-02-21 15:08:03 +0200

    Task VI

--- c21/A_28_Hristo_Ezekiev.js
+++ c21/A_28_Hristo_Ezekiev.js
@@ -1,9 +1,21 @@
-var x = 0;
-for(var i=0;i<3;++i)
+var Matrix = function(rows, cols)
 {
-    for(var t=0;t<4;++t)
+    var x = 0;
+    var y = 0;
+    for(var i=0;i<rows;++i)
     {
+        
+        rect(x, y, 20, 20);
+        for(var t=0;t<cols-1;++t)
+        {
+            y = y + 25;
+            rect(x, y, 20, 20);
+        }
         x = x + 25;
-        rect(x, 10, 20, 20);
+        y = 0;
     }
-}
+};
+
+var rows=3;
+var columns=4;
+Matrix(rows, columns);
