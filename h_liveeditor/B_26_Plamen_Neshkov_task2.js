5bc4e6a9d54cbe1d44d63d440a4562ca0f3354af
Plamen Neshkov <PlamenNeshkov@users.noreply.github.com>
2016-02-22 21:46:31 +0200

    Changes, changes.

--- h_liveeditor/B_26_Plamen_Neshkov_task2.js
+++ h_liveeditor/B_26_Plamen_Neshkov_task2.js
@@ -1,34 +1,32 @@
+var middle = 200;
+
 var getQuadEqOutput = function(a, b, c, x) { 
     return a*x*x + b*x + c;
 };
 
 var graphQuadEq = function(a, b, c) {
-    var middle = 200;
-    
     var x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4*a*c)) / (2*a);
     var x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4*a*c)) / (2*a);
+    
     if (isNaN(x1) || isNaN(x2)) {
         println("No real roots.");   
     } else {
-        println(x1);
-        println(x2);
-  
+        println("{x1 = " + x1 + "}, {x2 = " + x2 + "}");
+    }
         var outputs = [];
+        
         for (var i = 0; i < 100; i++) {
             outputs[i] = [i, getQuadEqOutput(a, b, c, i)];
         }
         
         for (var i = 0; i < 99; i++) {
-            println(outputs[i]);
             line(middle + outputs[i][0], middle - outputs[i][1], 
                  middle + outputs[i+1][0], middle - outputs[i+1][1]);
         }
         for (var i = 0; i < 99; i++) {
-            println(outputs[i]);
             line(middle - outputs[i][0], middle - outputs[i][1], 
                  middle - outputs[i+1][0], middle - outputs[i+1][1]);
         }
-    }
 };
 
 graphQuadEq(1, -2, -3);
