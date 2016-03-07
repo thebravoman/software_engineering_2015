17a0454b18486e0108a2b411cdbccc958b893bd2
Nikolay Karagyozov <newmast@abv.bg>
2016-03-07 18:33:23 +0200

    added comments controller and comments migration

---
+++ c23/TaskUploader/app/views/comments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @comment, :id, :content, :commentable_id, :commentable_type, :created_at, :updated_at
--- c23/TaskUploader/db/schema.rb
+++ c23/TaskUploader/db/schema.rb
@@ -11,17 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307150201) do
+ActiveRecord::Schema.define(version: 20160307163053) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "solutions", force: :cascade do |t|
     t.integer  "task_id"
     t.text     "code"
     t.text     "author"
-    t.integer  "automatic_score"
-  	t.integer  "manual_score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text     "tests_output"
   end
 
   create_table "tasks", force: :cascade do |t|
