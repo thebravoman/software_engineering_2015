88313a96821d945f92e4180ac1629a598d0086ca
Aleksandar Ivanov <aivanov92@gmail.com>
2016-03-14 18:37:51 +0200

    Add basic sidekiq worker

--- c23/TaskUploader/config/routes.rb
+++ c23/TaskUploader/config/routes.rb
@@ -1,5 +1,9 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   resources :comments
   resources :solutions, :has_many => :comments
   resources :tasks, :has_many => :comments
+
+  mount Sidekiq::Web => '/jobs' 
 end
