e607468c8c7c13368aeb91e6aa2a92acfae1f209
Aleksandar Ivanov <aivanov92@gmail.com>
2016-03-14 17:18:48 +0200

    Add edit and new for tasks

--- c23/TaskUploader/app/controllers/tasks_controller.rb
+++ c23/TaskUploader/app/controllers/tasks_controller.rb
@@ -6,4 +6,41 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to action: :show
+    else
+      render action: :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update_attributes(task_params)
+      redirect_to action: :show
+    else
+      render action: :edit
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task)
+      .permit(:title, :description, :tests)
+  end
 end
+
+
