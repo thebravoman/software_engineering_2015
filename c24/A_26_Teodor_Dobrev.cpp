4484113686a66b8d78d29486bfbda8cc9da45c9a
Velioo <velioocs@mail.bg>
2016-03-16 16:58:48 +0200

    Adding task1

---
+++ c24/A_26_Teodor_Dobrev.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cin << n;
+    for(int i = 0; i <= n; i++) {
+        if (n % i == 0) {
+            cout i;
+        }
+    }
+    return 0;
+}
