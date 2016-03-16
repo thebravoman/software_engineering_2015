444ae63ee6c28e670f0373970870c190991032cc
mehtehtrollface <mehtehtrollface@users.noreply.github.com>
2016-03-16 17:20:24 +0200

    Added a debug function

--- c24/A_18_Metodi_Ilchev.lua
+++ c24/A_18_Metodi_Ilchev.lua
@@ -9,16 +9,17 @@ function deliteli(num)
   return tb
 end
 
+function printtable(tab)
+  for k,v in pairs(tab) do
+    print(k.." = "..v)
+  end
+end
 
 n=prime1*prime2
 tont=(prime1-1)*(prime2-1)
 
 local deli=deliteli(tont)
-for k,v in pairs(deli) do
-  print(v)
-end
-for i=2,tont-1 do
-  for k,v in pairs(deli) do
-    
-  end
-end
+
+local temp={}
+-- MORE CODE TO BE ADDED HERE
+printtable(deli)
