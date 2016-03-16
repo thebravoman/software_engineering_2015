c9478572b58f2341274d7ce024713f49579c37c0
frostblooded <frostblooded@yahoo.com>
2016-03-16 16:54:33 +0200

    refactor divisers program

--- c24/A_21_Nikolay_Danailov.rb
+++ c24/A_21_Nikolay_Danailov.rb
@@ -1,12 +1,12 @@
 num = ARGV[0].to_i
-count = 0
+divisers_count = 0
 
 (2...num).each do |i|
   if num % i == 0
     print "#{i} "
-    count += 1
+    divisers_count += 1
   end
 end
 
 puts
-puts "The number #{num} has #{count} devisers"
+puts "The number #{num} has #{divisers_count} devisers"
