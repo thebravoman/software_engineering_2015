7b051b481eea8178cf19cd62c297e06f475c0ef5
Nessi98 <vanesa.nessi98@gmail.com>
2016-03-16 15:52:42 +0200

    classwork

--- c24/B_08_Vanessa_Stoynova.rb
+++ c24/B_08_Vanessa_Stoynova.rb
@@ -1,38 +1,68 @@
 # 1. p = 17, q = 23
 # 2. n = 391
 # 3. phi(n) = 352
+# 4. e = 117
+# 5. d = 349, 701
 
 def get_divisors(number)
-	result = []
+  result = []
 
-  	for counter in 1..number / 2
-    	result.push(counter) if number % counter == 0
-  	end
+  for counter in 1..number/2
+    result.push(counter) if number % counter == 0
+  end
 
-  	result.push(number)
-  	result
+  result.push(number)
+  result
 end
 
 def get_gcd(first, second)
 
-	first_divisors = get_divisors(first)
-	second_divisors = get_divisors(second)
+  first_divisors = get_divisors(first)
+  second_divisors = get_divisors(second)
 
-  	gcd = first_divisors.first
+  gcd = first_divisors.first
 
-  	first_divisors.each do |first_div|
-  		
-    		second_divisors.each do |second_div|
-      			if first_div == second_div
-        			gcd = first_div if first_div > gcd
-      			end
-    		end
-  	end
+  first_divisors.each do |first_div|
 
-  	gcd
+    second_divisors.each do |second_div|
+
+      if first_div == second_div
+        gcd = first_div if first_div > gcd
+      end
+    end
+  end
+
+  gcd
+
+end
+
+def get_modular_multiplicative_inverse(a, n)
+  t = 0
+  nt = 1
+  r = n
+  nr = a % n
+
+  n = -n if n < 0
+  a = n - (-a % n) if a < 0
+
+  while nr != 0
+    quot = (r / nr) | 0;
+    tmp = nt
+    nt = t - quot * nt
+    t = tmp
+    tmp = nr
+    nr = r - quot * nr
+    r = tmp
+  end
+
+  return -1 if r > 1
+
+  t += n if t < 0
+  return t;
 end
 
 number1 = ARGV[0].to_i
 number2 = ARGV[1].to_i
 
-puts get_gcd(number1, number2)
+puts 29 ** 117 % 391
+puts 3 ** 349 % 391
