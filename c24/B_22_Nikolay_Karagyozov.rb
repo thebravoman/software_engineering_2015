8a58ba6190090b1bea7739a7694bbe95e9ac9c41
Nikolay Karagyozov <newmast@abv.bg>
2016-03-16 15:47:35 +0200

    updated work

--- c24/B_22_Nikolay_Karagyozov.rb
+++ c24/B_22_Nikolay_Karagyozov.rb
@@ -1,7 +1,8 @@
 # 1. p = 17, q = 23
 # 2. n = 391
 # 3. phi(n) = 352
-# 4.
+# 4. e = 117
+# 5. d = 349, 701
 
 def get_divisors(number)
   result = []
@@ -29,7 +30,33 @@ def get_gcd(first, second)
   gcd
 end
 
+def get_modular_multiplicative_inverse(a, n)
+  t = 0
+  nt = 1
+  r = n
+  nr = a % n
+
+  n = -n if n < 0
+	a = n - (-a % n) if a < 0
+
+  while nr != 0
+    quot = (r / nr) | 0;
+    tmp = nt
+    nt = t - quot * nt
+    t = tmp
+    tmp = nr
+    nr = r - quot * nr
+    r = tmp
+  end
+
+  return -1 if r > 1
+
+  t += n if t < 0
+  return t;
+end
+
 first_number = ARGV[0].to_i
 second_number = ARGV[1].to_i
 
-puts get_gcd(first_number, second_number)
+puts 29 ** 117 % 391
+puts 3 ** 349 % 391
