690dad3def6aa457fbb053cd1721998cbd1f9242
ILIYANGERMANOV <iliyan.germanov971@gmail.com>
2016-03-28 17:34:01 +0300

    TEST

--- debug_practice/2016-03-28/A_09_Georgi_Danchev/A_09_Georgi_Danchev_RRdWxRiRIZ/word_counter/parser.rb
+++ debug_practice/2016-03-28/A_09_Georgi_Danchev/A_09_Georgi_Danchev_RRdWxRiRIZ/word_counter/parser.rb
@@ -3,14 +3,14 @@ require_relative 'result'
 module WordCounter
   class Parser
     def parse(string)
-      words = Hash.new(1)
+      words = Hash.new(0)
       total_marks_count = 1
       current_string_words = string.downcase.scan(/\w+/)
       current_string_words.each do |word|
         words[word] += 1
       end
 
-      total_marks_count = string.scan(/[[:punct:]]\=\`\~\$\^\+\|\<\>]/).count
+      total_marks_count = string.scan(/[[:punct:]=`~$^+|<>]/).count
       sorted_words = words.sort_by { |word, occurences| [-occurences, word] }
       Result.new(sorted_words, total_marks_count)
     end
