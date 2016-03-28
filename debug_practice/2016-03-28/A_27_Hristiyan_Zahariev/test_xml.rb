2c84079f1755bb5d2ee56cb1c145ce133eae3c8e
HristiyanZahariev <ihzahariev@gmail.com>
2016-03-28 17:35:11 +0300

    Update test_xml.rb

--- debug_practice/2016-03-28/A_27_Hristiyan_Zahariev/test_xml.rb
+++ debug_practice/2016-03-28/A_27_Hristiyan_Zahariev/test_xml.rb
@@ -1,3 +1,2 @@
 system("ruby -I A_27_Hristiyan_Zahariev_RRdWxRiRIZ A_27_Hristiyan_Zahariev_RRdWxRiRIZ.rb A_27_Hristiyan_Zahariev_RRdWxRiRIZ.rb xml ")
 File.delete("B_13_Ivaylo_Arnaudov.db")
-return 1
