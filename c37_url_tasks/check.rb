416c1c9913f640f42fc710b35adc16d63dab3ebe
Kiko <k.mitov@robopartans.com>
2016-05-11 16:51:40 +0300

    Checking the second url

--- c37_url_tasks/check.rb
+++ c37_url_tasks/check.rb
@@ -1,16 +1,29 @@
 require 'csv'
 
-results = {}
-Dir.glob("*.txt") do |file|
-	line = File.readlines(file).first.strip
-	p file
-	request = "curl --request GET \"#{line}?a=1&b=1&c=-4\""
-	results[file] = `#{request}`
-	p results[file]
+def send_request line_number, params
+	results = {}
+	Dir.glob("*.txt") do |file|
+		lines = File.readlines(file)
+		if lines.size > line_number
+			line = lines[line_number].strip
+			p file
+			request = "curl --request GET \"#{line}?#{params}\""
+			results[file] = `#{request}`
+			p results[file]
+		end
+	end
+	results
 end
 
-CSV.open("resuls.csv", "w") do |csv|
-	results.keys.each do |file|
-		csv << [file,results[file]]
+
+def write_file results
+	CSV.open("resuls.csv", "w") do |csv|
+		results.keys.each do |file|
+			csv << [file,results[file]]
+		end
 	end
 end
+# url quadratic - 0 "a=1&b=1&c=-4"
+# url quadratic json - 1 %7B%22a%22%3A1%2C%22b%22%3A1%2C%22c%22%3A-4%7D
+results = send_request ARGV[0].to_i, ARGV[1]
+write_file results
